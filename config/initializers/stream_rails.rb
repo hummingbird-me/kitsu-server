93abcfda83582815bd9f22210f56e7be67e1def8
Peter Lejeck <peter.lejeck@gmail.com>
2016-10-05 14:40:07 -0700

    List feeds we use in stream_rails initializer

--- config/initializers/stream_rails.rb
+++ config/initializers/stream_rails.rb
@@ -1,3 +1,11 @@
+# Feeds:
+#   user/2 (flat) - Posts made by user/2
+#   user_aggr/2 (aggregated) - Follows user by id for display
+#   media/anime:8932 (flat) - Gets CC'd posts related to the media
+#   media_aggr/anime:8932 (aggregated) - Follows media feed by id for display
+#   timeline/2 (aggregated) - Follows user feeds for whoever we follow
+#   notifications/2 (notifications) - Gets CC'd notifications for the user
+
 StreamRails.configure do |config|
   config.api_key = ENV['STREAM_API_KEY']
   config.api_secret = ENV['STREAM_API_SECRET']
