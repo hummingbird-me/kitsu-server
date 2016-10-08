44644b71deed413ccdd7ea5b1078f204355653a3
toyhammered <daniel@rassiner.com>
2016-10-07 22:15:27 -0700

    adds media_info and status

--- app/models/list_import/anime_planet/row.rb
+++ app/models/list_import/anime_planet/row.rb
@@ -10,13 +10,26 @@ class ListImport
 
       def media_info
         {
-          title: node.at_css('.tableTitle').text
-        }
+          title: node.at_css('.tableTitle').text,
+          show_type: node.at_css('.tableType')&.text,
+          episode_count: node.at_css('.epsRating select.episodes option[selected]')&.text&.to_i
+        }.compact
+      end
+
+      def status
+        case node.at_css('.epsRating select.changeStatus option[selected]')&.text
+        when 'Watched' then :completed
+        when 'Watching' then :current
+        when 'Want to Watch' then :planned
+        when 'Stalled' then :on_hold
+        when 'Dropped' then :dropped
+        when "Won't Watch" then :what_the_fuck_goes_here
+        end
       end
 
 
       def data
-      %i[media_info ].map do |k|
+      %i[media_info].map do |k|
         [k, send(k)]
       end.to_h
     end
--- spec/models/list_import/anime_planet/row_spec.rb
+++ spec/models/list_import/anime_planet/row_spec.rb
@@ -4,19 +4,89 @@ RSpec.describe ListImport::AnimePlanet::Row do
   let(:anime) { fixture('list_import/anime_planet/toy-anime.html') }
   let(:manga) { fixture('list_import/anime_planet/toy-manga.html') }
 
-  subject { described_class.new(
-    Nokogiri::HTML(anime).css('table.personalList tr')[1]
-  )}
+
 
   context 'Anime' do
+    subject { described_class.new(
+      Nokogiri::HTML(anime).css('table.personalList tr')[1]
+    )}
     describe '#media_info' do
       it 'should return the title' do
         expect(subject.media_info[:title]).to eq('07-Ghost')
+      end
+
+      it 'should return show_type' do
+        expect(subject.media_info[:show_type]).to eq('TV')
+      end
+
+      it 'should return episodes watched' do
+        expect(subject.media_info[:episode_count]).to eq(25)
+      end
+    end
+
+    describe '#status' do
+      context 'of "Watched"' do
+        it 'should return :completed' do
+          subject = described_class.new(Nokogiri::HTML(anime).css('table.personalList tr')[1])
+
+          expect(subject.status).to eq(:completed)
+        end
+      end
+      context 'of "Watching"' do
+        it 'should return :current' do
+          subject = described_class.new(Nokogiri::HTML(anime).css('table.personalList tr')[2])
+
+          expect(subject.status).to eq(:current)
+        end
+      end
+      context 'of "Want to Watch"' do
+        it 'should return :planned' do
+          subject = described_class.new(Nokogiri::HTML(anime).css('table.personalList tr')[3])
+
+          expect(subject.status).to eq(:planned)
+        end
+      end
+      context 'of "Stalled"' do
+        it 'should return :on_hold' do
+          subject = described_class.new(Nokogiri::HTML(anime).css('table.personalList tr')[4])
+
+          expect(subject.status).to eq(:on_hold)
+        end
+      end
+      context 'of "Dropped"' do
+        it 'should return :dropped' do
+          subject = described_class.new(Nokogiri::HTML(anime).css('table.personalList tr')[5])
+
+          expect(subject.status).to eq(:dropped)
+        end
+      end
+      context 'of "Wont Watch"' do
+        it 'should return :what_the_fuck_goes_here' do
+          subject = described_class.new(Nokogiri::HTML(anime).css('table.personalList tr')[6])
+
+          pending "What the fuck is this actually?"
+          # expect(subject.status).to eq(:what_the_fuck_goes_here_seriously)
+        end
       end
     end
   end
 
   context 'Manga' do
+    subject { described_class.new(
+      Nokogiri::HTML(manga).css('table.personalList tr')[1]
+    )}
+    describe '#media_info' do
+      it 'should return the title' do
+        expect(subject.media_info[:title]).to eq('07-Ghost')
+      end
 
+      it 'should return show_type' do
+        expect(subject.media_info[:show_type]).to eq(nil)
+      end
+
+      it 'should return episodes watched' do
+        expect(subject.media_info[:episode_count]).to eq(nil)
+      end
+    end
   end
 end
