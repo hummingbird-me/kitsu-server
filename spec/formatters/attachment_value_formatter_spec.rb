f1ae1bd0ce273ff795d8e6a97119059917109a8c
Peter Lejeck <peter.lejeck@gmail.com>
2016-10-11 12:08:14 -0700

    Stub Attachment#blank? for formatter testing, test empty state

--- spec/formatters/attachment_value_formatter_spec.rb
+++ spec/formatters/attachment_value_formatter_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe AttachmentValueFormatter do
   subject { AttachmentValueFormatter }
+  let(:attachment) do
+    Paperclip::Attachment.new(:avatar, double, styles: {
+      big: 'test',
+      small: 'test'
+    })
+  end
 
   context '.format' do
     context 'without attachment' do
@@ -9,13 +15,9 @@ RSpec.describe AttachmentValueFormatter do
         expect { subject.format('') }.to raise_error('Invalid attachment field')
       end
     end
-    context 'with an attachment' do
-      let(:attachment) do
-        Paperclip::Attachment.new(:file, double, styles: {
-          big: 'test',
-          small: 'test'
-        })
-      end
+
+    context 'with a non-empty attachment' do
+      before { allow(attachment).to receive(:blank?).and_return(false) }
       let(:formatted) { subject.format(attachment) }
       it 'should not raise an error' do
         expect { formatted }.not_to raise_error
@@ -26,6 +28,17 @@ RSpec.describe AttachmentValueFormatter do
       it 'should return all specified styles' do
         expect(formatted).to include(:big)
         expect(formatted).to include(:small)
+      end
+    end
+
+    context 'with an empty attachment' do
+      before { allow(attachment).to receive(:blank?).and_return(true) }
+      let(:formatted) { subject.format(attachment) }
+      it 'should not raise an error' do
+        expect { formatted }.not_to raise_error
+      end
+      it 'should return nil' do
+        expect(formatted).to be_nil
       end
     end
   end
