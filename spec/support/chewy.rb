e22af7864d14b4d188963a6aed93282f060a9248
Peter Lejeck <peter.lejeck@gmail.com>
2016-10-26 11:10:54 -0700

    Try something else to fix the builds

--- spec/support/chewy.rb
+++ spec/support/chewy.rb
@@ -8,5 +8,8 @@ RSpec.configure do |config|
   config.before(:example, elasticsearch: true) do
     Chewy.client.cluster.health wait_for_status: 'yellow', timeout: '10s'
     Chewy::Index.descendants.each(&:purge!)
+    Chewy.client.cluster.health wait_for_status: 'yellow', timeout: '10s'
+    # HACK: just fucking kill me
+    sleep 0.1
   end
 end
