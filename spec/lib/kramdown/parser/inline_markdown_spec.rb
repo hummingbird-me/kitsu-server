6537d425e5e1b2ee4e856b2946c4dca0a81b0fba
Peter Lejeck <peter.lejeck@gmail.com>
2016-10-27 17:07:19 -0700

    Allow paragraphs in InlineMarkdown

--- spec/lib/kramdown/parser/inline_markdown_spec.rb
+++ spec/lib/kramdown/parser/inline_markdown_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Kramdown::Parser::InlineMarkdown do
   it 'should not create headers' do
-    output = Kramdown::Document.new('# Header', input: 'InlineMarkdown').to_html
-    expect(output).not_to include('h1')
+    text = '# Header'
+    output = Kramdown::Document.new(text, input: 'InlineMarkdown').to_html
+    expect(output).not_to include('<h1')
+  end
+
+  it 'should create paragraphs' do
+    text = "two\n\nparagraphs"
+    output = Kramdown::Document.new(text, input: 'InlineMarkdown').to_html
+    paragraph_count = output.scan('<p').count
+    expect(paragraph_count).to eq(2)
   end
 end
