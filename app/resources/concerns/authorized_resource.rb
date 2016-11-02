eb138a42a107a2ebd7dbe737ec64706f5d2c99dc
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-02 15:54:06 -0700

    Test #show? on each record

--- app/resources/concerns/authorized_resource.rb
+++ app/resources/concerns/authorized_resource.rb
@@ -6,7 +6,18 @@ module AuthorizedResource
   class_methods do
     def find(filters, options = {})
       records = find_records(filters, options)
-      resources_for(records, options[:context])
+      resources = resources_for(records, options[:context])
+      resources.select { |resource| resource.send(:can, :show?) }
     end
+
+    private
+
+    def warn_if_show_defined; end
+  end
+
+  private
+
+  def show?(policy, record_id)
+    policy.scope.where(id: record_id).exists? && policy.show?
   end
 end
