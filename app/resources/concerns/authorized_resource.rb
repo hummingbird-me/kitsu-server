178d50794fbbb1eba32183dbb4a47085b284ec4d
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-02 13:43:53 -0700

    Remove errant print statement

--- app/resources/concerns/authorized_resource.rb
+++ app/resources/concerns/authorized_resource.rb
@@ -6,7 +6,6 @@ module AuthorizedResource
   class_methods do
     def find(filters, options = {})
       records = find_records(filters, options)
-      p records
       resources_for(records, options[:context])
     end
   end
