5c99167843972d56d90b149f8f74352e323445a6
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-03 00:48:41 -0700

    Allow using _none for nil queries in API

--- app/resources/base_resource.rb
+++ app/resources/base_resource.rb
@@ -4,4 +4,12 @@ class BaseResource < JSONAPI::Resource
   include AuthenticatedResource
   include AuthorizedResource
   include SearchableResource
+
+  def self.apply_filter(records, filter, value, options)
+    if value == '_none' || (value.is_a?(Array) && value[0] == '_none')
+      records.where(filter => nil)
+    else
+      super
+    end
+  end
 end
