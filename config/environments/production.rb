b22c7e23166db04dfd446bd6367b12f4be3b7c08
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-03 20:45:36 -0700

    Add existing S3 bucket to config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Use S3 buckets from V2
+  # TODO: make the URL not suck
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    },
+    s3_host_alias: 'static.hummingbird.me',
+    s3_protocol: :https,
+    url: ':s3_alias_url',
+    path: '/:class/:attachment/:id_partition/:style/:filename'
+  }
 end
