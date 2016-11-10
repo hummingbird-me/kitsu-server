73d61c92d50f0c86bfd763a269fd7d38930feddb
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-10 15:07:32 -0800

    Remove stray log statements (oops)

--- lib/html/pipeline/onebox_filter.rb
+++ lib/html/pipeline/onebox_filter.rb
@@ -2,12 +2,10 @@ module HTML
   class Pipeline
     class OneboxFilter < HTML::Pipeline::Filter
       def call
-        puts 'ONEBOXING', doc
         doc.search('a').each do |a|
           preview = Onebox.preview(a['href'], max_width: 500) rescue nil
           a.swap(preview.to_s) if preview&.to_s.present?
         end
-        puts 'ONEBOXED', doc
         doc
       end
     end
