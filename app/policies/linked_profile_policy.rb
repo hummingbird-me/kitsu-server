3d1dadf60e11f38ca8494419cb04126d03ac129c
toyhammered <daniel@rassiner.com>
2016-11-09 22:20:39 -0800

    Adds Pundit scope

--- app/policies/linked_profile_policy.rb
+++ app/policies/linked_profile_policy.rb
@@ -7,7 +7,19 @@ class LinkedProfilePolicy < ApplicationPolicy
 
   class Scope < Scope
     def resolve
+      return scope.where(private: true) unless user
+      t = LinkedProfile.arel_table
+      private, user_id = t[:private], t[:user_id]
 
+      # Don't apply privacy if user is admin
+      return scope if is_admin?
+      # RAILS-5: This can be replaced with a simple ActiveRecord.or
+      # (private == true && user == owner) || private == false
+      scope.where(
+        private.eq(false).or(
+          user_id.eq(user.id).and(private.eq(true))
+        )
+      )
     end
   end
 end
