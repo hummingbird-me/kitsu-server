8156d60560ba5d085404610fab02e7e463d19a2c
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-10 21:22:31 -0800

    Provide classnames for WhitelistedGeneric oneboxes

--- lib/html/pipeline/onebox_filter.rb
+++ lib/html/pipeline/onebox_filter.rb
@@ -3,9 +3,13 @@ module HTML
     class OneboxFilter < HTML::Pipeline::Filter
       def call
         doc.search('a').each do |a|
-          preview = Onebox.preview(a['href'], max_width: 500) rescue nil
+          url = a['href']
+          preview = Onebox.preview(url, max_width: 500) rescue nil
           if preview&.to_s.present?
             onebox_name = preview.send(:engine).class.onebox_name
+            if onebox_name == 'whitelistedgeneric'
+              onebox_name = URI.parse(url).host.split('.')[-2..-1].join('-')
+            end
             a.swap <<-EOF
               <div class="onebox onebox-#{onebox_name}">
                 #{preview}
