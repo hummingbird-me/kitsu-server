c8a520499257d1114add21f0ab17e898c2b5ea00
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-13 16:59:12 -0800

    Add code for counter cache resetting

---
+++ lib/counter_cache_resets.rb
@@ -0,0 +1,72 @@
+module CounterCacheResets
+  module_function
+
+  def posts
+    execute sql_for(Post, :post_likes)
+    execute sql_for(Post, :comments)
+  end
+
+  def sql_for(model, association_name)
+    association = model.reflections[association_name.to_s]
+    inverse = association.inverse_of
+    temp_table = "#{model}_#{association.name}_count"
+    [
+      <<-SQL.squish,
+        CREATE TEMP TABLE #{temp_table} AS
+        SELECT #{association.foreign_key}, count(*) AS count
+        FROM #{association.table_name}
+        GROUP BY #{association.foreign_key}
+      SQL
+      "CREATE INDEX ON #{temp_table} (post_id)",
+      "VACUUM #{temp_table}",
+      <<-SQL.squish,
+        UPDATE #{model.table_name}
+        SET #{inverse.counter_cache_column} = COALESCE((
+          SELECT count
+          FROM #{temp_table}
+          WHERE #{association.foreign_key} = #{model.table_name}.id
+        ), 0)
+      SQL
+      "DROP TABLE #{temp_table}"
+    ]
+  end
+
+  def execute(sql, title = 'Executing SQL')
+    if sql.respond_to?(:each)
+      say_with_time(title) do
+        sql.each do |query|
+          say("#{query}", true)
+          ActiveRecord::Base.connection.execute(query)
+        end
+      end
+    else
+      say_with_time(sql) do
+        ActiveRecord::Base.connection.execute(sql)
+      end
+    end
+  end
+
+  # Method pulled from ActiveRecord::Migration (under MIT, not Apache)
+  def say(message, subitem=false)
+    puts "#{subitem ? "   ->" : "--"} #{message}"
+  end
+
+  # Method pulled from ActiveRecord::Migration (under MIT, not Apache)
+  def say_with_time(message)
+    say(message)
+    result = nil
+    time = Benchmark.measure { result = yield }
+    say "%.4fs" % time.real, :subitem
+    say("#{result} rows", :subitem) if result.is_a?(Integer)
+    result
+  end
+
+  def progress_bar(title, count)
+    ProgressBar.create(
+      title: title,
+      total: count,
+      output: STDERR,
+      format: '%a (%p%%) |%B| %E %t'
+    )
+  end
+end
