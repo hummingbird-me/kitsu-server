ec7d70258346b14d83f1d4ee55a024607c31455f
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-14 15:05:37 -0800

    Allow filtering characters by name

--- app/resources/character_resource.rb
+++ app/resources/character_resource.rb
@@ -3,4 +3,21 @@ class CharacterResource < BaseResource
 
   has_one :primary_media, polymorphic: true
   has_many :castings
+
+  filter :slug
+
+  index CharactersIndex::Character
+  query :name,
+    mode: :query,
+    apply: -> (values, _ctx) {
+      {
+        multi_match: {
+          fields: %w[name people media],
+          query: values.join(' '),
+          fuzziness: 2,
+          max_expansions: 15,
+          prefix_length: 2
+        }
+      }
+    }
 end
