83ccd2290cec887c0edc4913d3e338cf39af6664
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-14 19:31:23 -0800

    Vastly improve character searching

--- app/resources/character_resource.rb
+++ app/resources/character_resource.rb
@@ -10,14 +10,6 @@ class CharacterResource < BaseResource
   query :name,
     mode: :query,
     apply: -> (values, _ctx) {
-      {
-        multi_match: {
-          fields: %w[name people media],
-          query: values.join(' '),
-          fuzziness: 2,
-          max_expansions: 15,
-          prefix_length: 2
-        }
-      }
+      CharactersIndex::Character.query_for(values.join(' '))
     }
 end
