f3d38ff6af701a91e785b30ee8f25dca9ec8fe88
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-21 22:11:54 -0800

    Add UserPolicy#{editable,visible}_attributes

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -10,4 +10,22 @@ class UserPolicy < ApplicationPolicy
   def destroy?
     is_admin?
   end
+
+  def editable_attributes(all)
+    if has_scope?(:email_password_reset, accept_all: false)
+      [:password]
+    elsif has_scope?(:email_confirm, accept_all: false)
+      [:confirmed_at, :unconfirmed_email, :email]
+    else
+      all
+    end
+  end
+
+  def visible_attributes(all)
+    if record == user
+      all
+    else
+      all - %i[email password confirmed_at unconfirmed_email]
+    end
+  end
 end
