4b53f423d2fc3d44f903545cc81c88e2528362e3
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-24 19:46:21 -0800

    Fix TRUSTED_PROXIES handling

--- config/initializers/trusted-proxies.rb
+++ config/initializers/trusted-proxies.rb
@@ -2,9 +2,9 @@ if Rails.env.production? || Rails.env.staging?
   cloudflare_ips = %w[
     https://www.cloudflare.com/ips-v4
     https://www.cloudflare.com/ips-v6
-  ].map { |url| (open(url) rescue nil)&.read&.split("\n") }.flatten.compact
+  ].map { |url| open(url).read.split("\n") }.flatten
 
-  env_ips = ENV['TRUSTED_PROXIES'].split(',')
+  env_ips = ENV['TRUSTED_PROXIES']&.split(',') || []
 
   trusted_ips = [cloudflare_ips, env_ips].flatten.map { |ip| IPAddr.new(ip) }
 
