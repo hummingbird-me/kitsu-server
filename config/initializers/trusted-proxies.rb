753401d1a642b5752e2598b67a5f4a2bfb7cba52
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-24 19:05:03 -0800

    Improve error handling in cloudflare_ips code

--- config/initializers/trusted-proxies.rb
+++ config/initializers/trusted-proxies.rb
@@ -2,7 +2,7 @@ if Rails.env.production? || Rails.env.staging?
   cloudflare_ips = %w[
     https://www.cloudflare.com/ips-v4
     https://www.cloudflare.com/ips-v6
-  ].map { |url| open(url).read.split("\n") }.flatten
+  ].map { |url| (open(url) rescue nil)&.read&.split("\n") }.flatten.compact
 
   env_ips = ENV['TRUSTED_PROXIES'].split(',')
 
