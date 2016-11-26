5af9a830c8db156d321573a4fbe99ec163437503
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-25 23:40:35 -0800

    Add initial attachment migrator code

---
+++ lib/migrate_attachments.rb
@@ -0,0 +1,45 @@
+class MigrateAttachments
+  BUCKET = 'kitsu-media'.freeze
+
+  SOURCE_PATH = ':class/:attachment/:id_partition/:style/:filename'.freeze
+  TARGET_PATH = ':class/:attachment/:id/:style.:content_type_extension'.freeze
+
+  attr_reader :client, :scope, :attachment
+
+  def initialize(scope, attachment)
+    @client = Aws::S3::Client.new
+    @scope = scope
+    @attachment = attachment
+  end
+
+  def run
+    each_item do |item|
+      move item.public_send(attachment)
+    end
+  end
+
+  private
+
+  def move(attachment)
+    old = Paperclip::Interpolations.interpolate(SOURCE, attachment, 'original')
+    new = Paperclip::Interpolations.interpolate(TARGET, attachment, 'original')
+
+    client.copy_object(bucket: BUCKET, copy_source: old, key: new)
+    client.delete_object(bucket: BUCKET)
+  end
+
+  def each_item(scope, title, &block)
+    items = scope.find_each.lazy
+    bar = progress_bar(title, scope.count(:all))
+    items.map(&block).map { |i| bar.increment; i }.reject(&:nil?)
+  end
+
+  def progress_bar(title, count)
+    ProgressBar.create(
+      title: title,
+      total: count,
+      output: STDERR,
+      format: '%a (%p%%) |%B| %E %t'
+    )
+  end
+end
