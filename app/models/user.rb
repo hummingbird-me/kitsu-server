66d99e096360a487b450b7c82e4d713dce2d4002
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-26 22:56:34 -0800

    Add styles to avatar attachment

--- app/models/user.rb
+++ app/models/user.rb
@@ -105,7 +105,17 @@ class User < ApplicationRecord
   has_many :review_likes, dependent: :destroy
   has_many :list_imports, dependent: :destroy
 
-  has_attached_file :avatar
+  has_attached_file :avatar, styles: {
+    tiny: '40x40#',
+    small: '64x64#',
+    medium: '100x100#',
+    large: '200x200#'
+  }, convert_options: {
+    tiny: '-quality 100 -strip',
+    small: '-quality 80 -strip',
+    medium: '-quality 70 -strip',
+    large: '-quality 60 -strip'
+  }
   has_attached_file :cover_image
 
   validates :email, presence: true,
