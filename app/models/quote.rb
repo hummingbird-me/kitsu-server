00902cad9d1a60debead7b2e8d5d0a461866b6fc
toyhammered <daniel@rassiner.com>
2016-11-27 18:49:56 -0800

    adds missing comma

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,7 +2,7 @@ class Quote < ActiveRecord::Base
   # defaults to required: true in Rails 5
   belongs_to :user, required: true
   belongs_to :anime, required: true
-  belongs_to :character required: true
+  belongs_to :character, required: true
 
   validates_presence_of :content
 end
