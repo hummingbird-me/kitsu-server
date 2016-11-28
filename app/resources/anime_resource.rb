2950a66ed56dbe036d92687026caa7a571e8e52c
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-27 19:49:06 -0800

    Pass nsfw field down to client

--- app/resources/anime_resource.rb
+++ app/resources/anime_resource.rb
@@ -1,7 +1,8 @@
 class AnimeResource < MediaResource
   include EpisodicResource
 
-  attributes :show_type, :youtube_video_id, :age_rating, :age_rating_guide
+  attributes :show_type, :youtube_video_id, :age_rating, :age_rating_guide,
+  attribute :nsfw, delegate: :nsfw?
   has_many :streaming_links
 
   # ElasticSearch hookup
@@ -10,4 +11,12 @@ class AnimeResource < MediaResource
   query :streamers, valid: -> (value, _ctx) { Streamer.find_by_name(value) }
   query :age_rating,
     valid: -> (value, _ctx) { Anime.age_ratings.keys.include?(value) }
+
+  def self.updatable_fields(context)
+    super - [:nsfw]
+  end
+
+  def self.creatable_fields(context)
+    super - [:nsfw]
+  end
 end
