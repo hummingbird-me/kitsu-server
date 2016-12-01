ee0e02cd3f61ecfd7093b1be8f410e71249718bb
Peter Lejeck <peter.lejeck@gmail.com>
2016-11-30 23:21:40 -0800

    Validate token before request

--- app/controllers/concerns/doorkeeper_helpers.rb
+++ app/controllers/concerns/doorkeeper_helpers.rb
@@ -1,6 +1,10 @@
 module DoorkeeperHelpers
   extend ActiveSupport::Concern
 
+  included do
+    before_action :validate_token!
+  end
+
   def current_user
     doorkeeper_token
   end
@@ -8,6 +12,14 @@ module DoorkeeperHelpers
   # Return boolean representing whether there is a user signed in
   def signed_in?
     current_user.present?
+  end
+
+  # Validate token
+  def validate_token!
+    # If we have a token, but it's not valid, explode
+    if doorkeeper_token && !doorkeeper_token.accessible?
+      jsonapi_render_errors json: [{ title: 'Invalid token' }], status: 403
+    end
   end
 
   # Provide context of current user to JR
