617a828ddee92a00e0f359f9311ebb26fb2d99b1
Peter Lejeck <peter.lejeck@gmail.com>
2016-12-01 16:54:14 -0800

    Pass errors from JR to Rollbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,6 @@ class ApplicationController < JSONAPI::ResourceController
   before_action :validate_token!
 
   force_ssl if Rails.env.production?
+
+  on_server_error { |error| Rollbar.error(error) }
 end
