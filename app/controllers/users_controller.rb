760ecf9cc32ef34ea20a671de0ed234a3fc69f18
Peter Lejeck <peter.lejeck@gmail.com>
2016-12-14 12:26:50 -0800

    Actually deliver the password reset email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
       render json: { errors: [{ title: 'User not found', status: '400' }] }, status: 400
       return
     end
-    UserMailer.password_reset(user)
+    UserMailer.password_reset(user).deliver_now
     render json: { username: query }
   end
 
