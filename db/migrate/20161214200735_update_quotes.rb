a497a41418fa1e84b1a7c91ed3e671e1fa8396ad
toyhammered <daniel@rassiner.com>
2016-12-14 12:55:06 -0800

    Add Quotes and Quote Likes

--- db/migrate/20161214200735_update_quotes.rb
+++ db/migrate/20161214200735_update_quotes.rb
@@ -2,25 +2,28 @@ class UpdateQuotes < ActiveRecord::Migration
   def change
     # Delete all entries with incomplete dataset
     Quote.where(anime_id: nil).destroy_all
-
     # Merge anime_id data over to media_id
     rename_column :quotes, :anime_id, :media_id
+    change_column_null :quotes, :media_id, false
+
+    # Content has to exist or else there is no quote
+    # Character can't say nothing
+    Quote.where(content: nil).destroy_all
+    change_column_null :quotes, :content, false
+
+    # Character needs to exist?
+    Quote.where(character_name: nil).destroy_all
+    change_column_null :quotes, :character_name, false
 
     # Add type for polymorphic relationship
-    add_column :quotes, :media_type, :string,
-    change_column_null :quotes, :media_type, false, default: 'Anime'
-
+    add_column :quotes, :media_type, :string
     # Update all existing entries
     Quote.update_all(media_type: 'Anime')
-
-    # Delete all entries with incomplete dataset
-    Quote.where(user_id: nil).destroy_all
-
-    # Makes sure no more invalid data can be added
-    change_column_null :quotes, :user_id, false
+    change_column_null :quotes, :media_type, false
 
     # Add reference to characters table
     add_reference :quotes, :character, foreign_key: true
+    change_column_null :quotes, :character_id, false
 
     # Rename our positive_votes to likes_count
     rename_column :quotes, :positive_votes, :likes_count
--- spec/policies/quote_policy_spec.rb
+++ spec/policies/quote_policy_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe QuotePolicy do
+  let(:other) { token_for build(:user) }
   let(:owner) { token_for build(:user) }
   let(:admin) { token_for create(:user, :admin) }
   let(:quote) { build(:quote, user: owner.resource_owner) }
@@ -13,7 +14,8 @@ RSpec.describe QuotePolicy do
 
   permissions :update?, :destroy? do
     it('should not allow anons') { should_not permit(nil, quote) }
-    it('should allow users') { should permit(owner, quote) }
+    it('should not allow others') { should_not permit(other, quote) }
+    it('should allow owner') { should permit(owner, quote) }
     it('should allow admins') { should permit(admin, quote) }
   end
 end
