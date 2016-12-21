52d0b63f1e5e23c680e48e0d9d53658bcc7e3c01
toyhammered <daniel@rassiner.com>
2016-12-20 20:44:14 -0800

    fix rubocop errors. WIP tests

--- spec/services/my_anime_list_sync_service_spec.rb
+++ spec/services/my_anime_list_sync_service_spec.rb
@@ -1,22 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe MyAnimeListSyncService do
-  let(:library_entry) { build(:library_entry,
-    status: 'current',
-    progress: 10
-  )}
-  let(:linked_profile) { build(:linked_profile,
-    url: 'myanimelist',
-    external_user_id: 'toyhammered',
-    token: 'fakefake'
-  )}
-  let(:mapping) { build(:mapping,
-    external_site: 'myanimelist',
-    external_id: 11_633
-  )}
+  let(:library_entry) do
+    build(:library_entry,
+      status: 'current',
+      progress: 10)
+  end
 
-  # personalize subject per create/update/destroy
-  subject { described_class.new(library_entry, 'update') }
+  let(:linked_profile) do
+    build(:linked_profile,
+      url: 'myanimelist',
+      external_user_id: 'toyhammered',
+      token: 'fakefake')
+  end
+
+  let(:mapping) do
+    build(:mapping,
+      external_site: 'myanimelist',
+      external_id: 11_633)
+  end
+
+  # delete or create/update
+  subject { described_class.new(library_entry, 'create/update') }
 
   before do
     host = described_class::ATARASHII_API_HOST
@@ -39,37 +44,39 @@ RSpec.describe MyAnimeListSyncService do
 
     # PUT request
     stub_request(:put, "#{host}animelist/anime/11633")
-      .with(:body => "episodes=10&rewatch_count=0&score&status=1",
-           :headers => {'Authorization'=>'Basic dG95aGFtbWVyZWQ6ZmFrZWZha2U='})
-      .to_return(status: 200, :body => "", :headers => {})
+      .with(
+        body: 'episodes=10&rewatch_count=0&score&status=1',
+        headers: {
+          Authorization: 'Basic dG95aGFtbWVyZWQ6ZmFrZWZha2U='
+        })
+      .to_return(status: 200)
 
     # POST request
-    stub_request(:post, "https://hbv3-mal-api.herokuapp.com/2.1/animelist/anime")
-      .with(:body => "anime_id=11633&episodes=10&score&status=1",
-           :headers => {'Authorization'=>'Basic dG95aGFtbWVyZWQ6ZmFrZWZha2U=' })
-      .to_return(:status => 200, :body => "", :headers => {})
+    stub_request(:post, "#{host}animelist/anime")
+      .with(
+        body: 'anime_id=11633&episodes=10&score&status=1',
+        headers: {
+          Authorization: 'Basic dG95aGFtbWVyZWQ6ZmFrZWZha2U='
+        })
+      .to_return(status: 200)
   end
 
   context 'Anime' do
-    describe '#execute_method' do
-
-    end
   end
 
   context 'Manga' do
-
   end
 
   context 'Tyhpoeus Requests' do
-    describe 'getting a url' do
+    describe '#get' do
       # it 'should issue a request to the server' do
-      #   stub_request(:get, 'example.com', linked_profile).to_return(body: 'HULLO')
+      #   stub_request(:get, 'example.com', linked_profile).to_return(body: 'HI')
       #   expect { |b|
-      #     subject.send(:get, 'example.com', linked_profile, &b)
+      #     subject.send(:get, 'example.com', linked_profile)
       #     subject.run
-      #   }.to yield_with_args('HULLO')
-      #   expect(WebMock).to have_requested(:get, 'example.com').once
-      # end
+      #   }.to yield_with_args('HI')
+        # expect(WebMock).to have_requested(:get, 'example.com', linked_profile).once
+      end
     end
 
     context 'which returns an error' do
@@ -83,5 +90,4 @@ RSpec.describe MyAnimeListSyncService do
       # end
     end
   end
-
 end
