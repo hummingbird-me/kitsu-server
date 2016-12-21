d219dd75df671f7d4d60e1e715c8572cae48a4ec
toyhammered <daniel@rassiner.com>
2016-12-20 21:19:11 -0800

    give access to user_id in kitsu_library

--- app/workers/my_anime_list_list_worker.rb
+++ app/workers/my_anime_list_list_worker.rb
@@ -2,14 +2,14 @@ class MyAnimeListListWorker
   include Sidekiq::Worker
 
   def perform(user_id)
-    kitsu_library.each do |row|
+    kitsu_library(user_id).each do |row|
       MyAnimeListSyncWorker.perform_async(row, 'create/update')
     end
   end
 
   private
 
-  def kitsu_library
+  def kitsu_library(user_id)
     # will get all anime/manga library entries for that user
     @kitsu_library ||= User.find(user_id).library_entries.all
   end
