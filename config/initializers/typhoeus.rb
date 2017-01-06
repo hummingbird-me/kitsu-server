c89b5e4fa18fc409f3f13dfc8543dae4f7e119cd
Peter Lejeck <peter.lejeck@gmail.com>
2017-01-05 23:02:07 -0800

    Disable Typhoeus logging, report requests to Sentry

---
+++ config/initializers/typhoeus.rb
@@ -0,0 +1,25 @@
+require 'typhoeus'
+require 'typhoeus/adapters/faraday'
+
+Ethon.logger = Logger.new(nil)
+
+module Typhoeus
+  class Request
+    def finish(response, bypass_memoization = nil)
+      Raven.breadcrumbs.record do |crumb|
+        crumb.category = 'requests'
+        crumb.type = 'http'
+        crumb.data = {
+          url: url,
+          method: options[:method],
+          status_code: response.code,
+          redirect_count: response.redirect_count,
+          total_time: response.total_time
+        }
+        crumb.level = :debug
+      end
+    ensure
+      super(response, bypass_memoization)
+    end
+  end
+end
