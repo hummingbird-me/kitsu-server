15aad816fd58035b56c6f41d8a09bede88d93117
Daniel Rassiner <daniel@rassiner.com>
2017-01-10 14:13:57 -0800

    fix MyAnimeListListWorker to pass in ids instead of objects (#37)

--- app/workers/my_anime_list_list_worker.rb
+++ app/workers/my_anime_list_list_worker.rb
@@ -2,15 +2,15 @@ class MyAnimeListListWorker
   include Sidekiq::Worker
 
   def perform(user_id)
-    kitsu_library(user_id).each do |row|
-      MyAnimeListSyncWorker.perform_async(row, 'create/update')
+    kitsu_library(user_id).each do |library_entry_ids|
+      MyAnimeListSyncWorker.perform_async(library_entry_ids, 'create/update')
     end
   end
 
   private
 
   def kitsu_library(user_id)
-    # will get all anime/manga library entries for that user
-    @kitsu_library ||= User.find(user_id).library_entries.all
+    # will get all anime/manga library entries ids for that user
+    @kitsu_library ||= User.find(user_id).library_entries.ids
   end
 end
