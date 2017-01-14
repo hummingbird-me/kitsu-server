9c018e4b850f1616cd2fb84db773b37842c28d9a
Peter Lejeck <peter.lejeck@gmail.com>
2017-01-14 15:17:40 -0800

    Remove space in lambdas in AnimeResource

--- app/resources/anime_resource.rb
+++ app/resources/anime_resource.rb
@@ -8,11 +8,11 @@ class AnimeResource < MediaResource
 
   # ElasticSearch hookup
   index MediaIndex::Anime
-  query :season, valid: -> (value, _ctx) { Anime::SEASONS.include?(value) }
+  query :season, valid: ->(value, _ctx) { Anime::SEASONS.include?(value) }
   query :season_year
-  query :streamers, valid: -> (value, _ctx) { Streamer.find_by_name(value) }
+  query :streamers, valid: ->(value, _ctx) { Streamer.find_by_name(value) }
   query :age_rating,
-    valid: -> (value, _ctx) { Anime.age_ratings.keys.include?(value) }
+    valid: ->(value, _ctx) { Anime.age_ratings.keys.include?(value) }
 
   def self.updatable_fields(context)
     super - [:nsfw]
