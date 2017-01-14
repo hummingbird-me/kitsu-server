b2a0c47515ff84881f00ca594c4e229faf7b9061
Peter Lejeck <peter.lejeck@gmail.com>
2017-01-13 18:45:14 -0800

    Add support for in-network trending

--- app/services/trending_service.rb
+++ app/services/trending_service.rb
@@ -1,19 +1,31 @@
 class TrendingService
   EPOCH = Date.new(2030, 1, 1).to_time.to_i
+  ITEM_LIMIT = 200
+  NETWORK_LIMIT = 20
+  TRIM_PROBABILITY = 0.1
 
-  attr_reader :namespace, :half_life, :limit
+  attr_reader :namespace, :half_life
 
-  def initialize(namespace, half_life: 7.days.to_i, limit: 200)
+  def initialize(namespace, half_life: 7.days.to_i, user: nil)
     @namespace = namespace
     @half_life = half_life
-    @limit = limit
+    @user = user
   end
 
   def vote(id, weight = 1.0)
     $redis.with do |conn|
-      conn.zincrby(key, change_for(weight))
+      key = trending_key
+      update_score(key, change, id)
+      trim(key, limit: ITEM_LIMIT) if rand < TRIM_PROBABILITY
     end
-    trim! if rand * 20 < 1
+  end
+
+  def fanout_vote
+    followers.each do |uid|
+      key = trending_key(uid)
+      update_score(key)
+      trim(key, limit: NETWORK_LIMIT) if rand < TRIM_PROBABILITY
+    end
   end
 
   def get(limit = 5)
@@ -24,8 +36,7 @@ class TrendingService
     results
   end
 
-  def trim!(key = nil)
-    key ||= trending_key
+  def trim(key, limit: 100)
     $redis.with do |conn|
       conn.zremrangebyrank(key, 0, -limit)
     end
@@ -33,13 +44,26 @@ class TrendingService
 
   private
 
-  def trending_key
+  def trending_key(user = nil)
+    user = user.id if user.respond_to?(:id)
     namespace = namespace.table_name if enrichable?
-    "trending:#{namespace}"
+    key = "trending:#{namespace}"
+    key += ":user:#{user}" if user
+    key
+  end
+
+  def update_score(key, weight = 1.0, id)
+    $redis.with do |conn|
+      conn.zincrby(key, change_for(weight), id)
+    end
   end
 
   def change_for(weight)
     weight * (2.0**(Time.now.to_i - EPOCH))
+  end
+
+  def followers
+    user.followers.pluck(:follower_id)
   end
 
   def enrich(list)
