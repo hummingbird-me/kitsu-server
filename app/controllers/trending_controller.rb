65e4aadd7556e5bbc4e739e2717b786bf63036d3
Peter Lejeck <peter.lejeck@gmail.com>
2017-01-13 21:26:11 -0800

    Fix trending controller

--- app/controllers/trending_controller.rb
+++ app/controllers/trending_controller.rb
@@ -1,6 +1,8 @@
 class TrendingController < ApplicationController
+  skip_after_action :enforce_policy_use
+
   def index
-    serializer = JSONAPI::ResourceSerializer.new(resource_klass)
+    serializer = JSONAPI::ResourceSerializer.new(resource_class)
     json = serializer.serialize_to_hash(resources)
     render json: json
   end
@@ -8,32 +10,38 @@ class TrendingController < ApplicationController
   private
 
   def resources
-    trending.map { |item| resource_class.new(item) }
+    trending.map { |item| resource_class.new(item, context) }
   end
 
   def user
-    doorkeeper_token.resource_owner
+    doorkeeper_token&.resource_owner
   end
 
   def trending_service
-    TrendingService.new(namespace, user: user)
+    TrendingService.new(namespace_class, user: user)
   end
 
   def trending
     if params[:in_network].present?
-      trending_service.get_network(params[:limit])
+      raise 'Must be logged in' unless user
+      trending_service.get_network(params[:limit] || 10)
     else
-      trending_service.get(params[:limit])
+      trending_service.get(params[:limit] || 10)
     end
   end
 
   def namespace
+    return @namespace if @namespace
     namespace = params[:namespace].classify
     raise 'Bad namespace' unless %w[Anime Manga Drama].include? namespace
+    @namespace = namespace
+  end
+
+  def namespace_class
     namespace.safe_constantize
   end
 
   def resource_class
-    "#{namespace.class.name}Resource".safe_constantize
+    "#{namespace}Resource".safe_constantize
   end
 end
