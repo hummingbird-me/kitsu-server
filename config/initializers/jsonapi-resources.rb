06c0d72eaf4839140c917200a1ad67b5f9bda7ce
Peter Lejeck <peter.lejeck@gmail.com>
2017-01-19 00:07:23 -0800

    Include count metadata in JR responses

--- config/initializers/jsonapi-resources.rb
+++ config/initializers/jsonapi-resources.rb
@@ -1,7 +1,16 @@
 JSONAPI.configure do |config|
+  # Keying
   config.json_key_format = :camelized_key
+
+  # Pagination
   config.default_paginator = :offset
   config.default_page_size = 10
   config.maximum_page_size = 20
+
+  # Caching
   config.resource_cache = Rails.cache
+
+  # Metadata
+  config.top_level_meta_include_record_count = true
+  config.top_level_meta_record_count_key = :count
 end
