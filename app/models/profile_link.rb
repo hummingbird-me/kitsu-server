a61f66a612a4a57e08765152b1e0c65da8fd23ee
toyhammered <daniel@rassiner.com>
2017-01-31 14:10:27 -0800

    fix setter to handle nil values

--- app/models/profile_link.rb
+++ app/models/profile_link.rb
@@ -28,7 +28,7 @@ class ProfileLink < ApplicationRecord
   validates :url, presence: true
 
   def url=(value)
-    super value.gsub(
+    super value&.gsub(
       profile_link_site.validate_find,
       profile_link_site.validate_replace
     )
