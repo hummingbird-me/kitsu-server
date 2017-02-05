d651720d5598c426ec9514995ee037fe27d30a50
Tony Xie <tx248@nyu.edu>
2017-02-05 13:47:31 -0800

    fixed url_for running twice

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,8 +1,11 @@
 module AdminHelper
   def url_for(options = nil)
-    split_url = super.split("/admin")
-    joined_url = x.join("/api/admin")
-    #for some reason it's necessary to split and rejoin, otherwise we either get /admin or /api/api/admin
+    puts super
+    return super if super.include?("/api")
+    split_url = super.split('/admin')
+    joined_url = split_url.join('/api/admin')
+    puts joined_url
+    # for some reason it's necessary to split and rejoin, otherwise we either get /admin or /api/api/admin
     joined_url
-  end
+    end
 end
