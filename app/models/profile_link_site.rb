d8063f3177b118ee6ddb0c223f180416b1d2e70b
Daniel Rassiner <daniel@rassiner.com>
2017-02-08 11:36:39 -0800

    Profile link validation fix (#59)
    * fix profile link validation to work properly
    
    * fix most profile_link_site tests but factorygirl linter is failing + associations
    
    * fix factorygirl lint issue
    
    * fix profile link to check for presence of url
    
    * rename profile url custom validation to url_formatted

--- app/models/profile_link_site.rb
+++ app/models/profile_link_site.rb
@@ -14,4 +14,8 @@
 
 class ProfileLinkSite < ApplicationRecord
   validates_presence_of :name, :validate_find, :validate_replace
+
+  def validate_find
+    Regexp.new(self[:validate_find]) unless self[:validate_find].nil?
+  end
 end
