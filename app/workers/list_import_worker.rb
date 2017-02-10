0f20b7f70004943b179f21163916c8bb4b7aa0a3
Peter Lejeck <peter.lejeck@gmail.com>
2017-02-10 15:19:25 -0800

    Use Sidekiq Middleware for Chewy instead

--- app/workers/list_import_worker.rb
+++ app/workers/list_import_worker.rb
@@ -2,9 +2,7 @@ class ListImportWorker
   include Sidekiq::Worker
 
   def perform(import_id)
-    Chewy.strategy(:atomic) do
-      import = ListImport.find(import_id)
-      import.apply!
-    end
+    import = ListImport.find(import_id)
+    import.apply!
   end
 end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,8 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
+  config.server_middleware do |chain|
+    chain.add Sidekiq::Middleware::Server::Chewy
+  end
 end
 
 Sidekiq.configure_client do |config|
