bae6031e1756cec8f03e7dcf60c994eae3da204d
Tony Xie <tx248@nyu.edu>
2017-02-13 15:11:25 -0800

    un-overrode the auth token

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,10 +5,6 @@ class AdminController < ActionController::Base
   helper AdminHelper
   protect_from_forgery with: :null_session
   before_action :set_paper_trail_whodunnit
-  def handle_unverified_request
-    # super # call the default behaviour, including Devise override
-    # authenticate_user!
-  end
 
   rescue_from ActionController::InvalidAuthenticityToken do
     render text: 'Token expired/invalid', status: 498
