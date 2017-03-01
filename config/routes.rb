9a9fac41380bed6a2e8e1ed38bc60bb4086d8b77
Peter Lejeck <peter.lejeck@gmail.com>
2017-03-01 11:30:45 -0800

    Organize config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,36 @@
+# rubocop:disable Metrics/BlockLength
+
 Rails.application.routes.draw do
   scope '/edge' do
-    jsonapi_resources :library_entries
-    jsonapi_resources :anime
-    jsonapi_resources :manga
-    jsonapi_resources :drama
+    ### Users
     jsonapi_resources :users
+    post '/users/_recover', to: 'users#recover'
+    # Profile data
+    jsonapi_resources :profile_links
+    jsonapi_resources :profile_link_sites
+    # Follows/Blocks/Memberships
     jsonapi_resources :follows do
       post :import_from_facebook, on: :collection
       post :import_from_twitter, on: :collection
     end
     jsonapi_resources :media_follows
-    jsonapi_resources :characters
-    jsonapi_resources :people
-    jsonapi_resources :castings
-    jsonapi_resources :genres
-    jsonapi_resources :streamers
-    jsonapi_resources :streaming_links
-    jsonapi_resources :franchises
-    jsonapi_resources :installments
-    jsonapi_resources :mappings
-    jsonapi_resources :posts
-    jsonapi_resources :comments
-    jsonapi_resources :post_likes
-    jsonapi_resources :comment_likes
     jsonapi_resources :blocks
-    jsonapi_resources :favorites
-    jsonapi_resources :episodes
-    jsonapi_resources :list_imports
-    jsonapi_resources :reviews
-    jsonapi_resources :review_likes
-    jsonapi_resources :roles
-    jsonapi_resources :user_roles
-    jsonapi_resources :reports
+    # Imports & Linked Accounts
     jsonapi_resources :linked_accounts
-    jsonapi_resources :profile_links
-    jsonapi_resources :profile_link_sites
-    jsonapi_resources :producers
-    jsonapi_resources :anime_productions
-    jsonapi_resources :media_relationships
+    jsonapi_resources :list_imports
+    # Permissions
+    jsonapi_resources :user_roles
+    jsonapi_resources :roles
+
+    ### Library
+    jsonapi_resources :library_entries
+    jsonapi_resources :favorites
+
+    ### Media
+    jsonapi_resources :anime
+    jsonapi_resources :manga
+    jsonapi_resources :drama
+    # Cast Info
     jsonapi_resources :anime_characters
     jsonapi_resources :anime_castings
     jsonapi_resources :anime_staff
@@ -46,29 +39,63 @@ Rails.application.routes.draw do
     jsonapi_resources :drama_staff
     jsonapi_resources :manga_characters
     jsonapi_resources :manga_staff
-    jsonapi_resources :groups
-    jsonapi_resources :group_members
-    jsonapi_resources :group_invites
-    jsonapi_resources :group_permissions
-    jsonapi_resources :group_reports
-    jsonapi_resources :group_neighbors
+    # Other Info
+    jsonapi_resources :mappings
+    jsonapi_resources :genres
+    jsonapi_resources :streaming_links
+    jsonapi_resources :streamers
+    jsonapi_resources :media_relationships
+    jsonapi_resources :anime_productions
+    jsonapi_resources :episodes
+    # DEPRECATED: Legacy systems
+    jsonapi_resources :castings
+    get '/anime/:anime_id/_languages', to: 'anime#languages'
+    jsonapi_resources :franchises
+    jsonapi_resources :installments
+    # Reviews
+    jsonapi_resources :reviews
+    jsonapi_resources :review_likes
+    # Trending
+    get '/trending/:namespace', to: 'trending#index'
 
+    ### People/Characters/Companies
+    jsonapi_resources :characters
+    jsonapi_resources :people
+    jsonapi_resources :producers
+
+    ### Feeds
+    jsonapi_resources :posts
+    jsonapi_resources :post_likes
+    jsonapi_resources :comments
+    jsonapi_resources :comment_likes
+    jsonapi_resources :reports
     resources :activities, only: %i[destroy]
     get '/feeds/:group/:id', to: 'feeds#show'
     post '/feeds/:group/:id/_read', to: 'feeds#mark_read'
     post '/feeds/:group/:id/_seen', to: 'feeds#mark_seen'
+    delete '/feeds/:group/:id/activities/:uuid', to: 'feeds#destroy_activity'
+
+    ### Groups
+    jsonapi_resources :groups
+    jsonapi_resources :group_members
+    jsonapi_resources :group_permissions
+    jsonapi_resources :group_reports
+    jsonapi_resources :group_neighbors
+    # Invites
+    jsonapi_resources :group_invites
     post '/group-invites/:id/_accept', to: 'group_invites#accept'
     post '/group-invites/:id/_decline', to: 'group_invites#decline'
-    delete '/feeds/:group/:id/activities/:uuid', to: 'feeds#destroy_activity'
-    get '/trending/:namespace', to: 'trending#index'
-    post '/users/_recover', to: 'users#recover'
-    get '/anime/:anime_id/_languages', to: 'anime#languages'
   end
 
+  ### Debug APIs
   get '/debug/dump_all', to: 'debug#dump_all'
   post '/debug/trace_on', to: 'debug#trace_on'
   get '/debug/gc_info', to: 'debug#gc_info'
+
+  ### Staging Sync
   post '/user/_prodsync', to: 'users#prod_sync'
+
+  ### Authentication
   use_doorkeeper
 
   root to: 'home#index'
