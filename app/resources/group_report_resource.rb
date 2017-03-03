1195321885b29faacd3961131f956e849672a836
Thomas McNiven <hello@vevix.net>
2017-03-04 03:43:33 +1000

    Allow GroupReportResource to be sortable by created_at & filter by group

--- app/resources/group_report_resource.rb
+++ app/resources/group_report_resource.rb
@@ -8,10 +8,14 @@ class GroupReportResource < BaseResource
   has_one :user
   has_one :moderator
 
-  filters :user_id, :naughty_id, :naughty_type, :status, :reason
+  filters :group, :user, :naughty, :naughty_type, :status, :reason
 
   log_verb do
     status if action == :update
   end
   log_target []
+
+  def self.sortable_fields(context)
+    super(context) << :created_at
+  end
 end
