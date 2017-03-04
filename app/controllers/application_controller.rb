ee578ac5e1f34064e49a4a867ae0b249785d5456
Peter Lejeck <peter.lejeck@gmail.com>
2017-03-04 11:53:13 -0800

    Disable Sentry hooks when !capture_allowed?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,13 +11,17 @@ class ApplicationController < JSONAPI::ResourceController
 
   force_ssl if Rails.env.production?
 
-  on_server_error { |error| Raven.capture_exception(error) }
+  if Raven.configuration.capture_allowed?
+    on_server_error do |error|
+      Raven.capture_exception(error)
+    end
 
-  before_action :tag_sentry_context
+    before_action :tag_sentry_context
 
-  def tag_sentry_context
-    user = current_user&.resource_owner
-    Raven.user_context(id: user.id, name: user.name) if user
-    Raven.extra_context(url: request.url)
+    def tag_sentry_context
+      user = current_user&.resource_owner
+      Raven.user_context(id: user.id, name: user.name) if user
+      Raven.extra_context(url: request.url)
+    end
   end
 end
