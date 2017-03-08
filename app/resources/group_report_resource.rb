2a042e3460fd90d2eedd33560c1b22a96602a26e
Thomas McNiven <hello@vevix.net>
2017-03-09 07:47:40 +1000

    add created_at to GroupReportResource

--- app/resources/group_report_resource.rb
+++ app/resources/group_report_resource.rb
@@ -1,7 +1,7 @@
 class GroupReportResource < BaseResource
   include GroupActionLogger
 
-  attributes :reason, :status, :explanation
+  attributes :reason, :status, :explanation, :created_at
 
   has_one :group
   has_one :naughty, polymorphic: true
