95c6594b52df774bbe338f3300cb5690de3d3168
Thomas McNiven <hello@vevix.net>
2017-03-09 09:40:14 +1000

    Drop content_formatted from GroupTicketMessageResource

--- app/resources/group_ticket_message_resource.rb
+++ app/resources/group_ticket_message_resource.rb
@@ -1,5 +1,5 @@
 class GroupTicketMessageResource < BaseResource
-  attributes :kind, :content, :content_formatted, :created_at
+  attributes :kind, :content, :created_at
 
   has_one :ticket
   has_one :user
