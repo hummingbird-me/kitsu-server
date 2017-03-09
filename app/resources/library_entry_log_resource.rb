07c0f44a72b7a5c2aee7b55bd47676b1c26caedd
toyhammered <daniel@rassiner.com>
2017-03-09 14:40:34 -0800

    add filters for linked_account_id to logs

--- app/resources/library_entry_log_resource.rb
+++ app/resources/library_entry_log_resource.rb
@@ -4,4 +4,6 @@ class LibraryEntryLogResource < BaseResource
     :reconsuming, :status, :volumes_owned, :action_performed, :sync_status
 
   has_one :linked_account
+
+  filters :linked_account_id
 end
