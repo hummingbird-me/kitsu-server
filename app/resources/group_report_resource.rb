6481a340377c884404c0b5060ff5a8097efcf90e
Peter Lejeck <peter.lejeck@gmail.com>
2017-03-09 14:37:53 -0800

    Fix filtering GroupReport by status

--- app/resources/group_report_resource.rb
+++ app/resources/group_report_resource.rb
@@ -1,14 +1,18 @@
 class GroupReportResource < BaseResource
   include GroupActionLogger
 
-  attributes :reason, :status, :explanation, :created_at, :naughty_type, :naughty_id
+  attributes :reason, :status, :explanation, :created_at, :naughty_type,
+    :naughty_id
 
   has_one :group
   has_one :naughty, polymorphic: true
   has_one :user
   has_one :moderator
 
-  filters :group, :user, :naughty, :naughty_type, :status, :reason
+  filters :group, :user, :naughty, :naughty_type, :reason
+  filter :status, verify: ->(values, _) {
+    values.map { |v| GroupReport.statuses[v] || v }
+  }
 
   log_verb do |action|
     status if action == :update
