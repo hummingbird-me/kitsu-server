7a50e3186a46b610f64a7f8499a28fbfbef34240
Peter Lejeck <peter.lejeck@gmail.com>
2017-03-09 15:58:51 -0800

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308231654) do
+ActiveRecord::Schema.define(version: 20170309124949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -493,7 +493,6 @@ ActiveRecord::Schema.define(version: 20170308231654) do
     t.integer  "status",      default: 0, null: false
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
-    t.string   "title",                   null: false
   end
 
   add_index "group_tickets", ["assignee_id"], name: "index_group_tickets_on_assignee_id", using: :btree
