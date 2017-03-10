afa025af5a36dbed6320aa036fb0ddc45413e819
Peter Lejeck <peter.lejeck@gmail.com>
2017-03-10 11:59:27 -0800

    Don't explode when a ticket isn't assigned

--- app/chewy/group_tickets_index.rb
+++ app/chewy/group_tickets_index.rb
@@ -2,7 +2,7 @@ class GroupTicketsIndex < Chewy::Index
   define_type GroupTicket do
     field :group_id
     field :user, value: ->(ticket) { ticket.user.name }
-    field :assignee, value: ->(ticket) { ticket.assignee.name }
+    field :assignee, value: ->(ticket) { ticket.assignee&.name }
     field :messages, value: ->(ticket) { ticket.messages.map(&:content) }
     field :status
   end
