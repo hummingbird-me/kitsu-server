9705f22145fe5d2b51576df218ef6441b7d8eae9
Peter Lejeck <peter.lejeck@gmail.com>
2017-03-11 18:32:02 -0800

    Create a follow in the GroupInvite factory

--- spec/factories/group_invites.rb
+++ spec/factories/group_invites.rb
@@ -29,6 +29,9 @@
 
 FactoryGirl.define do
   factory :group_invite do
+    before(:create) do |_invite, evaluator|
+      Follow.create!(follower: evaluator.user, followed: evaluator.sender)
+    end
     group
     user
     association :sender, factory: :user
