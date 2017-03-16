ec8f668e7cee47d5efded94c65dd0bf9427b52de
Peter Lejeck <peter.lejeck@gmail.com>
2017-03-16 13:51:23 -0700

    Improve error handling in session copying

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,6 @@
 class AdminController < ActionController::Base
   before_action :redirect_to_session
   include Pundit
-  include Doorkeeper
-  include DoorkeeperHelpers
   helper AdminHelper
   protect_from_forgery with: :null_session
 
@@ -15,7 +13,7 @@ class AdminController < ActionController::Base
   end
 
   def current_user
-    User.find(pundit_user[:resource_owner_id])
+    User.find(pundit_user[:resource_owner_id]) if pundit_user
   end
 
   def redirect_to_session
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ActionController::Base
   def new; end
 
   def create
+    return render status: 403, text: 'Token missing' if params[:token].blank?
     session[:token] = params[:token]
-    head :ok
+    render status: 200, text: 'Success!'
   end
 end
