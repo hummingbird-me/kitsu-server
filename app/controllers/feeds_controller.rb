3d4f7b568357c046d81f6cec51300e6fee41d0a3
Peter Lejeck <peter.lejeck@gmail.com>
2017-03-18 15:44:15 -0700

    Handle nil current_user in feed_visible?

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -74,7 +74,7 @@ class FeedsController < ApplicationController
       group && show?(group)
     when 'notifications', 'timeline'
       user = User.find_by(id: params[:id])
-      user == current_user.resource_owner
+      user == current_user&.resource_owner
     when 'global' then true
     when 'reports_aggr'
       user = current_user&.resource_owner
