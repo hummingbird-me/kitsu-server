d08cd0edc6e654123a1dae427b76f7dbbb7c9ab4
matthewdias <matthewdias@me.com>
2017-04-06 13:05:38 -0500

    handle enum

--- app/resources/media_relationship_resource.rb
+++ app/resources/media_relationship_resource.rb
@@ -1,7 +1,11 @@
 class MediaRelationshipResource < BaseResource
   attribute :role
 
-  filter :role
+  filter :role, apply: ->(records, values, _options) {
+    roles = MediaRelationship.roles.values_at(*values).compact
+    roles = values if roles.empty?
+    records.where(role: roles)
+  }
 
   has_one :source, polymorphic: true
   has_one :destination, polymorphic: true
