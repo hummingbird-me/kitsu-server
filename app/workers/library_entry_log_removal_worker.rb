350c8b675f7fc7fcde5a2aafc3d1ecb3655816e7
toyhammered <daniel@rassiner.com>
2017-04-06 20:30:24 -0700

    change sidetiq to use sidekiq scheduler

--- app/workers/library_entry_log_removal_worker.rb
+++ app/workers/library_entry_log_removal_worker.rb
@@ -1,8 +1,5 @@
 class LibraryEntryLogRemovalWorker
   include Sidekiq::Worker
-  include Sidetiq::Schedulable
-
-  recurrence { daily }
 
   def perform
     LibraryEntryLog.where('created_at <= ?', 1.week.ago).destroy_all
