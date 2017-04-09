ab3f9d5ea473a6406897d6ae0cfea4f28c3bf44e
Peter Lejeck <peter.lejeck@gmail.com>
2017-04-08 22:58:33 -0700

    Add image dimensions validation (#93)

--- app/models/concerns/with_cover_image.rb
+++ app/models/concerns/with_cover_image.rb
@@ -14,6 +14,8 @@ module WithCoverImage
     validates_attachment :cover_image, content_type: {
       content_type: %w[image/jpg image/jpeg image/png]
     }
+    # Accept slightly vertical (1:4) to absurdly wide (20:1)
+    validates :cover_image, image_dimensions: { aspect_ratio: 0.25..20 }
     process_in_background :cover_image,
       only_process: %i[tiny large],
       processing_image_url: ->(cover) {
---
+++ app/validators/image_dimensions_validator.rb
@@ -0,0 +1,33 @@
+class ImageDimensionsValidator < ActiveModel::EachValidator
+  def validate_each(record, attr, value)
+    return if value.blank?
+
+    actual = dimensions_for(value)
+
+    height = options[:height]
+    width = options[:width]
+    aspect = options[:aspect_ratio]
+
+    record.errors[attr] << validate_value('height', actual.height, height)
+    record.errors[attr] << validate_value('width', actual.width, width)
+    record.errors[attr] << validate_value('aspect ratio', actual.aspect, aspect)
+
+    record.errors[attr].compact!
+  end
+
+  def dimensions_for(attachment)
+    file = attachment.queued_for_write[:original].path
+    Paperclip::Geometry.from_file(file)
+  end
+
+  def validate_value(name, value, target)
+    return unless target
+    return if target === value # rubocop:disable Style/CaseEquality
+
+    if target.respond_to?(:begin) && target.respond_to?(:end)
+      "#{name} must be between #{target.begin} and #{target.end}"
+    else
+      "#{name} must be #{target}"
+    end
+  end
+end
