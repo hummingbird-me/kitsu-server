165cf203aadc36817600b29faee5255c2b8420e8
Daniel Rassiner <daniel@rassiner.com>
2017-04-10 22:17:45 +0200

    Add created_at attribute to logs resource (#101)

--- app/resources/library_entry_log_resource.rb
+++ app/resources/library_entry_log_resource.rb
@@ -4,7 +4,7 @@ class LibraryEntryLogResource < BaseResource
   immutable
   paginator :unlimited
   attributes :progress, :rating, :reconsume_count, :reconsuming,
-    :status, :volumes_owned, :action_performed, :sync_status
+    :status, :volumes_owned, :action_performed, :sync_status, :created_at
 
   has_one :linked_account
   has_one :media, polymorphic: true
