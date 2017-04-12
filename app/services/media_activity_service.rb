zhaomingbo/hummingbird-server

Refactor feed filtering to use split feeds
* Use split feeds to filter activity
* Creating users and media sets the correct follows between feeds
* User activity gets created into the corresponding filtered feed
* Updated Stream Sync Rake tasks to point (and repoint) feeds correctly
* BONUS: Fix code cleaning up unenriched keys (was ignoring symbols)
* BONUS: Check resource permissions based on user ID and not object identity
* BONUS: Set `activity_copy_limit` parameter to 300 as 1000 is not supported for batch follows (Stream API bug)

app/services/media_activity_service.rb
@@ -8,7 +8,7 @@ class MediaActivityService
   end
 
   def status(status)
-    fill_defaults user.feed.activities.new(
+    fill_defaults user.media_feed.activities.new(
       foreign_id: "LibraryEntry:#{library_entry.id}:updated-#{status}",
       verb: 'updated',
       status: status
@@ -16,7 +16,7 @@ class MediaActivityService
   end
 
   def rating(rating)
-    fill_defaults user.feed.activities.new(
+    fill_defaults user.media_feed.activities.new(
       foreign_id: "LibraryEntry:#{library_entry.id}:rated",
       verb: 'rated',
       rating: rating,
@@ -26,7 +26,7 @@ class MediaActivityService
 
   def progress(progress, unit = nil)
     return if progress == 0
-    fill_defaults user.feed.activities.new(
+    fill_defaults user.media_feed.activities.new(
       verb: 'progressed',
       foreign_id: "LibraryEntry:#{library_entry.id}:progressed-#{progress}",
       progress: progress,
@@ -35,7 +35,7 @@ class MediaActivityService
   end
 
   def reviewed(review)
-    fill_defaults user.feed.activities.new(
+    fill_defaults user.media_feed.activities.new(
       foreign_id: review,
       verb: 'reviewed',
       review: review
@@ -47,7 +47,7 @@ class MediaActivityService
       activity.actor ||= user
       activity.object ||= library_entry
       activity.to ||= []
-      activity.to << media&.feed
+      activity.to << media&.media_feed
       activity.media ||= media
       activity.nsfw ||= media.try(:nsfw?)
       activity.foreign_id ||= library_entry

hummingbird-me/kitsu-server

app/services/media_activity_service.rb
@@ -8,7 +8,7 @@ class MediaActivityService
   end
 
   def status(status)
-    fill_defaults user.feed.activities.new(
+    fill_defaults user.media_feed.activities.new(
       foreign_id: "LibraryEntry:#{library_entry.id}:updated-#{status}",
       verb: 'updated',
       status: status
@@ -16,7 +16,7 @@ class MediaActivityService
   end
 
   def rating(rating)
-    fill_defaults user.feed.activities.new(
+    fill_defaults user.media_feed.activities.new(
       foreign_id: "LibraryEntry:#{library_entry.id}:rated",
       verb: 'rated',
       rating: rating,
@@ -27,7 +27,7 @@ class MediaActivityService
 
   def progress(progress, unit = nil)
     return if progress.zero?
-    fill_defaults user.feed.activities.new(
+    fill_defaults user.media_feed.activities.new(
       verb: 'progressed',
       foreign_id: "LibraryEntry:#{library_entry.id}:progressed-#{progress}",
       progress: progress,
@@ -36,7 +36,7 @@ class MediaActivityService
   end
 
   def reviewed(review)
-    fill_defaults user.feed.activities.new(
+    fill_defaults user.media_feed.activities.new(
       foreign_id: review,
       verb: 'reviewed',
       review: review
@@ -48,7 +48,7 @@ class MediaActivityService
       act.actor ||= user
       act.object ||= library_entry
       act.to ||= []
-      act.to << media&.feed
+      act.to << media&.media_feed
       act.media ||= media
       act.nsfw ||= media.try(:nsfw?)
       act.foreign_id ||= library_entry