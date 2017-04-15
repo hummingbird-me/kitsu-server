28ba1ab36e6ca21a63c15ab548a0751688889a13
Peter Lejeck <peter.lejeck@gmail.com>
2017-04-15 14:01:56 -0700

    Make split feeds optional on feed request

--- app/services/feed_query_service.rb
+++ app/services/feed_query_service.rb
@@ -2,13 +2,6 @@ class FeedQueryService
   MEDIA_VERBS = %w[updated rated progressed].freeze
   POST_VERBS = %w[post comment follow review].freeze
 
-  KIND_TO_FEED = {
-    media_aggr: { posts: 'media_posts_aggr', media: 'media_media_aggr' },
-    user_aggr: { posts: 'user_posts_aggr', media: 'user_media_aggr' },
-    timeline: { posts: 'timeline_posts', media: 'timeline_media' },
-    global: { posts: 'global_posts', media: 'global_media' }
-  }.freeze
-
   attr_reader :params, :user
 
   def initialize(params, user)
@@ -18,27 +11,41 @@ class FeedQueryService
 
   def list
     return @list if @list
-    list = feed.activities
+    list = if split_feeds?
+             feed.activities
+           else
+             feed.activities(filter: kind_filter)
+           end
     list = list.page(id_lt: cursor) if cursor
     list = list.per(limit) if limit
     list = list.where_id(*id_query) if id_query
     list = list.mark(mark) if mark
     list = list.sfw if sfw_filter?
     list = list.blocking(blocked)
+    if kind_select && !split_feeds?
+      list = list.select(kind_select[:ratio], &kind_select[:proc])
+    end
     @list = list
   end
 
   def feed
-    return @feed if @feed
-    kind = params.dig(:filter, :kind).try(:to_sym)
-    group = params[:group].try(:to_sym)
-    feed_name = KIND_TO_FEED.dig(group, kind) || group
-    @feed = Feed.new(feed_name, params[:id])
+    if split_feeds?
+      return @feed if @feed
+      feed_name = params[:group].split('_').first
+      @feed = Feed.class_for(feed_name).new(params[:id])
+    else
+      # Just use the basic SreamFeed class for the old system
+      @feed ||= Feed::StreamFeed.new(params[:group], params[:id])
+    end
   end
 
   private
 
   delegate :sfw_filter?, to: :user, allow_nil: true
+
+  def split_feeds?
+    params.key?(:_split_feeds)
+  end
 
   def cursor
     params.dig(:page, :cursor)
@@ -56,6 +63,38 @@ class FeedQueryService
     return unless params.dig(:filter, :id).is_a? Hash
     operator, id = params.dig(:filter, :id).to_a.flatten
     [operator.to_sym, id]
+  end
+
+  def kind_filter
+    params.dig(:filter, :kind)
+  end
+
+  def kind_select
+    @kind_filter ||=
+      case kind_filter
+      when 'media'
+        {
+          ratio: 0.8,
+          proc: ->(act) do
+            if MEDIA_VERBS.include?(act['verb'])
+              true
+            else
+              throw :remove_group
+            end
+          end
+        }
+      when 'posts'
+        {
+          ratio: 0.2,
+          proc: ->(act) do
+            if POST_VERBS.include?(act['verb'])
+              true
+            else
+              throw :remove_group
+            end
+          end
+        }
+      end
   end
 
   def blocked
