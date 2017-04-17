e838292953da756acc82d058a52cbe5b90688f9c
Peter Lejeck <peter.lejeck@gmail.com>
2017-04-17 13:10:29 -0700

    Enrich RecommendationsService::Media#recommendations_for(klass)

--- app/services/recommendations_service/media.rb
+++ app/services/recommendations_service/media.rb
@@ -10,15 +10,23 @@ class RecommendationsService
     end
 
     def poll
-      client.get "poll/#{user.id}/"
+      client.get("poll/#{user.id}/")
     end
 
     def recommendations_for(klass)
-      client.get "recommendations/#{user.id}/"
+      res = client.get("recommendations/#{user.id}/")
+      reccs = res.body.dig('results', 'recommendations', 'media', klass.name)
+      reccs = reccs.reduce(&:merge)
+      media_ids = reccs.keys.map { |key| key.split(':').last.to_i }
+      media = klass.find(media_ids).index_by { |m| "#{m.class.name}:#{m.id}" }
+      reccs = reccs.transform_keys { |key| media[key] }
+      reccs = reccs.transform_values { |val| val['score'] }
+      reccs = reccs.sort_by(&:last).reverse
+      reccs.map(&:first)
     end
 
     def strength
-      client.get "profile_strength/#{user.id}/"
+      client.get("profile_strength/#{user.id}/")
     end
   end
 end
