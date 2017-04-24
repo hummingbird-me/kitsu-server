5e72d41d3752e32cb44fe8a9eb8a310462068454
Peter Lejeck <peter.lejeck@gmail.com>
2017-04-24 14:14:49 -0700

    Refactor StreamDump#split_feed stuff

--- lib/stream_dump.rb
+++ lib/stream_dump.rb
@@ -5,39 +5,59 @@ end
 module StreamDump
   module_function
 
-  def split_posts(scope = User)
+  def split_profiles(scope = User)
     results = each_user(scope) do |user_id|
-      activities = Feed::ProfileFeed.new(user_id).activities_for(type: :flat)
-                                    .unenriched.to_enum
-      posts_activities = []
-      media_activities = []
-      profile_feed = Feed::ProfileFeed.new(user_id)
-      posts_feed = profile_feed.stream_feed_for(filter: :posts).stream_id
-      media_feed = profile_feed.stream_feed_for(filter: :media).stream_id
-
-      activities.each do |act|
-        if Feed::MEDIA_VERBS.include?(act.verb)
-          media_activities << act.activities.first
-        elsif Feed::POST_VERBS.include?(act.verb)
-          posts_activities << act.activities.first
-        end
-      end
-
-      [
-        {
-          instruction: 'add_activities',
-          feedId: media_feed,
-          data: media_activities.map(&:as_json)
-        },
-        {
-          instruction: 'add_activities',
-          feedId: posts_feed,
-          data: posts_activities.map(&:as_json)
-        }
-      ]
+      # Split the feed
+      split_feed(Feed::ProfileFeed.new(user_id))
     end
     # Flatten the results lazily
     results.flat_map { |x| x }
+  end
+
+  def split_media
+    results = each_media do |media_type, media_id|
+      split_feed(Feed::MediaFeed.new(media_type, media_id))
+    end
+    # Flatten the results lazily
+    results.flat_map { |x| x }
+  end
+
+  def split_timelines(scope = User)
+    results = each_user(scope) do |user_id|
+      split_feed(Feed::Timeline.new(user_id))
+    end
+    # Flatten the results lazily
+    results.flat_map { |x| x }
+  end
+
+  def split_feed(feed)
+    activities = feed.activities_for(type: :flat).unenriched.to_enum
+
+    posts_activities = []
+    media_activities = []
+    posts_feed = feed.stream_feed_for(filter: :posts).stream_id
+    media_feed = feed.stream_feed_for(filter: :media).stream_id
+
+    activities.each do |act|
+      if Feed::MEDIA_VERBS.include?(act.verb)
+        media_activities << act.activities.first
+      elsif Feed::POST_VERBS.include?(act.verb)
+        posts_activities << act.activities.first
+      end
+    end
+
+    [
+      {
+        instruction: 'add_activities',
+        feedId: media_feed,
+        data: media_activities.map(&:as_json)
+      },
+      {
+        instruction: 'add_activities',
+        feedId: posts_feed,
+        data: posts_activities.map(&:as_json)
+      }
+    ]
   end
 
   def posts(scope = User)
