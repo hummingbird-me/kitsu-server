c7948f79fa79d021c7c05fdffe40ca9b00426777
Peter Lejeck <peter.lejeck@gmail.com>
2017-05-01 23:09:21 -0700

    Send SiteAnnouncements to their feed

--- app/models/site_announcement.rb
+++ app/models/site_announcement.rb
@@ -1,5 +1,11 @@
 class SiteAnnouncement < ApplicationRecord
+  include WithActivity
+
   belongs_to :user, required: true
 
   validates :text, presence: true
+
+  def stream_activity
+    Feed::SiteAnnouncementsFeed.global.activities.new
+  end
 end
