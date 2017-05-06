ca03462cab6c91d93588e6db72c3a01da0ce5b6a
Peter Lejeck <peter.lejeck@gmail.com>
2017-05-06 12:07:38 -0700

    Convince Global feed that it's flat

--- app/models/feed/global.rb
+++ app/models/feed/global.rb
@@ -6,7 +6,7 @@ class Feed
       super('global')
     end
 
-    def stream_feed_for(filter: nil, type: :aggregated)
+    def stream_feed_for(filter: nil, type: :flat)
       super(filter: filter, type: type)
     end
   end
