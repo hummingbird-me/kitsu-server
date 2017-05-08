7ef5211a9f29dce7616a8982f5b740084ae8421b
Peter Lejeck <peter.lejeck@gmail.com>
2017-05-07 22:32:48 -0700

    Apply Group#visible_for to GroupMember

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -52,6 +52,7 @@ class GroupMember < ApplicationRecord
   scope :followed_first, ->(u) { joins(:user).merge(User.followed_first(u)) }
   scope :leaders, -> { where.not(rank: 'pleb') }
   scope :blocking, ->(users) { where.not(user_id: users) }
+  scope :visible_for, ->(u) { joins(:group).merge(Group.visible_for(u)) }
 
   def has_permission?(perm)
     permissions.for_permission(perm).exists?
