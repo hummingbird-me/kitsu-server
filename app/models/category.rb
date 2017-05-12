da7b47645940230ae195bd78894d2fd4bdfc44cd
cinquemb <cinque_mcfarlane-blake@brown.edu>
2017-05-12 08:57:48 -0400

    addressing houndbot

--- app/models/category.rb
+++ app/models/category.rb
@@ -35,7 +35,6 @@ class Category < ActiveRecord::Base
   has_and_belongs_to_many :anime
   has_and_belongs_to_many :manga
   has_and_belongs_to_many :drama
-  
   validates_attachment :image, content_type: {
     content_type: %w[image/jpg image/jpeg image/png]
   }
