f19b4d401a8aab1f65ced44cfe10cc780dd4ede6
cinquemb <cinque_mcfarlane-blake@brown.edu>
2017-05-12 09:14:06 -0400

    removing s from CategoryFavorite policy

--- app/policies/category_favorite_policy.rb
+++ app/policies/category_favorite_policy.rb
@@ -1,4 +1,4 @@
-class CategoryFavoritesPolicy < ApplicationPolicy
+class CategoryFavoritePolicy < ApplicationPolicy
   alias_method :update?, :is_owner?
   alias_method :create?, :is_owner?
   alias_method :destroy?, :is_owner?
