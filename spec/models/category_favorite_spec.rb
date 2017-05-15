bba780caebcb346fcdf1d0ed1949053c7037c869
cinquemb <cinque_mcfarlane-blake@brown.edu>
2017-05-15 07:21:17 -0400

    adding model specs for category and category follow

--- spec/models/category_favorite_spec.rb
+++ spec/models/category_favorite_spec.rb
@@ -24,5 +24,8 @@
 require 'rails_helper'
 
 RSpec.describe CategoryFavorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:category_favorite) }
+
+  it { should belong_to(:user) }
+  it { should belong_to(:category) }
 end
