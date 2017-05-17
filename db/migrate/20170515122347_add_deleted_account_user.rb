6cc764bcece9820bd3da03a150b79d361e23151e
Thomas McNiven <hello@vevix.net>
2017-05-17 21:28:39 +1000

    Add deleted_at column to users

--- db/migrate/20170515122347_add_deleted_account_user.rb
+++ db/migrate/20170515122347_add_deleted_account_user.rb
@@ -1,5 +1,7 @@
 class AddDeletedAccountUser < ActiveRecord::Migration
   def up
+    add_column :users, :deleted_at, :datetime
+
     # TODO: once we get unregistered accounts, switch it to one of those
     User.by_name('deleted').update_all(name: "deleted_#{SecureRandom.hex(1)}")
     User.create!(
@@ -11,6 +13,7 @@ class AddDeletedAccountUser < ActiveRecord::Migration
   end
 
   def down
+    remove_column :users, :deleted_at
     User.find(-10).destroy!
   end
 end
