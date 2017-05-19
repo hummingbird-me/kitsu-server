7e1070380a7bcf57e01455b502b3cc9f9b4c9b02
Peter Lejeck <peter.lejeck@gmail.com>
2017-05-18 22:15:21 -0700

    Ignore status filter when generating per-status counts

--- app/processors/library_entry_processor.rb
+++ app/processors/library_entry_processor.rb
@@ -3,7 +3,7 @@ class LibraryEntryProcessor < JSONAPI::Processor
     result = super
 
     # Send filters to #section_counts method.
-    filters = params[:filters]
+    filters = params[:filters].reject { |k, _| k == :status }
     verified_filters = resource_klass.verify_filters(filters, context)
     counts = resource_klass.status_counts(verified_filters, context: context)
     result.meta[:status_counts] = counts
