3c74633e238da56a697de7751217984b5edaeef9
Thomas McNiven <hello@vevix.net>
2017-05-29 04:55:49 +1000

    Fix Rack::Attack paths

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,12 +1,12 @@
 class Rack::Attack
   throttle('logins/ip', limit: 15, period: 60.seconds) do |req|
-    if req.path == '/oauth/token' && req.post? && req.params['grant_type'] == 'password'
+    if req.path == '/api/oauth/token' && req.post? && req.params['grant_type'] == 'password'
       ActionDispatch::Request.new(req.env).remote_ip
     end
   end
 
   throttle('posts/token', limit: 3, period: 60.seconds) do |req|
-    if req.path == '/edge/posts' && req.post?
+    if req.path == '/api/edge/posts' && req.post?
       # return the email if present, nil otherwise
       req.env['HTTP_AUTHORIZATION']
     end
