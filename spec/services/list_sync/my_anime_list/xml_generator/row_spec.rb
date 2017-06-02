d36fd1ced2e8a30d9cba082c84958cb3b537f1c3
Peter Lejeck <peter.lejeck@gmail.com>
2017-06-01 20:06:26 -0700

    Refactor MAL XML generation to ListSync::MyAnimeList::XmlGenerator

---
+++ spec/services/list_sync/my_anime_list/xml_generator/row_spec.rb
@@ -0,0 +1,85 @@
+require 'rails_helper'
+
+RSpec.describe ListSync::MyAnimeList::XmlGenerator::Row do
+  subject { described_class.new(library_entry) }
+  let(:library_entry) { build(:library_entry, anime: media, media: media) }
+  let(:mapping) { OpenStruct.new(external_id: '123') }
+  let(:media) { build(:anime) }
+  before { allow(media).to receive(:mapping_for).and_return(mapping) }
+
+  describe '#to_xml' do
+    subject { described_class.new(library_entry).to_xml }
+    context 'for an Anime' do
+      let(:media) { build(:anime) }
+      let(:library_entry) { build(:library_entry, anime: media, media: media) }
+
+      it 'should have a series_animedb_id node' do
+        expect(subject.at_css('series_animedb_id').content).to eq('123')
+      end
+
+      it 'should have a my_watched_episodes node' do
+        expect(subject.at_css('my_watched_episodes')).not_to be_nil
+      end
+
+      it 'should have a my_times_watched node' do
+        expect(subject.at_css('my_times_watched')).not_to be_nil
+      end
+
+      context 'on planned list' do
+        before { library_entry.status = :planned }
+        it 'should have a my_status node with "Plan to Watch"' do
+          expect(subject.at_css('my_status').content).to eq('Plan to Watch')
+        end
+      end
+
+      context 'on current list' do
+        before { library_entry.status = :current }
+        it 'should have a my_status node with "Watching"' do
+          expect(subject.at_css('my_status').content).to eq('Watching')
+        end
+      end
+    end
+
+    context 'for a Manga' do
+      let(:media) { build(:manga) }
+      let(:library_entry) { build(:library_entry, manga: media, media: media) }
+
+      it 'should have a manga_mangadb_id node' do
+        expect(subject.at_css('manga_mangadb_id').content).to eq('123')
+      end
+
+      it 'should have a my_read_chapters node' do
+        expect(subject.at_css('my_read_chapters')).not_to be_nil
+      end
+
+      it 'should have a my_times_read node' do
+        expect(subject.at_css('my_times_read')).not_to be_nil
+      end
+    end
+
+    context 'with a started_at time' do
+      before { library_entry.started_at = 1.week.ago }
+
+      it 'should have a my_start_date node in YYYY-MM-DD format' do
+        expect(subject.at_css('my_start_date')).to match(/\d{4}-\d{2}-\d{2}/)
+      end
+    end
+
+    context 'with a finished_at time' do
+      before { library_entry.finished_at = 1.week.ago }
+
+      it 'should have a my_finish_date node in YYYY-MM-DD format' do
+        expect(subject.at_css('my_finish_date')).to match(/\d{4}-\d{2}-\d{2}/)
+      end
+    end
+
+    context 'with a rating' do
+      let(:rating) { rand(2..20) }
+      before { library_entry.rating = rating }
+
+      it 'should have a my_score node with half the Kitsu rating' do
+        expect(subject.at_css('my_score').content).to eq((rating / 2).to_s)
+      end
+    end
+  end
+end
