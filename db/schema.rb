f869333aed44778e26b89bf02f3743f187917d9f
cinquemb <cinque_mcfarlane-blake@brown.edu>
2017-06-03 06:51:08 -0400

    rebase with master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 20170601214239) do
 
   # These are extensions that must be enabled in order to support this database
