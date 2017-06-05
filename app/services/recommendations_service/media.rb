da1f2ab9e85c7b4d6e5d6abad9bfccf5a1af80f3
cinquemb <cinque_mcfarlane-blake@brown.edu>
2017-06-05 01:49:51 -0400

    drying by using enrich method for recomendations

--- app/services/recommendations_service/media.rb
+++ app/services/recommendations_service/media.rb
@@ -16,31 +16,29 @@ class RecommendationsService
     def recommendations_for(klass)
       res = client.get("recommendations/#{user.id}/")
       reccs = res.body.dig('results', 'recommendations', 'media', klass.name)
-      reccs = reccs.reduce(&:merge)
-      media_ids = reccs.keys.map { |key| key.split(':').last.to_i }
-      media = klass.find(media_ids).index_by { |m| "#{m.class.name}:#{m.id}" }
-      reccs = reccs.transform_keys { |key| media[key] }
-      reccs = reccs.transform_values { |val| val['score'] }
-      reccs = reccs.sort_by(&:last).reverse
-      reccs.map(&:first)
+      enrich(klass, reccs)
     end
 
     def realtime_recommendations_for(klass)
       res = client.get("realtime/#{user.id}/")
       reccs = res.body.dig('results', klass.name)
-      reccs = reccs.reduce(&:merge)
-      media_ids = reccs.keys.map { |key| key.split(':').last.to_i }
-      media = klass.find(media_ids).index_by { |m| "#{m.class.name}:#{m.id}" }
-      reccs = reccs.transform_keys { |key| media[key] }
-      reccs = reccs.transform_values { |val| val['score'] }
-      reccs = reccs.sort_by(&:last).reverse
-      reccs.map(&:first)
+      enrich(klass, reccs)
     end
 
     def strength
       res = client.get("profile_strength/#{user.id}/")
       strengths = res.body.dig('results', 'profile_strength')
       strengths
+    end
+
+    def enrich(klass, reccs)
+      reccs = reccs.reduce(&:merge)
+      media_ids = reccs.keys.map { |key| key.split(':').last.to_i }
+      media = klass.find(media_ids).index_by { |m| "#{m.class.name}:#{m.id}" }
+      reccs = reccs.transform_keys { |key| media[key] }
+      reccs = reccs.transform_values { |val| val['score'] }
+      reccs = reccs.sort_by(&:last).reverse
+      reccs.map(&:first)
     end
   end
 end
