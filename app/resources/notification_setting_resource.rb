239c9da3b5e73f42014e75e75bca09f4076040ab
cinquemb <cinque_mcfarlane-blake@brown.edu>
2017-06-14 09:57:20 -0400

    fixing resource attributes

--- app/resources/notification_setting_resource.rb
+++ app/resources/notification_setting_resource.rb
@@ -1,6 +1,6 @@
 class NotificationSettingResource < BaseResource
   attributes :email_enabled, :fb_messenger_enabled,
-    :mobile_enabled, :web_enabled :setting_type
+    :mobile_enabled, :web_enabled, :setting_type
 
   has_one :user
 
