060faa1f7c01738c8a84294172474ff48d79aac8
cinquemb <cinque_mcfarlane-blake@brown.edu>
2017-06-16 01:53:12 -0400

    adding migration for users notification settings

---
+++ db/migrate/20170616053125_add_notification_settings_for_users.rb
@@ -0,0 +1,18 @@
+class AddNotificationSettingsForUsers < ActiveRecord::Migration
+  def change
+  	all_users = User.all
+    all_users_notifications = []
+    all_users.each do |user|
+      NotificationSetting::NOTIFICATION_TYPES.each do |st|
+        all_users_notifications << {
+          setting_type: st,
+          user: user,
+          fb_messenger_enabled: false,
+          mobile_enabled: false,
+          email_enabled: false
+        }
+      end
+    end
+    NotificationSetting.create(all_users_notifications)
+  end
+end
