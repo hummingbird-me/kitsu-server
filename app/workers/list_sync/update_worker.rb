fbb818213bce2150a1adaf1d06b28777df13f07c
Peter Lejeck <peter.lejeck@gmail.com>
2017-06-16 21:05:02 -0700

    Fix reference to missing pending_logs variable

--- app/workers/list_sync/update_worker.rb
+++ app/workers/list_sync/update_worker.rb
@@ -9,7 +9,7 @@ module ListSync
       library_entry = LibraryEntry.find(library_entry_id)
       logs = LibraryEntryLog.for_entry(library_entry).pending
 
-      capture_sync_errors(linked_account, pending_logs) do
+      capture_sync_errors(linked_account, logs) do
         linked_account.list_sync.update!(library_entry)
         logs.each { |log| log.update!(sync_status: :success) }
       end
