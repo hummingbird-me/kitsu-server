d024c09f0c0ab580a98fb4096575014fcf4b9e34
Peter Lejeck <peter.lejeck@gmail.com>
2017-06-18 23:57:56 -0700

    Hide ranked-model details from API

--- app/resources/favorite_resource.rb
+++ app/resources/favorite_resource.rb
@@ -5,4 +5,19 @@ class FavoriteResource < BaseResource
   has_one :item, polymorphic: true
 
   filters :user_id, :item_type, :item_id
+
+  def self.find_records(filters, opts = {})
+    super(filters, opts).select(<<-SQL.squish)
+      *, (
+        row_number() OVER (
+          PARTITION BY user_id, item_type
+          ORDER BY fav_rank ASC
+        ) - 1
+      ) AS fav_rank
+    SQL
+  end
+
+  def fav_rank=(val)
+    _model.fav_rank_position = val
+  end
 end
