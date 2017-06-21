d775d2ecebee67165913ee0e420117be552eb778
Cinque McFarlane-Blake <cinque_mcfarlane-blake@brown.edu>
2017-06-20 23:33:05 -0400

    Fix/recalculate episode length (#160)

--- app/models/concerns/episodic.rb
+++ app/models/concerns/episodic.rb
@@ -4,6 +4,11 @@ module Episodic
   def recalculate_episode_length!
     # Try for the statistical mode (most common value) of episode lengths
     length, num = episodes.length_mode.values_at(:mode, :count)
+
+    # use length if num is nil
+    update(episode_length: length) if num.nil?
+    return if num.nil?
+
     # If it's less than half of episodes, use average instead
     if episode_count && num < (episode_count / 2)
       length = episodes.length_average
