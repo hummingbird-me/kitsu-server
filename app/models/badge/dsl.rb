38ef3a05626de3e9f34492dc2d355ee6861fb002
Peter Lejeck <peter.lejeck@gmail.com>
2017-06-27 01:38:03 -0700

    Improve Badge DSL code

--- app/models/badge/dsl.rb
+++ app/models/badge/dsl.rb
@@ -2,87 +2,46 @@ class Badge
   module DSL
     extend ActiveSupport::Concern
 
+    class Rank
+      attr_reader :goal
+
+      def initialize(&block)
+        super.instance_eval(&block)
+      end
+
+      def bestow_when(goal)
+        @goal = goal
+      end
+    end
+
+    included do
+      class_attribute :_progress, :_ranks, :_hidden
+      protected :_progress, :_ranks, :_hidden
+
+      self._ranks = {}
+      self._hidden = false
+    end
+
     class_methods do
-      def on(model, action = :save)
-        return if model.nil?
-        @model_class = model
-        @model_action = action
-        badge = self
-        model.public_send("after_#{action}") do
-          user = if self.class == User
-                   self
-                 else
-                   self.user
-                 end
-          badge.new(user).run
-        end
+      def progress(&block)
+        self._progress = block
       end
 
-      def progress(value = nil, &block)
-        if value || block
-          const_set('PROGRESS', value || block)
-        else
-          return nil unless defined? self::PROGRESS
-          const_get('PROGRESS')
-        end
-      end
-
-      # Provide `key "value"` methods for each of these (also accept blocks)
-      %i[title description].each do |attr|
-        define_method(attr) do |value = nil, &block|
-          if value || block
-            instance_variable_set("@#{attr}", value || block)
-          else
-            instance_variable_get("@#{attr}".to_sym)
-          end
-        end
-      end
-
-      # Create a new rank within the current badge or returns the current
-      # badge's rank
-      def rank(value = nil, &block)
-        return @rank if value.nil?
-        subclass = const_set("Rank#{value}", Class.new(self, &block))
-        subclass.rank = value
-        subclass.on(@model_class, @model_action)
-      end
-
-      def rank=(value)
-        @rank = value
-      end
-
-      def ranks
-        constants.grep(/^Rank/)
+      def rank(num, &block)
+        _ranks[num] = Rank.new(&block).goal
       end
 
       # Declare the goal for the badge
       def bestow_when(value = nil)
-        if value
-          @goal = value
-        elsif block_given?
-          @goal = Proc.new
-        end
+        _ranks[1] = value ? ->(progress) { progress > value } : Proc.new
       end
 
-      def goal
-        @goal
-      end
-
-      # Short-circuit hidden to set true
       def hidden
-        @hidden = true
+        self._hidden = true
       end
 
       def hidden?
-        @hidden
-      end
-
-      def root?
-        superclass == Badge
-      end
-
-      def processed?
-        !(root? && ranks.present?)
+        _hidden
       end
     end
   end
