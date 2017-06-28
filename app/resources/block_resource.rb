2bd14e53dfcdc0cca0a7541e569fe09f875c513a
Peter Lejeck <peter.lejeck@gmail.com>
2017-06-28 02:08:37 -0700

    Override BlockResource#records_for(:user)

--- app/resources/block_resource.rb
+++ app/resources/block_resource.rb
@@ -3,4 +3,9 @@ class BlockResource < BaseResource
   has_one :blocked
 
   filter :user
+
+  def records_for(relation_name)
+    return super unless relation_name == :user
+    _model.public_send relation_name
+  end
 end
