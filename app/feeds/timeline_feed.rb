2c180bea009677c0146edb585c5e79278b67a1c4
Peter Lejeck <peter.lejeck@gmail.com>
2017-06-29 22:57:05 -0700

    Fix TimelineFeed#stream_feed_for(type:) signature

--- app/feeds/timeline_feed.rb
+++ app/feeds/timeline_feed.rb
@@ -2,7 +2,9 @@ class TimelineFeed < Feed
   include MediaUpdatesFilterable
 
   # Prevent the suffix `_aggr` from being added, by marking it as "flat"
-  def stream_feed_for(filter: nil)
+  # rubocop:disable Lint/UnusedMethodArgument
+  def stream_feed_for(filter: nil, type: :flat)
+    # rubocop:enable lint/UnusedMethodArgument
     super(filter: filter, type: :flat)
   end
   alias_method :stream_feed, :stream_feed_for
