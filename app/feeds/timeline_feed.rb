95dd01fa213c703a9302cc9e72ac6590060340f3
Peter Lejeck <peter.lejeck@gmail.com>
2017-06-29 23:00:27 -0700

    Extract unsuffixed aggregated feed logic into concern

--- app/feeds/timeline_feed.rb
+++ app/feeds/timeline_feed.rb
@@ -1,13 +1,6 @@
 class TimelineFeed < Feed
   include MediaUpdatesFilterable
-
-  # Prevent the suffix `_aggr` from being added, by marking it as "flat"
-  # rubocop:disable Lint/UnusedMethodArgument
-  def stream_feed_for(filter: nil, type: :flat)
-    # rubocop:enable lint/UnusedMethodArgument
-    super(filter: filter, type: :flat)
-  end
-  alias_method :stream_feed, :stream_feed_for
+  include UnsuffixedAggregatedFeed
 
   def setup!
     # Follow own profile feed
