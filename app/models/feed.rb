7f9029d9e6da13e483ad23e9e1acb4d24d473755
Peter Lejeck <peter.lejeck@gmail.com>
2017-07-06 21:01:25 -0700

    Add Feed#follow_many method

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -27,6 +27,19 @@ class Feed
     Feed::StreamFeed.follow_many(follows_for(target), scrollback)
   end
 
+  # Follow multiple Feeds, optionally with a scrollback.
+  # Under the hood, this acts similar to {#follow}, generating a list of follows
+  # via {#follows_for} and then sending them to {Feed::StreamFeed#follow_many}
+  # in batches of up to 1000.
+  def follow_many(targets, scrollback: 30)
+    follows = targets.map { |target| follows_for(target) }.reduce(&:merge)
+    # Split the follows into groups of 990 to stay below the limit.  I'm not
+    # certain, but I believe the limit is 1000 follows per follow_many request.
+    follows.in_groups_of(990, false) do |follow_batch|
+      Feed::StreamFeed.follow_many(follow_batch, scrollback)
+    end
+  end
+
   # Unfollow another Feed, optionally keeping the history
   def unfollow(target, keep_history: false)
     # Stream doesn't provide an unfollow_many method, so we just gotta settle
