fe7bdd70568630f692ec65faf0ec9c1e844571fb
Peter Lejeck <peter.lejeck@gmail.com>
2017-07-24 00:24:38 -0700

    Fix policy specs

--- spec/policies/follow_policy_spec.rb
+++ spec/policies/follow_policy_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe FollowPolicy do
-  let(:follower) { token_for build(:user) }
-  let(:following) { build(:user) }
+  let(:follower) { token_for create(:user) }
+  let(:following) { token_for create(:user) }
   let(:other) { token_for build(:user) }
   let(:follow) do
-    build(:follow, follower: follower.resource_owner, followed: following)
+    build(:follow, follower: follower.resource_owner, followed: following.resource_owner)
   end
   subject { described_class }
 
   permissions :update? do
-    it('should not allow users') { should_not permit(follower, follow) }
+    it('should allow the follower') { should permit(follower, follow) }
+    it('should not allow the followed') { should_not permit(following, follow) }
+    it('should not allow users') { should_not permit(other, follow) }
     it('should not allow anons') { should_not permit(nil, follow) }
   end
 
