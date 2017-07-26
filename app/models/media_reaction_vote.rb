ee045634c92e7756c865208295c0fce40c45527b
Peter Lejeck <peter.lejeck@gmail.com>
2017-07-26 14:03:49 -0700

    Add notification when users like your reactions

--- app/models/media_reaction_vote.rb
+++ app/models/media_reaction_vote.rb
@@ -23,6 +23,8 @@
 # rubocop:enable Metrics/LineLength
 
 class MediaReactionVote < ApplicationRecord
+  include WithActivity
+
   belongs_to :media_reaction, required: true
   belongs_to :user, required: true
 
@@ -31,8 +33,13 @@ class MediaReactionVote < ApplicationRecord
   validate :vote_on_self
 
   def vote_on_self
-    if media_reaction.user == user
-      errors.add(:user, 'You can not vote for yourself')
-    end
+    errors.add(:user, 'You can not vote for yourself') if media_reaction.user == user
+  end
+
+  def stream_activity
+    media_reaction.user.notifications.activities.new(
+      verb: 'vote',
+      target: media_reaction
+    )
   end
 end
