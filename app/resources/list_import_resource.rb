856f7271c26725dbce0a96519ee7df39ccb16892
Peter Lejeck <peter.lejeck@gmail.com>
2017-07-25 22:39:19 -0700

    Serialize ListImport::MyAnimeListXML via ListImportResource

--- app/resources/list_import_resource.rb
+++ app/resources/list_import_resource.rb
@@ -1,6 +1,7 @@
 class ListImportResource < BaseResource
   include STIResource
 
+  model_hint model: ListImport::MyAnimeListXML
   model_hint model: ListImport::MyAnimeList
   model_hint model: ListImport::AnimePlanet
   model_hint model: ListImport::Anilist
