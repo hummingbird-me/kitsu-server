3c780f586d94764a7e2a73eb6036fa573980ad64
toyhammered <daniel@rassiner.com>
2017-07-27 18:03:43 -0700

    add progress bar to task

--- app/services/regenerate_stat_service.rb
+++ app/services/regenerate_stat_service.rb
@@ -35,10 +35,22 @@ class RegenerateStatService
     private
 
     def user_stat(media_column, stat_type)
-      User.where(id: LibraryEntry.select(:user_id).by_kind(media_column))
-          .find_each do |user|
-            user.stats.find_or_initialize_by(type: stat_type).recalculate!
-          end
+      users = User.where(id: LibraryEntry.select(:user_id).by_kind(media_column))
+      bar = progress_bar(stat_type, users.count)
+
+      users.find_each do |user|
+        bar.increment
+        user.stats.find_or_initialize_by(type: stat_type).recalculate!
+      end
+    end
+
+    def progress_bar(title, count)
+      ProgressBar.create(
+        title: title,
+        total: count,
+        output: STDERR,
+        format: '%a (%p%%) |%B| %E %t'
+      )
     end
   end
 end
