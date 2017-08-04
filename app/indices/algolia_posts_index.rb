91aa0dab0b66c3f01afb0a2df2eda05015b27c08
Peter Lejeck <peter.lejeck@gmail.com>
2017-08-03 20:08:29 -0700

    Add posts index

---
+++ app/indices/algolia_posts_index.rb
@@ -0,0 +1,6 @@
+class AlgoliaPostsIndex < BaseIndex
+  attribute :content
+  attribute :post_likes_count, frequency: 2.5
+  has_one :user, as: :name
+  has_one :group, as: :name, via: :target_group
+end
