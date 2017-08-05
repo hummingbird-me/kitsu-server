c09d7626e729f6737a8c6585797439f6b2922e78
toyhammered <daniel@rassiner.com>
2017-08-04 20:59:18 -0700

    first attempt at increment

--- app/models/concerns/stat/activity_history.rb
+++ app/models/concerns/stat/activity_history.rb
@@ -18,6 +18,7 @@ class Stat < ApplicationRecord
     DEFAULT_STATS = {
       'days' => {},
       'first_event_date' => Time.now.to_date,
+      'last_update_date' => Time.now.to_date,
       'week_high_score' => 0,
       'total_progress' => 0
     }.freeze
@@ -45,10 +46,11 @@ class Stat < ApplicationRecord
       stats_data['total_progress'] = 0
       # Record the date their first library_event was created
       stats_data['first_event_date'] = activity_dates.first[0].to_date
+      stats_data['last_update_date'] = activity_dates.last[0].to_date
       # Set the progress and time for each day
       # Set the total_progress from all days
       activity_dates.each do |date, amount, time|
-        stats_data['days'][date.year][date.month][date.day] = {
+        stats_data['days'][date.year.to_s][date.month.to_s][date.day.to_s] = {
           'total_progress' => amount,
           'total_time' => time
         }
@@ -57,14 +59,15 @@ class Stat < ApplicationRecord
       # Set the week_high_score from their first library_event date
       stats_data['week_high_score'] = week_high_score(activity_dates.first[0])
 
+      pp stats_data
       save!
     end
 
     def pregenerate_all_days(start_date)
       dates = start_date.to_date..Time.now.to_date
 
-      stats_data['days'] = dates.each_with_object(preset_hash) do |date, h|
-        h[date.year][date.month][date.day] = {
+      dates.each_with_object(preset_hash) do |date, h|
+        h[date.year.to_s][date.month.to_s][date.day.to_s] = {
           'total_progress' => 0,
           'total_time' => 0
         }
@@ -81,7 +84,7 @@ class Stat < ApplicationRecord
       highest = 0
 
       dates.each do |date|
-        current << stats_data['days'][date.year][date.month][date.day]['total_progress']
+        current << stats_data['days'][date.year.to_s][date.month.to_s][date.day.to_s]['total_progress']
 
         highest = current.sum if current.sum > highest
         current.shift if current.count == 7
@@ -92,20 +95,46 @@ class Stat < ApplicationRecord
 
     class_methods do
       def increment(user, library_event)
+        # return if library_event does not contain progress
+
         record = user.stats.find_or_initialize_by(
           type: "Stat::#{media_type}ActivityHistory"
         )
 
-        record.stats_data = DEFAULT_STATS.deep_dup if record.new_record?
+        if record.new_record?
+          record.stats_data = DEFAULT_STATS.deep_dup
+          record.stats_data['days'] = record.pregenerate_all_days(record.stats_data['last_update_date'])
+        end
 
-        # In case recalculate has not been run this will prevent any errors
-        record.stats_data['activity'] ||= []
-        record.stats_data['total'] ||= 0
+        last_update = record.stats_data['last_update_date'].to_date + 1.day
+        event_created = library_event['created_at'].to_date
 
-        # add 1 to total
-        record.stats_data['total'] += 1
-        # push library_event into activity array
-        record.stats_data['activity'].push(library_event)
+        # pregenerate all the missing days information
+        if last_update <= event_created
+          record.stats_data['days'].deep_merge!(record.pregenerate_all_days(last_update))
+        end
+
+        # update the library_event day that is being referenced
+        total_progress = library_event.changed_data['progress'][1] - library_event.changed_data['progress'][0]
+        total_time = (library_event.media_episode_length * total_progress)
+        record.stats_data['days'][event_created.year.to_s][event_created.month.to_s][event_created.day.to_s]['total_progress'] += total_progress
+        record.stats_data['days'][event_created.year.to_s][event_created.month.to_s][event_created.day.to_s]['total_time'] += total_time
+        record.stats_data['total_progress'] += total_progress
+
+        record.stats_data['last_update_data'] = event_created
+
+        dates = (6.days.ago.to_date..Time.now.to_date).to_date
+        check_week_high = []
+
+        dates.each do |date|
+          check_week_high << record.stats_data['days'][date.year.to_s][date.month.to_s][date.day.to_s]['total_progress']
+        end
+
+        # update week high score
+        record.stats_data['week_high_score'] = check_week_high if check_week_high > record.stats_data['week_high_score']
+
+
+
 
         record.save!
       end
