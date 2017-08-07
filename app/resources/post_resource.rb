e0ecb316a70686cac3e126c3c274ec89bad65d4c
Peter Lejeck <peter.lejeck@gmail.com>
2017-08-07 01:16:11 -0700

    Remove references to ama_id in PostResource

--- app/resources/post_resource.rb
+++ app/resources/post_resource.rb
@@ -3,13 +3,14 @@ class PostResource < BaseResource
 
   attributes :content, :content_formatted, :comments_count, :post_likes_count,
     :spoiler, :nsfw, :blocked, :deleted_at, :top_level_comments_count,
-    :edited_at, :target_interest, :ama_id
+    :edited_at, :target_interest
 
   has_one :user
   has_one :target_user
   has_one :target_group
   has_one :media, polymorphic: true
   has_one :spoiled_unit, polymorphic: true
+  has_one :ama, foreign_key: 'original_post_id', foreign_key_on: :related
   has_many :post_likes
   has_many :comments
   has_many :uploads
@@ -21,5 +22,4 @@ class PostResource < BaseResource
   def target_interest
     _model.target_interest.underscore.dasherize if _model.target_interest
   end
-  filters :ama_id
 end
