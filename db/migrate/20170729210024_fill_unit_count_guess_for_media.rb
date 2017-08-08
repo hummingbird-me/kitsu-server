eab94f806222f2ca67a50abf80030f762d0422b1
Peter Lejeck <peter.lejeck@gmail.com>
2017-08-08 04:43:16 -0700

    Suppress errors in FillUnitCountGuessForMedia migration

--- db/migrate/20170729210024_fill_unit_count_guess_for_media.rb
+++ db/migrate/20170729210024_fill_unit_count_guess_for_media.rb
@@ -6,15 +6,21 @@ class FillUnitCountGuessForMedia < ActiveRecord::Migration
                 .group(:anime_id)
                 .maximum(:progress)
                 .each do |a, pr|
-                  anime = Anime.find(a)
-                  anime.update_unit_count_guess([(pr + 1), anime.default_progress_limit].min)
+                  begin
+                    anime = Anime.find(a)
+                    anime.update_unit_count_guess([(pr + 1), anime.default_progress_limit].min)
+                  rescue
+                  end
                 end
     LibraryEntry.where(manga_id: Manga.where(chapter_count: nil).ids)
                 .group(:manga_id)
                 .maximum(:progress)
                 .each do |m, pr|
-                  manga = Manga.find(m)
-                  manga.update_unit_count_guess([(pr + 1), manga.default_progress_limit].min)
+                  begin
+                    manga = Manga.find(m)
+                    manga.update_unit_count_guess([(pr + 1), manga.default_progress_limit].min)
+                  rescue
+                  end
                 end
   end
 end
