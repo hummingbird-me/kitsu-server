b00a3467153ca2a7d58376f7f0e7263df409126d
Peter Lejeck <peter.lejeck@gmail.com>
2017-08-08 02:42:06 -0700

    Optimize FillUnitCountGuessForMedia migration

--- db/migrate/20170729210024_fill_unit_count_guess_for_media.rb
+++ db/migrate/20170729210024_fill_unit_count_guess_for_media.rb
@@ -2,19 +2,19 @@ class FillUnitCountGuessForMedia < ActiveRecord::Migration
   disable_ddl_transaction!
 
   def change
-    Anime.where(episode_count: nil)
-         .joins('INNER JOIN library_entries le ON le.anime_id = anime.id')
-         .where('le.progress = (SELECT MAX(le.progress) FROM library_entries le WHERE le.media_id = anime.id)')
-         .pluck('anime.id, le.progress').each do |a, pr|
-           anime = Anime.find(a)
-           anime.update_unit_count_guess([(pr + 1), anime.default_progress_limit].min)
-         end
-    Manga.where(chapter_count: nil)
-         .joins('INNER JOIN library_entries le ON le.manga_id = manga.id')
-         .where('le.progress = (SELECT MAX(le.progress) FROM library_entries le WHERE le.media_id = manga.id)')
-         .pluck('manga.id, le.progress').each do |m, pr|
-           manga = Manga.find(m)
-           manga.update_unit_count_guess([(pr + 1), manga.default_progress_limit].min)
-         end
+    LibraryEntry.where(anime_id: Anime.where(episode_count: nil).ids)
+                .group(:anime_id)
+                .maximum(:progress)
+                .each do |a, pr|
+                  anime = Anime.find(a)
+                  anime.update_unit_count_guess([(pr + 1), anime.default_progress_limit].min)
+                end
+    LibraryEntry.where(manga_id: Manga.where(chapters_count: nil).ids)
+                .group(:manga_id)
+                .maximum(:progress)
+                .each do |a, pr|
+                  manga = Manga.find(m)
+                  manga.update_unit_count_guess([(pr + 1), manga.default_progress_limit].min)
+                end
   end
 end
