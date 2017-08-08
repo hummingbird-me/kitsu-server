6f19b975faf8f7d0c762b7a584a4d1948989480e
Peter Lejeck <peter.lejeck@gmail.com>
2017-08-07 17:54:56 -0700

    Bail early if there are no links

--- lib/html/pipeline/embed_filter.rb
+++ lib/html/pipeline/embed_filter.rb
@@ -3,6 +3,7 @@ module HTML
     class EmbedFilter < HTML::Pipeline::Filter
       def call
         first_link = doc.at_css('a.autolink')
+        return doc unless first_link
         result[:embed] = EmbedService.new(first_link['href']).as_json
         result[:embeddable_links] = doc.css('a.autolink').map { |a| a['href'] }
         doc
