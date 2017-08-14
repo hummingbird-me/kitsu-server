4a110aa51a6b3a863de15e145b1f757d372b1a8e
Peter Lejeck <peter.lejeck@gmail.com>
2017-08-14 16:33:07 -0700

    Set Chewy.request_strategy

--- config/initializers/chewy.rb
+++ config/initializers/chewy.rb
@@ -1,2 +1,5 @@
 Chewy.use_after_commit_callbacks = false if Rails.env.test?
-Chewy.strategy(ENV['CHEWY_STRATEGY'].to_sym) if ENV['CHEWY_STRATEGY']
+if ENV['CHEWY_STRATEGY']
+  Chewy.strategy(ENV['CHEWY_STRATEGY'].to_sym)
+  Chewy.request_strategy = ENV['CHEWY_STRATEGY'].to_sym
+end
