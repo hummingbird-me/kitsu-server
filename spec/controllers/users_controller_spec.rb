2de3f1c3e841618666d3b673fcbd56ff8d3c23bf
Peter Lejeck <peter.lejeck@gmail.com>
2017-08-18 23:28:01 -0700

    Abandon the bio attribute

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -151,7 +151,7 @@ RSpec.describe UsersController, type: :controller do
         type: 'users',
         attributes: {
           name: 'Senjougahara',
-          bio: 'hitagi crab',
+          about: 'hitagi crab',
           email: 'senjougahara@hita.gi',
           password: 'headtilt'
         }
