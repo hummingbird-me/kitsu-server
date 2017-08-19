f1331449c4b89555657dcc6a8de36f2e2683032c
Peter Lejeck <peter.lejeck@gmail.com>
2017-08-18 23:34:24 -0700

    Clean up UserResource attributes more

--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -1,13 +1,12 @@
 class UserResource < BaseResource
   PRIVATE_FIELDS = %i[email password confirmed previous_email language time_zone country
                       share_to_global title_language_preference sfw_filter rating_system
-                      theme].freeze
+                      theme facebook_id].freeze
 
-  attributes :name, :past_names, :slug, :about, :about_formatted, :location, :waifu_or_husbando,
-    :followers_count, :facebook_id, :following_count, :life_spent_on_anime, :birthday, :gender,
-    :comments_count, :favorites_count, :likes_given_count, :reviews_count, :likes_received_count,
-    :posts_count, :ratings_count, :media_reactions_count, :pro_expires_at, :title,
-    :profile_completed, :feed_completed, :website, :slug
+  attributes :name, :past_names, :slug, :about, :location, :waifu_or_husbando, :followers_count,
+    :following_count, :life_spent_on_anime, :birthday, :gender, :comments_count, :favorites_count,
+    :likes_given_count, :reviews_count, :likes_received_count, :posts_count, :ratings_count,
+    :media_reactions_count, :pro_expires_at, :title, :profile_completed, :feed_completed, :website
   attributes :avatar, :cover_image, format: :attachment
   attributes(*PRIVATE_FIELDS)
 
