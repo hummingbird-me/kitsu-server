608019926db93a49ca48f496eb15d79ed37df6b5
Peter Lejeck <peter.lejeck@gmail.com>
2017-08-29 17:43:47 -0700

    Annotate ListImport::Zorro

--- app/models/list_import/zorro.rb
+++ app/models/list_import/zorro.rb
@@ -1,3 +1,27 @@
+# rubocop:disable Metrics/LineLength
+# == Schema Information
+#
+# Table name: list_imports
+#
+#  id                      :integer          not null, primary key
+#  error_message           :text
+#  error_trace             :text
+#  input_file_content_type :string
+#  input_file_file_name    :string
+#  input_file_file_size    :integer
+#  input_file_updated_at   :datetime
+#  input_text              :text
+#  progress                :integer
+#  status                  :integer          default(0), not null
+#  strategy                :integer          not null
+#  total                   :integer
+#  type                    :string           not null
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  user_id                 :integer          not null
+#
+# rubocop:enable Metrics/LineLength
+
 require 'zorro'
 
 class ListImport
