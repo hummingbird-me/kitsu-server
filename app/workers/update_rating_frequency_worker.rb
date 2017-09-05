9015e6ccd2d4c927f748dab729f525f7166bd8a0
cinquemb <cinque_mcfarlane-blake@brown.edu>
2017-09-05 02:20:08 -0400

    passing rating and diff into the worker

--- app/workers/update_rating_frequency_worker.rb
+++ app/workers/update_rating_frequency_worker.rb
@@ -1,10 +1,16 @@
 class UpdateRatingFrequencyWorker
   include Sidekiq::Worker
 
-  def perform(klass_name, klass_id, query)
+  def perform(klass_name, klass_id, rating, diff)
+    update_query = <<-EOF
+      rating_frequencies = rating_frequencies
+        || hstore('#{rating}', (
+          COALESCE(rating_frequencies->'#{rating}', '0')::integer + #{diff}
+        )::text)
+    EOF
     klass = klass_name.safe_constantize
     klass_obj = klass.find(klass_id)
-    klass.where(id: klass_id).update_all(query)
+    klass.where(id: klass_id).update_all(update_query)
     klass_obj.touch
   end
 end
