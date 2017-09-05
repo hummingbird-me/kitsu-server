88b10fb303f3fa4f6992e0956efa221d07ac2f4a
Peter Lejeck <peter.lejeck@gmail.com>
2017-09-05 01:18:44 -0700

    Add ao_facebook_id import

--- app/services/zorro/wrapper/user.rb
+++ app/services/zorro/wrapper/user.rb
@@ -20,19 +20,19 @@ module Zorro
       end
 
       def followers_count
-        @details['followersCount']
+        details['followersCount']
       end
 
       def following_count
-        @details['followingCount']
+        details['followingCount']
       end
 
       def about
-        @details['about']
+        details['about']
       end
 
       def avatar
-        file(@details['avatarRegular'])
+        file(details['avatarRegular'])
       end
 
       def cover_image
@@ -40,17 +40,21 @@ module Zorro
       end
 
       def pro?
-        @data['badges'].include?('PRO')
+        @data['badges']&.include?('PRO') || false
       end
 
       def pro_plus?
-        @data['badges'].include?('PRO+')
+        @data['badges']&.include?('PRO+') || false
       end
 
       def pro_tier
         return :pro if pro?
         return :pro_plus if pro_plus?
         nil
+      end
+
+      def facebook_id
+        @data.dig('_auth_data_facebook', 'id')
       end
 
       def admin?
@@ -61,7 +65,10 @@ module Zorro
       def initial_merge_onto(user)
         password_key = user.password_digest? ? :ao_password : :password_digest
         user.assign_attributes(
+          # Password Auth
           password_key => password_digest,
+          # Facebook Auth
+          ao_facebook_id: facebook_id,
           ao_id: id,
           ao_pro: pro_tier
         )
