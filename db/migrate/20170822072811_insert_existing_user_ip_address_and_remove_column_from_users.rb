b9984cae51753eab81b528922e3a73a82d470663
Peter Lejeck <peter.lejeck@gmail.com>
2017-09-08 19:58:24 -0700

    Skip bad IPs in migration

--- db/migrate/20170822072811_insert_existing_user_ip_address_and_remove_column_from_users.rb
+++ db/migrate/20170822072811_insert_existing_user_ip_address_and_remove_column_from_users.rb
@@ -3,9 +3,10 @@ class InsertExistingUserIpAddressAndRemoveColumnFromUsers < ActiveRecord::Migrat
   def change
     User.find_each do |user|
       ips = user.ip_addresses.map do |ip|
+        return puts "BAD IP: #{ip} for #{user.id}" unless ip.is_a?(IPAddr)
         {ip_address: ip, user_id: user.id}
       end
-      UserIpAddress.create!(ips)
+      UserIpAddress.create!(ips.compact)
     end
     remove_column :users, :ip_addresses, :inet
   end
