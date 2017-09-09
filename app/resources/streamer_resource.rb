ab47b542f0e2b920bbd0db7cf86c8c11dcbeee98
Peter Lejeck <peter.lejeck@gmail.com>
2017-09-09 01:54:27 -0700

    Expose Streamer#streaming_links_count in API

--- app/resources/streamer_resource.rb
+++ app/resources/streamer_resource.rb
@@ -1,7 +1,7 @@
 require 'unlimited_paginator'
 
 class StreamerResource < BaseResource
-  attributes :site_name
+  attributes :site_name, :streaming_links_count
   attributes :logo, format: :attachment
 
   has_many :streaming_links
