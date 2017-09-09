0908d6d5337fe9ac2c9bd133c7598891aa1d4ec3
Peter Lejeck <peter.lejeck@gmail.com>
2017-09-09 01:42:07 -0700

    Expose Streamer#logo as an attachment in API

--- app/resources/streamer_resource.rb
+++ app/resources/streamer_resource.rb
@@ -1,7 +1,8 @@
 require 'unlimited_paginator'
 
 class StreamerResource < BaseResource
-  attributes :site_name, :logo
+  attributes :site_name
+  attributes :logo, format: :attachment
 
   has_many :streaming_links
 
