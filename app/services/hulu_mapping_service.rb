0914a9ec12982ce2572ee8f55b32c9cbb6d67e76
cinquemb <cinque_mcfarlane-blake@brown.edu>
2017-09-11 09:37:37 -0400

    addressing more comments on pr

--- app/services/hulu_mapping_service.rb
+++ app/services/hulu_mapping_service.rb
@@ -9,17 +9,21 @@ class HuluMappingService
     @since = since
   end
 
-  # Grabs all available series from hulu, maps them as wel as their episodes
-  def sync_series_and_episodes
-    ActiveRecord::Base.logger = Logger.new(nil)
-    Chewy.strategy(:bypass)
+  # Grabs all available series from hulu
+  def get_series_for_episodes(limit)
     offset = 0
-    limit = 10_000
     series_res = Typhoeus.get(
       "#{HULU_URL}/series?guid=#{guid}&primary_category=animation&offset=#{offset}&limit=#{limit}"
     )
-    return unless series_res.sucess?
-    hulu_anime = JSON.parse(series_res.body)['results']
+    return unless series_res.success?
+    JSON.parse(series_res.body)['results']
+  end
+
+  # Start of syncing series and episodes on hulu with kitsu anime
+  def sync_series_and_episodes
+    limit = 10_000
+    hulu_anime = get_series_for_episodes(limit)
+    return if hulu_anime.nil?
     hulu_anime.each do |item|
       item = item.deep_symbolize_keys
       series_id = item[:id]
@@ -29,30 +33,34 @@ class HuluMappingService
       }
       kitsu_anime = Mapping.guess('Anime', mapping_object)
       next unless kitsu_anime
-      get_hulu_episodes_for_series(kitsu_anime, series_id)
+      create_kitsu_hulu_series_mapping(kitsu_anime, series_id)
+      hulu_episodes = get_hulu_episodes_for_series(series_id, limit)
+      next unless hulu_episodes
+      sync_episodes_for_anime(hulu_episodes, kitsu_anime)
     end
   end
 
-  def get_hulu_episodes_for_series(kitsu_anime, series_id)
+  def create_kitsu_hulu_series_mapping(kitsu_anime, series_id)
     # creating mapping with series to kitsu anime
     kitsu_anime.mappings.where(
       external_site: 'hulu',
       external_id: series_id
     ).first_or_create
+  end
 
+  def get_hulu_episodes_for_series(series_id, limit)
     # grab available episodes from hulu with anime
-    since_param = since.advance(hours: -24).strftime('%F') if since
+    since_param = (since - 24.hours).strftime('%F') if since
     ep_url = "#{HULU_URL}/assets?guid=#{guid}&series_id=#{series_id}&type=episode&limit=#{limit}"
     ep_url += "&since=#{since_param}" if since
     episode_res = Typhoeus.get(
       ep_url
     )
-    return unless episode_res.sucess?
-    hulu_episodes = JSON.parse(episode_res.body)
-    sync_episodes_for_anime(hulu_episodes)
+    return unless episode_res.success?
+    JSON.parse(episode_res.body)
   end
 
-  def sync_episodes_for_anime(hulu_episodes)
+  def sync_episodes_for_anime(hulu_episodes, kitsu_anime)
     hulu_ep_hash = hulu_episodes.each_with_object({}) do |ep, acc|
       ep = ep.deep_symbolize_keys
       acc[ep[:number]] = ep
