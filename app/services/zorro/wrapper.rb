c33f071b7f7bad8e244f9dfedb954934a66a3e6b
Peter Lejeck <peter.lejeck@gmail.com>
2017-09-14 22:51:16 -0700

    Comment Zorro::Wrapper and add Zorro::Wrapper::BasePost

--- app/services/zorro/wrapper.rb
+++ app/services/zorro/wrapper.rb
@@ -2,25 +2,35 @@ module Zorro
   class Wrapper
     FILE_PREFIX = 'https://aozora-assets.s3.amazonaws.com/'.freeze
 
+    # @param [String] data The Data to wrap up
     def initialize(data)
       @data = data
     end
 
+    # @return [String] the MongoDB ID from Aozora
     def id
       @data['_id']
     end
 
+    # @return [Time] the creation time
     def created_at
-      @data['createdAt']
+      @data['_created_at']
     end
 
+    # @return [Time] the update time
     def updated_at
-      @data['updatedAt']
+      @data['_updated_at']
     end
 
+    # Wrap a filename into a complete fully-qualified URL to the file in the Aozora S3 bucket
+    # @param name [String] the name of the file
+    # @return [String] the complete URL to the file
     def self.file(name)
-      "#{FILE_PREFIX}#{name}"
+      return nil unless name.present?
+      "#{FILE_PREFIX}#{name}".tap { |x| puts x }
     end
+
+    # Shortcuts to {Wrapper.file} and {Zorro::DB.assoc} for subclasses to use
     delegate :file, to: :class
     delegate :assoc, to: Zorro::DB
   end
