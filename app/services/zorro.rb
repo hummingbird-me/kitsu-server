ceffb41ef17330feefd52c331f89908cc8e89f9e
Peter Lejeck <peter.lejeck@gmail.com>
2017-09-14 22:51:40 -0700

    Comment Zorro.assoc method

--- app/services/zorro.rb
+++ app/services/zorro.rb
@@ -1,26 +1,50 @@
 module Zorro
+  # Connect to MongoDB if configured
   Client = Mongo::Client.new(ENV['AOZORA_MONGO_URL']) if ENV['AOZORA_MONGO_URL'].present?
 
   module DB
+    # Create shortcuts to various useful collections if we're connected
     if ENV['AOZORA_MONGO_URL'].present?
-      # Shortcuts to various collections
-      User = Client['_User']
+      # Profiles
+      User = Client['_User'] # Parse prefixes the User collection with an underscore
       UserDetails = Client['UserDetails']
+      # Media
       Anime = Client['Anime']
+      # Community
       Post = Client['Post']
       TimelinePost = Client['TimelinePost']
       Thread = Client['Thread']
+      # Library
       AnimeProgress = Client['AnimeProgress']
     end
 
-    def self.assoc(assoc)
-      case assoc
+    # Process a Parse pointer and load the document it refers to
+    #
+    # @overload assoc(ref)
+    #   @param ref [String, Hash] a reference to a document
+    #   @return [Hash] the data in the referenced document
+    #   @example With a String-based reference
+    #     assoc("User$1d2c3b4a") #=> { "_id": "1d2c3b4a", ... }
+    #   @example With a Hash-based reference
+    #     assoc(
+    #       '__type' => 'Pointer',
+    #       'className' => 'User',
+    #       'objectId' => '1d2c3b4a'
+    #     )
+    #     #=> { "_id": "1d2c3b4a", ... }
+    # @overload assoc(ref)
+    #   @param ref [Array<Hash,String>] a list of references to document
+    #   @return [Array<Hash>] the data in the referenced documents
+    def self.assoc(ref)
+      case ref
       when String
-        collection, id = assoc.split('$')
+        collection, id = ref.split('$')
         Zorro::Client[collection].find(_id: id).limit(1).first
       when Hash
-        collection, id = assoc.values_at('className', 'objectId')
+        collection, id = ref.values_at('className', 'objectId')
         Zorro::Client[collection].find(_id: id).limit(1).first
+      when Array
+        ref.map { |r| assoc(r) }
       end
     end
   end
