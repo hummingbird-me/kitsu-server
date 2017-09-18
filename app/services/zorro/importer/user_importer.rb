2fb5059806f33983231a68fcf3dbc50bfa12920f
Peter Lejeck <peter.lejeck@gmail.com>
2017-09-18 15:07:22 -0700

    Add UserImporter to coordinate importing a user from Aozora

---
+++ app/services/zorro/importer/user_importer.rb
@@ -0,0 +1,46 @@
+module Zorro
+  module Importer
+    # Imports a user from the Aozora database into Kitsu, importing the profile and (if there's no
+    # existing library entries) the library.
+    class UserImporter
+      # @param user [Hash] the user document from the Aozora database
+      def initialize(user)
+        @user = Zorro::Wrapper::User.new(user)
+      end
+
+      # Execute the import, optionally forcing an overwrite
+      #
+      # @param force [Boolean] whether to forcibly overwrite existing Kitsu data with Aozora data
+      # @return [void]
+      def run!(force: false)
+        # Import the profile data
+        user_id = import_profile(force: force).id
+        # Import the library if they don't have an existing library
+        import_library_to(user) if force || LibraryEntry.where(user_id: user_id).blank?
+      end
+
+      # Import the profile
+      #
+      # @param force [Boolean] whether to forcibly overwrite existing Kitsu data with Aozora data
+      # @return [User] the Kitsu user affected by this import
+      def import_profile(force: false)
+        @profile ||= Zorro::Importer::ProfileImporter.new(@user).run!(force: force)
+      end
+
+      # Import the library data
+      #
+      # @param user_id [Integer] the Kitsu User ID to apply the import to
+      # @return [ListImport::Zorro] the list import task generated for this user
+      def import_library_to(user_id)
+        ListImport::Zorro.create!(user_id: user_id, strategy: :obliterate, input_text: @user.id)
+      end
+
+      private
+
+      # @return [User, nil] any existing Kitsu user with the same email
+      def email_user
+        @email_user ||= ::User.by_email(@user.email).first
+      end
+    end
+  end
+end
