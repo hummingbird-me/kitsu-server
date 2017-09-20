4b9504a67be73613aa7e974cdb153a2bdf820948
Peter Lejeck <peter.lejeck@gmail.com>
2017-09-19 17:48:29 -0700

    Finish out PostImporter

--- app/services/zorro/importer/post_importer.rb
+++ app/services/zorro/importer/post_importer.rb
@@ -1,17 +1,36 @@
+require_dependency 'with_progress_bar'
+
 module Zorro
   module Importer
+    # Imports a post from Aozora to Kitsu, and provides a way to import all posts in one fell swoop.
     class PostImporter
+      include WithProgressBar
+
+      # The collections we need to import posts from
+      COLLECTIONS = [Zorro::DB::TimelinePost, Zorro::DB::Thread, Zorro::DB::Post].freeze
+
+      # Import all posts from Aozora to Kitsu
       def self.run!
-        Zorro::DB::Post.find.each do |post|
-          new(post).run!
-          puts post['_id']
-        end
+        COLLECTIONS.each { |coll| run_for! coll }
       end
 
+      # Import all posts from a collection into Kitsu
+      # @param collection [Mongo::Collection] the collection to import from
+      def self.run_for!(collection)
+        bar = progress_bar(collection.name, collection.count)
+        collection.find.each do |post|
+          new(post).run!
+          bar.increment
+        end
+        bar.finish
+      end
+
+      # @param post [Hash] the document representing the post in Aozora
       def initialize(post)
         @post = Zorro::Wrapper::PostWrapper.wrap(post)
       end
 
+      # Saves the post into the Kitsu database
       def run!
         @post.save!
       end
