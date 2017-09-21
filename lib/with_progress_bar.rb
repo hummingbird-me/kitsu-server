81ea6035c724bb15788477412cb7792c4dc85393
Peter Lejeck <peter.lejeck@gmail.com>
2017-09-20 19:09:32 -0700

    Expose WithProgressBar#progress_bar on both class & instance

--- lib/with_progress_bar.rb
+++ lib/with_progress_bar.rb
@@ -2,17 +2,21 @@
 module WithProgressBar
   extend ActiveSupport::Concern
 
-  # Shortcut to generating a new progress bar object
-  #
-  # @param title [String] the label to be displayed for the progress bar
-  # @param count [Integer] the number of ticks this progress bar has to completion
-  # @return [ProgressBar] the progress bar object
-  def progress_bar(title, count)
-    ProgressBar.create(
-      title: title,
-      total: count,
-      output: STDERR,
-      format: '%a (%p%%) |%B| %E %t'
-    )
+  class_methods do
+    # Shortcut to generating a new progress bar object
+    #
+    # @param title [String] the label to be displayed for the progress bar
+    # @param count [Integer] the number of ticks this progress bar has to completion
+    # @return [ProgressBar] the progress bar object
+    def progress_bar(title, count)
+      ProgressBar.create(
+        title: title,
+        total: count,
+        output: STDERR,
+        format: '%a (%p%%) |%B| %E %t'
+      )
+    end
   end
+
+  delegate :progress_bar, to: :class
 end
