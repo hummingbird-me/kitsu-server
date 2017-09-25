4724c3288ed55294c8aeebc2e01ed5c4213472b5
cinquemb <cinque_mcfarlane-blake@brown.edu>
2017-09-25 06:47:09 -0400

    updating policy/validations for uploads

--- app/policies/upload_policy.rb
+++ app/policies/upload_policy.rb
@@ -1,2 +1,18 @@
 class UploadPolicy < ApplicationPolicy
+  def show?
+    true
+  end
+
+  def create?
+    user ? true : false
+  end
+
+  alias_method :update?, :is_owner?
+  alias_method :destroy?, :is_owner?
+
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
 end
