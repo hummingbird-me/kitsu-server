80a403c85220a64e9138446f74f23fea58e0315a
Peter Lejeck <peter.lejeck@gmail.com>
2017-10-02 17:07:29 -0700

    Fix admin check in GroupPolicy::Scope

--- app/policies/group_policy.rb
+++ app/policies/group_policy.rb
@@ -29,7 +29,7 @@ class GroupPolicy < ApplicationPolicy
 
   class Scope < Scope
     def resolve
-      return scope if is_admin?
+      return scope if user&.has_role?(:admin, Group)
       return scope.visible_for(user) if see_nsfw?
       scope.sfw.visible_for(user)
     end
