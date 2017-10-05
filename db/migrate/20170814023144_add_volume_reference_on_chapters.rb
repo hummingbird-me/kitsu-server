e2d7f663d63d406e4f3d12082a7da5d231d9d1e0
cinquemb <cinque_mcfarlane-blake@brown.edu>
2017-10-04 22:49:23 -0400

    remove unessisary args for volume ref on chapters

--- db/migrate/20170814023144_add_volume_reference_on_chapters.rb
+++ db/migrate/20170814023144_add_volume_reference_on_chapters.rb
@@ -1,6 +1,5 @@
 class AddVolumeReferenceOnChapters < ActiveRecord::Migration
   def change
-    add_reference :chapters, :volume, index: true,
-                                      foreign_key: true, null: true, default: nil, required: true
+    add_reference :chapters, :volume, index: true, foreign_key: true
   end
 end
