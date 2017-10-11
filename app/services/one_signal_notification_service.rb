30e8e130fb03219aa1bf19dbcac2f347a1204dce
Peter Lejeck <peter.lejeck@gmail.com>
2017-10-10 20:06:56 -0700

    Bring back deletion of failed players

--- app/services/one_signal_notification_service.rb
+++ app/services/one_signal_notification_service.rb
@@ -4,21 +4,29 @@ class OneSignalNotificationService
     @activity = activity
   end
 
-  def notification
-    @notification ||= Feed::NotificationPresenter.new(@activity, @user)
+  def run!
+    notification = notify!
+    invalid_players = notification.dig('errors', 'invalid_player_ids')
+    OneSignalPlayer.where(player_id: invalid_players).delete_all if invalid_players
   end
 
-  def run!
-    players = OneSignalPlayer.where(platform: notification.setting.enabled_platforms, user: @user)
+  private
+
+  def notify!
+    platforms = OneSignalPlayer.values_at(*notification.setting.enabled_platforms)
+    players = OneSignalPlayer.where(platform: platforms, user: @user)
     return unless players.exists?
-    OneSignal::Notification.create(params: {
+    res = OneSignal::Notification.create(params: {
       app_id: app_id,
       include_player_ids: players.pluck(:player_id),
       contents: { en: notification.message }
     })
+    JSON.parse(res.body)
   end
 
-  private
+  def notification
+    @notification ||= Feed::NotificationPresenter.new(@activity, @user)
+  end
 
   def app_id
     ENV['ONE_SIGNAL_APP_ID']
