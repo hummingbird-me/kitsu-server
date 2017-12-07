479d83788b3b0af6927552d064cddc81863881fd
Peter Lejeck <peter.lejeck@gmail.com>
2017-12-06 21:19:07 -0800

    Add Slug to User and Group search

--- app/indices/algolia_groups_index.rb
+++ app/indices/algolia_groups_index.rb
@@ -1,7 +1,7 @@
 class AlgoliaGroupsIndex < BaseIndex
   self.index_name = 'groups'
 
-  attributes :name, :about, :locale, :tagline, :privacy, :nsfw
+  attributes :name, :about, :locale, :tagline, :privacy, :nsfw, :slug
   attribute :last_activity_at, frequency: 2.5
   attribute :members_count, frequency: 10
   attribute :avatar, format: AttachmentValueFormatter
--- app/indices/algolia_users_index.rb
+++ app/indices/algolia_users_index.rb
@@ -1,7 +1,7 @@
 class AlgoliaUsersIndex < BaseIndex
   self.index_name = 'users'
 
-  attributes :name, :past_names
+  attributes :name, :past_names, :slug
   attribute :followers_count, frequency: 2.5
   attribute :avatar, format: AttachmentValueFormatter
 end
