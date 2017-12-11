73f2016765016edce3378e3e240cf312eb9b29d1
Peter Lejeck <peter.lejeck@gmail.com>
2017-12-11 12:49:02 -0800

    Validate uniqueness of player_id

--- app/models/one_signal_player.rb
+++ app/models/one_signal_player.rb
@@ -24,6 +24,8 @@ class OneSignalPlayer < ApplicationRecord
   belongs_to :user
 
   enum platform: %i[web mobile]
+  
+  validates :player_id, uniqueness: true
 
   scope :enabled_for_setting, ->(setting) do
     where(platform: platforms & setting.enabled_platforms)
