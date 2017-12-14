ee6b07e879ff82adecdd5acce5d2aefb4b844ffb
Peter Lejeck <peter.lejeck@gmail.com>
2017-12-13 18:49:34 -0800

    Require player_id to be present

--- app/models/one_signal_player.rb
+++ app/models/one_signal_player.rb
@@ -25,7 +25,7 @@ class OneSignalPlayer < ApplicationRecord
 
   enum platform: %i[web mobile]
   
-  validates :player_id, uniqueness: true
+  validates :player_id, uniqueness: true, presence: true
 
   scope :enabled_for_setting, ->(setting) do
     where(platform: platforms & setting.enabled_platforms)
