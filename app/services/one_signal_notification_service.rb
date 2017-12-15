1fe9b00e4c71acd25545c21c0b58f7442e41eef0
Peter Lejeck <peter.lejeck@gmail.com>
2017-12-14 16:55:29 -0800

    Send to each platform separately

--- app/services/one_signal_notification_service.rb
+++ app/services/one_signal_notification_service.rb
@@ -6,7 +6,8 @@ class OneSignalNotificationService
 
   def run!
     notification = notify!
-    invalid_players = notification&.dig('errors', 'invalid_player_ids')
+    invalid_players = notification.map { |res| res&.dig('errors', 'invalid_player_ids') }
+    invalid_players = invalid_players.flatten.compact
     OneSignalPlayer.where(player_id: invalid_players).delete_all if invalid_players
   end
 
@@ -17,17 +18,36 @@ class OneSignalNotificationService
     platforms = OneSignalPlayer.platforms.values_at(*notification.setting.enabled_platforms)
     players = OneSignalPlayer.where(platform: platforms, user: @user)
     return unless players.exists?
-    player_ids = players.pluck(:player_id).uniq.reject(&:blank?)
-    res = OneSignal::Notification.create(params: {
-      app_id: app_id,
-      include_player_ids: player_ids,
-      headings: { en: notification.message },
-      contents: { en: notification.message },
-      ios_badgeType: 'Increase',
-      ios_badgeCount: 1,
-      url: notification.url
-    })
+    # load players, grouping by platform
+    player_ids = players.group(:platform).pluck(:platform, 'array_agg(player_id)').to_h
+    # fix platform enum keys
+    player_ids.transform_keys! { |k| OneSignalPlayer.platforms.invert[k].to_sym }
+    # Remove duplicate IDs and blanks
+    player_ids.transform_values! { |v| v.uniq.reject(&:blank?) }
+    # Notify them
+    player_ids.map { |platform, ids| notify_players(platform, ids) }
+  end
+
+  def notify_players(platform, players)
+    params = params_for(platform).merge(include_player_ids: players)
+    res = OneSignal::Notification.create(params: params)
     JSON.parse(res.body)
+  end
+
+  def params_for(platform)
+    params = {
+      app_id: app_id,
+      contents: { en: notification.message }
+    }
+    case platform
+    when :mobile
+      params[:ios_badgeType] = 'Increase'
+      params[:ios_badgeCount] = 1
+      params[:data] = notification.reference
+    when :web
+      params[:url] = notification.url
+    end
+    params
   end
 
   def notification
