80fe0c5d37b000759107b6f4046b5e70e675b7b9
Peter Lejeck <peter.lejeck@gmail.com>
2017-12-31 18:40:25 -0800

    Pull library entry diff code into separate service

--- app/callbacks/library_time_spent_callbacks.rb
+++ app/callbacks/library_time_spent_callbacks.rb
@@ -10,43 +10,7 @@ class LibraryTimeSpentCallbacks < Callbacks
     return unless record.progress_changed? || record.reconsume_count_changed?
     return unless record.media.respond_to?(:episodes)
 
-    record.time_spent += time_diff
-  end
-
-  private
-
-  # @return [Integer] the difference between the new progress and the previous
-  def progress_diff
-    return 0 unless record.progress_changed?
-    record.progress - record.progress_was
-  end
-
-  # @return [Integer] the difference between the new reconsume_count and the previous
-  def reconsume_diff
-    return 0 unless record.reconsume_count_changed?
-    record.reconsume_count - record.reconsume_count_was
-  end
-
-  # @return [Integer] the time difference caused by the progress change
-  def progress_time_diff
-    return 0 if progress_diff.zero?
-    action, range = if progress_diff.positive?
-                      [:+, (record.progress_was...record.progress)]
-                    elsif progress_diff.negative?
-                      [:-, ((record.progress + 1)..record.progress_was)]
-                    end
-    # Set the sign
-    0.send(action, record.media.episodes.for_range(range).sum(:length))
-  end
-
-  # @return [Integer] the time difference caused by the reconsume_count change
-  def reconsume_time_diff
-    return 0 if reconsume_diff.zero?
-    reconsume_diff * record.media.total_length
-  end
-
-  # @return [Integer] the time difference from this entire update
-  def time_diff
-    progress_time_diff + reconsume_time_diff
+    diff = LibraryEntryDiff.new(record)
+    record.time_spent += diff.time_diff
   end
 end
