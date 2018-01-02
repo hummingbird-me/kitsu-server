a3915b724dff1871418c8f90fb46a2eea621a58a
Peter Lejeck <peter.lejeck@gmail.com>
2018-01-01 19:42:10 -0800

    Pull stats code into LibraryStatCallbacks

---
+++ app/callbacks/library_stat_callbacks.rb
@@ -0,0 +1,38 @@
+# Hooks onto LibraryEntry and manages updating the stats
+class LibraryStatCallbacks < Callbacks
+  # @param klass [Class] the class to hook the callbacks for
+  def self.hook(klass)
+    klass.after_update(self)
+    klass.after_create(self)
+    klass.after_destroy(self)
+  end
+
+  def after_update
+    perform_for :update
+  end
+
+  def after_create
+    perform_for :create
+  end
+
+  def after_destroy
+    perform_for :destroy
+  end
+
+  private
+
+  def perform_for(action)
+    case record.kind
+    when :anime
+      perform 'Stat::AnimeCategoryBreakdown', action
+      perform 'Stat::AnimeAmountConsumed', action
+    when :manga
+      perform 'Stat::MangaCategoryBreakdown', action
+      perform 'Stat::MangaAmountConsumed', action
+    end
+  end
+
+  def perform(worker, action)
+    StatWorker.perform_async(worker, record.user, action, record)
+  end
+end
--- app/models/library_entry.rb
+++ app/models/library_entry.rb
@@ -161,6 +161,7 @@ class LibraryEntry < ApplicationRecord
   end
 
   LibraryTimeSpentCallbacks.hook(self)
+  LibraryStatCallbacks.hook(self)
 
   before_validation do
     # TEMPORARY: If media is set, copy it to kind_id, otherwise if kind_id is
@@ -238,50 +239,6 @@ class LibraryEntry < ApplicationRecord
 
   after_commit on: :create do
     MediaFollowUpdateWorker.perform_for_entry(self, :create, progress)
-  end
-
-  after_create do
-    Stat::LibraryCreateWorker.perform_async(
-      kind, user_id, id,
-      options: {
-        progress: progress,
-        progress_was: progress_was,
-        progress_changed: progress_changed?
-      }
-    )
-  end
-
-  after_update do
-    Stat::LibraryUpdateWorker.perform_async(
-      kind, user_id, id,
-      options: {
-        progress: progress,
-        progress_was: progress_was,
-        progress_changed: progress_changed?
-      }
-    )
-  end
-
-  after_destroy do
-    # HACK: because we object association we have to decrement before we lose access
-    # We should think about some type of absraction to handle all the stats though.
-    options = {
-      progress: progress,
-      progress_was: progress_was,
-      progress_changed: progress_changed?
-    }
-    case kind
-    when :anime
-      Stat::AnimeCategoryBreakdown.decrement(user, self)
-      Stat::AnimeAmountConsumed.decrement(user, self, options)
-      Stat::AnimeFavoriteYear.decrement(user, self)
-      Stat::AnimeActivityHistory.decrement(user)
-    when :manga
-      Stat::MangaCategoryBreakdown.decrement(user, self)
-      Stat::MangaAmountConsumed.decrement(user, self, options)
-      Stat::MangaFavoriteYear.decrement(user, self)
-      Stat::MangaActivityHistory.decrement(user)
-    end
   end
 
   after_commit(on: :create, if: :sync_to_mal?) do
