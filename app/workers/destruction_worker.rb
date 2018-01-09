07ff2e52982fb5560eed7ab1f00a5e9c71c66080
Peter Lejeck <peter.lejeck@gmail.com>
2018-01-08 22:27:08 -0800

    Handle RecordNotFound in DestructionWorker

--- app/workers/destruction_worker.rb
+++ app/workers/destruction_worker.rb
@@ -3,6 +3,7 @@ class DestructionWorker
   sidekiq_options queue: 'soon'
 
   def perform(klass, id)
-    klass.constantize.unscoped.find(id).destroy
+    klass.constantize.unscoped.find(id).destroy!
+  rescue ActiveRecord::RecordNotFound # rubocop:disable Lint/HandleExceptions
   end
 end
