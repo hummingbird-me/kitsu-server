aeb96ed6b888f10d2bf0b30c1ea2fa338b93c83c
Peter Lejeck <peter.lejeck@gmail.com>
2018-01-09 14:50:25 -0800

    Simplify CleanUpLibraryEvents migration since parts already ran

--- db/migrate/20171215020034_clean_up_library_events.rb
+++ db/migrate/20171215020034_clean_up_library_events.rb
@@ -1,28 +1,5 @@
 class CleanUpLibraryEvents < ActiveRecord::Migration
-  REMOVE_COLUMNS = %w[
-    id nsfw reconsuming
-    anime_id manga_id drama_id media_type media_id user_id
-    created_at updated_at progressed_at started_at finished_at
-  ].freeze
-
-  class LibraryEvent < ActiveRecord::Base
-    enum event: {
-      progressed: 0,
-      updated: 1,
-      reacted: 2,
-      rated: 3
-    }
-  end
-
   def up
-    LibraryEvent.find_each do |le|
-      le.update_column(:changed_data, le.changed_data.except(*REMOVE_COLUMNS))
-    end
-    LibraryEvent.where("changed_data = '{}'").delete_all
-    LibraryEvent.update_all(event: -1)
-    LibraryEvent.where("changed_data ? 'progress'").update_all(event: 0)
-    LibraryEvent.where("changed_data ? 'status'").update_all(event: 1)
-    LibraryEvent.where("changed_data ? 'rating'").update_all(event: 3)
     rename_column :library_events, :event, :kind
   end
 
