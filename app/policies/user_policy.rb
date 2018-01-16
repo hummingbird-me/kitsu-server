ab0e35915b4b1e13c0a83ba71ae08eb06e8fe97f
Peter Lejeck <peter.lejeck@gmail.com>
2018-01-15 19:39:42 -0800

    Revert changes in UserPolicy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -4,7 +4,7 @@ class UserPolicy < ApplicationPolicy
   end
 
   def update?
-    user&.id == record.id || is_admin?
+    user == record || is_admin?
   end
   alias_method :destroy?, :update?
 
@@ -23,7 +23,7 @@ class UserPolicy < ApplicationPolicy
   end
 
   def visible_attributes(all)
-    if record.id == user&.id
+    if record == user
       all
     else
       all - %i[email password confirmed previous_email language time_zone country share_to_global
