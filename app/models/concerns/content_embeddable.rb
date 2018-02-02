91264cf1a4ff0a55023f9800ff00928e487d2ee4
Emma Lejeck <nuck@kitsu.io>
2018-02-01 18:58:01 -0800

    Try to annotate posts with embed data on initialize too

--- app/models/concerns/content_embeddable.rb
+++ app/models/concerns/content_embeddable.rb
@@ -8,10 +8,18 @@ module ContentEmbeddable
 
       attr_accessor url_attr
 
-      before_validation do
+      before_validation if: -> {
+        send("#{url_attr}_changed?") || send("#{content_attr}_changed?")
+      } do
         processed = send(processed_attr)
-        embed_url = send(url_attr)
-        self.embed = embed_url ? EmbedService.new(embed_url).as_json : processed[:embed]
+        embed_url = send(url_attr) || processed[:embeddable_links].first
+        self.embed = EmbedService.new(embed_url).as_json
+      end
+
+      after_find unless: :embed? do
+        processed = send(processed_attr)
+        embed_url = send(url_attr) || processed[:embeddable_links].first
+        update(embed: EmbedService.new(embed_url).as_json)
       end
     end
   end
