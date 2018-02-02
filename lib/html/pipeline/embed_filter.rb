2da8c88215d7b1b5bcfe4f410a960ab156d28f59
Emma Lejeck <nuck@kitsu.io>
2018-02-01 21:20:37 -0800

    Fix onebox embedding

--- lib/html/pipeline/embed_filter.rb
+++ lib/html/pipeline/embed_filter.rb
@@ -2,7 +2,7 @@ module HTML
   class Pipeline
     class EmbedFilter < HTML::Pipeline::Filter
       def call
-        result[:embeddable_links] = doc.css('a.autolink, onebox a').map { |a| a['href'] }
+        result[:embeddable_links] = doc.css('a.autolink, .onebox a').map { |a| a['href'] }
         doc
       end
     end
