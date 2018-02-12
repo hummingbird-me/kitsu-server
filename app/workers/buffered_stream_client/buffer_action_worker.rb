f990ff59d4d94405232356e4d2b83c12c30b26b2
Emma Lejeck <nuck@kitsu.io>
2018-02-12 14:51:25 -0800

    Add metrics to Stream stuff

--- app/workers/buffered_stream_client/buffer_action_worker.rb
+++ app/workers/buffered_stream_client/buffer_action_worker.rb
@@ -4,6 +4,7 @@ class BufferedStreamClient
     sidekiq_options queue: 'soon'
 
     def perform(group, id, method, parameters)
+      Librato.increment("getstream.#{method}.total", feed_group: group)
       StreamRails.client.feed(group, id).public_send(method, *parameters)
     end
   end
--- lib/buffered_stream_client/follow_buffer.rb
+++ lib/buffered_stream_client/follow_buffer.rb
@@ -8,15 +8,29 @@ class BufferedStreamClient
         queue.each do |scrollback, follows|
           # If there's not many, send them individually to avoid triggering rate limits
           if follows.count <= BULK_THRESHOLD
+            increment_metrics(follows, bulk: false, scrollback: scrollback)
             follows.each do |follow|
               group, id = follow[:source].split(':')
               feed = client.feed(group, id)
               feed.follow(follow[:target], activity_copy_limit: scrollback)
             end
           else
+            increment_metrics(follows, bulk: true, scrollback: scrollback)
             client.follow_many(*follows, activity_copy_limit: scrollback)
           end
         end
+      end
+    end
+
+    private
+
+    def increment_metrics(follows, tags = {})
+      Librato.increment('getstream.follow.sync', tags)
+      follows.each do |follow|
+        Librato.increment('getstream.follow.total', tags: {
+          source_group: follow[:source]&.split(':')&.first,
+          target_group: follow[:target]&.split(':')&.first
+        }.merge(tags))
       end
     end
   end
