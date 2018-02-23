4110340bbce9fc8bf6f5ef3d69e51f59bb276df8
Emma Lejeck <nuck@kitsu.io>
2018-02-22 17:58:01 -0800

    Add ProfileFeed routes to FeedRouter, organize all FeedRouter routes

--- app/services/feed_router.rb
+++ app/services/feed_router.rb
@@ -2,24 +2,37 @@
 class FeedRouter
   def self.route(group, id)
     case group
+    # Media Pages
     when 'media', 'media_aggr'
       case id
       when /Anime-(\d+)/ then AnimeFeed.new($1)
       when /Manga-(\d+)/ then MangaFeed.new($1)
       end
+    # Media Unit Pages
+    when 'episode' then EpisodeFeed.new(id)
+    when 'chapter' then ChapterFeed.new(id)
+
+    # Timelines
+    when 'global' then GlobalFeed.new
+    when 'timeline' then TimelineFeed.new(id)
     when 'interest_timeline'
       case id
       when /(\d+)-Anime/ then AnimeTimelineFeed.new($1)
       when /(\d+)-Manga/ then MangaTimelineFeed.new($1)
       end
-    when 'timeline' then TimelineFeed.new(id)
+
+    # Profiles
+    when 'user', 'user_aggr' then ProfileFeed.new(id)
+
+    # Notifications
     when 'notifications' then NotificationsFeed.new(id)
-    when 'global' then GlobalFeed.new
     when 'site_announcements' then SiteAnnouncementsFeed.new(id)
+
+    # Groups
     when 'group', 'group_aggr' then GroupFeed.new(id)
+
+    # Reports
     when 'reports' then ReportsFeed.new(id)
-    when 'episode' then EpisodeFeed.new(id)
-    when 'chapter' then ChapterFeed.new(id)
     end
   end
 end
