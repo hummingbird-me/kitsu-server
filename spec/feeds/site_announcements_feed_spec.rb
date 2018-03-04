cb86b47c7315557c8fe491a62ceb34826da79477
Emma Lejeck <nuck@kitsu.io>
2018-03-03 21:34:46 -0800

    Add scrollback: 1 to SiteAnnouncementsFeedSpec expectation

--- spec/feeds/site_announcements_feed_spec.rb
+++ spec/feeds/site_announcements_feed_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe SiteAnnouncementsFeed, type: :model do
   describe '#setup!' do
     it 'should make the feed follow the global' do
       feed = described_class.new('5554')
-      expect(feed).to receive(:follow).with(SiteAnnouncementsGlobalFeed.new)
+      expect(feed).to receive(:follow).with(SiteAnnouncementsGlobalFeed.new, scrollback: 1)
         .once
       feed.setup!
     end
