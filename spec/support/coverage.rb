452be8d4ebe93df4c2a8ddcb69b7348cb86465da
Emma Lejeck <nuck@kitsu.io>
2018-03-03 22:23:23 -0800

    Write coverage in lcov format too

--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -1,37 +1,38 @@
 require 'simplecov'
+require 'simplecov-lcov'
 
 module SimpleCov
   module Formatter
     class MergedFormatter
       def format(result)
         SimpleCov::Formatter::HTMLFormatter.new.format(result)
-        CodeClimate::TestReporter::Formatter.new.format(result)
+        SimpleCov::Formatter::LcovFormatter.new.format(result)
+        if ENV['CODECLIMATE_REPO_TOKEN']
+          require 'codeclimate-test-reporter'
+          CodeClimate::TestReporter::Formatter.new.format(result)
+        end
       end
     end
   end
 end
 
-if ENV['CODECLIMATE_REPO_TOKEN']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
-  SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
-else
-  SimpleCov.start do
-    add_filter '/spec/'
-    add_filter '/config/'
-    add_filter '/db/'
-    add_filter '/vendor/'
+CodeClimate::TestReporter.start if ENV['CODECLIMATE_REPO_TOKEN']
 
-    add_group 'Controllers', 'app/controllers'
-    add_group 'Models', 'app/models'
-    add_group 'Mailers', 'app/mailers'
-    add_group 'Services', 'app/services'
-    add_group 'Workers', 'app/workers'
-    add_group 'Serializers', 'app/serializers'
-    add_group 'Policies', 'app/policies'
-    add_group 'Libs', 'lib/'
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/config/'
+  add_filter '/db/'
+  add_filter '/vendor/'
 
-    track_files '{app,lib}/**/*.rb'
-  end
-  SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Mailers', 'app/mailers'
+  add_group 'Services', 'app/services'
+  add_group 'Workers', 'app/workers'
+  add_group 'Serializers', 'app/serializers'
+  add_group 'Policies', 'app/policies'
+  add_group 'Libs', 'lib/'
+
+  track_files '{app,lib}/**/*.rb'
 end
+SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
