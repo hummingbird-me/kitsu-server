04a54750325c16f147008d4ec4af6abf4a34308b
Emma Lejeck <nuck@kitsu.io>
2018-03-13 15:21:05 -0700

    UnitFeed.follows_for_progress(nil) should return empty array

--- app/feeds/chapter_feed.rb
+++ app/feeds/chapter_feed.rb
@@ -4,6 +4,7 @@ class ChapterFeed < Feed
   end
 
   def self.follows_for_progress(media, progress, limit: 3)
+    return [] unless progress
     chapter_ids = media.chapters.for_progress(progress).limit(limit).order(number: :desc).ids
     chapter_ids.map { |id| new(id) }
   end
--- app/feeds/episode_feed.rb
+++ app/feeds/episode_feed.rb
@@ -4,6 +4,7 @@ class EpisodeFeed < Feed
   end
 
   def self.follows_for_progress(media, progress, limit: 3)
+    return [] unless progress
     episode_ids = media.episodes.for_progress(progress).limit(limit).order(number: :desc).ids
     episode_ids.map { |id| new(id) }
   end
