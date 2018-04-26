9b205c08d80435e20800347bbf31646f69f8c981
Emma Lejeck <nuck@kitsu.io>
2018-04-25 22:28:10 -0700

    Flesh out MyAnimeListScraper::MediaPage with #import and #call methods

--- app/scrapers/my_anime_list_scraper/media_page.rb
+++ app/scrapers/my_anime_list_scraper/media_page.rb
@@ -1,15 +1,38 @@
 class MyAnimeListScraper
   class MediaPage < MyAnimeListScraper
+    using NodeSetContentMethod
     include SidebarKeyValueParser
 
+    def call
+      super
+      import.save!
+      scrape_async "#{@url}/characters"
+      scrape_async "#{@url}/pics"
+    end
+
+    def import
+      media.synopsis ||= "#{synopsis}\n\n#{background}"
+      media.genres += genres
+      media.subtype ||= subtype
+      media.poster_image ||= poster_image
+    end
+
+    def poster_image
+      URI(sidebar.at_css('[itemprop="image"]')['src'].sub(/(\.[a-z]+)\z/i, 'l\1'))
+    end
+
+    def subtype
+      information['Type'].content.strip.underscore.to_sym
+    end
+
     def synopsis
-      text = main_sections['Synopsis'].map(&:content).join
+      text = main_sections['Synopsis'].content
       return if EMPTY_TEXT =~ text
       clean_text(text)
     end
 
     def background
-      text = main_sections['Background'].map(&:content).join
+      text = main_sections['Background'].content
       return if EMPTY_TEXT =~ text
       clean_text(text)
     end
