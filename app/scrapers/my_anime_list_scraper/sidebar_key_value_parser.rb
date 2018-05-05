adf184b43b244c0b9cd9291aca302891b2cc0235
Emma Lejeck <nuck@kitsu.io>
2018-05-04 22:35:30 -0700

    Improve SidebarKeyValueParser to handle lack of .spaceit_pad

--- app/scrapers/my_anime_list_scraper/sidebar_key_value_parser.rb
+++ app/scrapers/my_anime_list_scraper/sidebar_key_value_parser.rb
@@ -4,14 +4,18 @@ class MyAnimeListScraper
 
     def hash_for_sidebar_section(section_name)
       # For each top-level <div> tag in the section
-      sidebar_sections[section_name].css('div.spaceit_pad').each_with_object({}) do |row, out|
+      sidebar_sections[section_name].css('.dark_text').each_with_object({}) do |row, out|
         # Extract the key
-        key = row.at_css('.dark_text').content.delete(':').strip
+        key = row.content.delete(':').strip
 
-        # Build a NodeSet for our value
         out[key] = Nokogiri::XML::NodeSet.new(page)
-        row.children.each do |node|
-          out[key] << node unless node['class'] == 'dark_text'
+        curr = row
+        loop do
+          curr = curr.next
+          break unless curr
+          break if curr['class']&.include?('dark_text')
+          break if curr.at_css('.dark_text').present?
+          out[key] << curr
         end
       end
     end
