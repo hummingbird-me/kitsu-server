174814cc0c129ff2074790718b69a71219a37298
Emma Lejeck <nuck@kitsu.io>
2018-05-25 15:04:17 -0700

    Add Scrape model for orchestration

---
+++ spec/models/scrape_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Scrape, type: :model do
+  it { should define_enum_for(:status).with(%i[queued running failed completed]) }
+  it { should belong_to(:parent).class_name('Scrape') }
+  it do
+    should have_many(:children).class_name('Scrape').with_foreign_key(:parent_id)
+                               .dependent(:destroy)
+  end
+end
