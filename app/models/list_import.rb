0caac19c1532fc2cfeee59fc6f2416f59f05fcc9
Emma Lejeck <nuck@kitsu.io>
2018-05-29 15:28:22 -0700

    Correctly handle media without progress limits

--- app/models/list_import.rb
+++ app/models/list_import.rb
@@ -73,7 +73,9 @@ class ListImport < ApplicationRecord
         le = LibraryEntry.where(user_id: user.id, media: media).first_or_initialize
         le.imported = true
         le = merged_entry(le, data)
-        le.progress = [le.progress, media.progress_limit - 1].min
+        if media.progress_limit && le.progress >= media.progress_limit
+          le.progress = media.progress_limit - 1
+        end
         le.save! unless le.status.nil?
         yield({ status: :running, total: total, progress: index + 1 })
       end
