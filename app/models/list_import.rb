b8b0bb7182a783f6c7661983bad611599e8d8858
Emma Lejeck <nuck@kitsu.io>
2018-05-30 19:12:20 -0700

    Remove duplicate progress limit handling in list import

--- app/models/list_import.rb
+++ app/models/list_import.rb
@@ -73,9 +73,6 @@ class ListImport < ApplicationRecord
         le = LibraryEntry.where(user_id: user.id, media: media).first_or_initialize
         le.imported = true
         le = merged_entry(le, data)
-        if media.progress_limit && le.progress >= media.progress_limit
-          le.progress = media.progress_limit - 1
-        end
         le.save! unless le.status.nil?
         yield({ status: :running, total: total, progress: index + 1 })
       end
