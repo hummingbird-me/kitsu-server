04d66a475ccac2e9d21356eb01d7628995d11b74
Emma Lejeck <nuck@kitsu.io>
2018-06-19 19:11:16 -0700

    Add ProSubscription::StripeSubscription subclass

---
+++ app/models/pro_subscription/stripe_subscription.rb
@@ -0,0 +1,18 @@
+class ProSubscription
+  class StripeSubscription < ProSubscription
+    def subscription
+      @subscription ||= Stripe::Subscription.retrieve(billing_id)
+    end
+
+    after_destroy do
+      subscription.delete
+    end
+
+    after_create do
+      self.billing_id = Stripe::Subscription.create(
+        customer: user.stripe_customer.id,
+        items: [{ plan: 'pro-yearly' }]
+      ).id
+    end
+  end
+end
