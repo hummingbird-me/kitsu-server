4aa2533e3f2248101c6ed67766838784b187348f
Emma Lejeck <nuck@kitsu.io>
2018-06-20 22:03:02 -0700

    Add spec for StripeRenewalService

---
+++ spec/services/stripe_renewal_service_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe StripeRenewalService do
+  let(:stripe_mock) { StripeMock.create_test_helper }
+
+  before do
+    stripe_mock.create_plan(id: 'pro-yearly')
+  end
+
+  it "should update the user's pro subscription for the period of the invoice" do
+    user = create(:user, pro_expires_at: Time.now)
+    user.stripe_customer.save(source: stripe_mock.generate_card_token)
+    sub = ProSubscription::StripeSubscription.create!(user: user)
+    invoice = Stripe::Invoice.create(
+      subscription: sub.billing_id,
+      period_start: Time.now,
+      period_end: 30.days.from_now
+    )
+    expect {
+      StripeRenewalService.new(invoice).call
+    }.to(change { user.reload.pro_expires_at })
+    expect(user.reload.pro_expires_at).to be > 29.days.from_now
+  end
+end
