08d45dd88c6c53ac14d819ed9ab2014030c19eec
Emma Lejeck <nuck@kitsu.io>
2018-06-25 13:21:28 -0700

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180504045902) do
+ActiveRecord::Schema.define(version: 20180524205232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1368,6 +1368,17 @@ ActiveRecord::Schema.define(version: 20180504045902) do
 
   add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
   add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
+
+  create_table "scrapes", force: :cascade do |t|
+    t.text     "target_url",               null: false
+    t.string   "scraper_name"
+    t.integer  "depth",        default: 0, null: false
+    t.integer  "max_depth",    default: 0, null: false
+    t.integer  "parent_id"
+    t.integer  "status",       default: 0, null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "site_announcements", force: :cascade do |t|
     t.integer  "user_id",     null: false
