c8b6eb31f6ee3c5414652c7083a9e48703bb5771
Emma Lejeck <nuck@kitsu.io>
2018-06-25 13:02:47 -0700

    Fix ListImport progress updating

--- app/models/list_import.rb
+++ app/models/list_import.rb
@@ -97,7 +97,8 @@ class ListImport < ApplicationRecord
 
     apply do |info|
       # Apply every [frequency] updates unless the status is not :running
-      if %i[running partially_failed].include?(info[:status]) || (info[:progress] % frequency).zero?
+      if !%i[running partially_failed].include?(info[:status]) ||
+          (info[:progress] % frequency).zero?
         update info
         yield info if block_given?
       end
