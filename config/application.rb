07cbaee2b8017594d7f2c62db69b0dbb466e8cfb
Emma Lejeck <nuck@kitsu.io>
2018-06-30 15:53:31 -0700

    Fix eager load path for lib

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Kitsu
     # Include all concern directories in app/*/concerns
     concern_dirs = Dir['app/*/concerns'].map { |d| File.expand_path(d) }
     config.eager_load_paths += concern_dirs
-    config.eager_load_paths += "#{Rails.root}/lib"
+    config.eager_load_paths += %W[#{Rails.root}/lib]
     # Rip out any non-unique entries
     config.eager_load_paths.uniq!
 
