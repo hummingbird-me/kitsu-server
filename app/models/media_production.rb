fe714a8c49dd0fb92a78d269ccf14c5300bccce7
Emma Lejeck <nuck@kitsu.io>
2018-07-20 21:02:04 -0700

    Use Producer model for Company right now

--- app/models/media_production.rb
+++ app/models/media_production.rb
@@ -2,5 +2,5 @@ class MediaProduction < ApplicationRecord
   enum role: %i[producer licensor studio serialization]
 
   belongs_to :media, polymorphic: true, required: true
-  belongs_to :company, required: true
+  belongs_to :company, class_name: 'Producer', required: true
 end
