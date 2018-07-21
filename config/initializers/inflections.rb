39cf7e747c23b6bc7931621a943713e7c7ae5431
Emma Lejeck <nuck@kitsu.io>
2018-07-20 17:56:43 -0700

    Mark MediaStaff as uncountable

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ActiveSupport::Inflector.inflections(:en) do |inflect|
-  inflect.uncountable %w[anime manga media anime_staff manga_staff drama_staff]
+  inflect.uncountable %w[anime manga media anime_staff manga_staff drama_staff media_staff]
   inflect.acronym 'XML'
   inflect.acronym 'SSO'
   inflect.acronym 'STI'
