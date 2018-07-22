e2a7ec7ec6351f99b1c41235308325fe994a2343
Emma Lejeck <nuck@kitsu.io>
2018-07-22 15:39:18 -0700

    Copy MAL ID to Mappings

---
+++ db/migrate/20180722222944_copy_mal_id_to_mappings.rb
@@ -0,0 +1,41 @@
+class CopyMALIdToMappings < ActiveRecord::Migration
+  def change
+    execute <<-SQL.squish
+      INSERT INTO mappings (
+        external_site,
+        external_id,
+        item_id,
+        item_type,
+        created_at,
+        updated_at
+      ) SELECT
+        'myanimelist/character' AS external_site,
+        mal_id AS external_id,
+        id AS item_id,
+        'Character' AS item_type,
+        created_at,
+        updated_at
+      FROM characters
+      WHERE mal_id IS NOT NULL
+    SQL
+
+    execute <<-SQL.squish
+      INSERT INTO mappings (
+        external_site,
+        external_id,
+        item_id,
+        item_type,
+        created_at,
+        updated_at
+      ) SELECT
+        'myanimelist/people' AS external_site,
+        mal_id AS external_id,
+        id AS item_id,
+        'Person' AS item_type,
+        created_at,
+        updated_at
+      FROM people
+      WHERE mal_id IS NOT NULL
+    SQL
+  end
+end
