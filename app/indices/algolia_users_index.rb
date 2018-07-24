bb7a8d8ebd7ffd7274561112fa6696174b9e3a00
Emma Lejeck <nuck@kitsu.io>
2018-07-23 22:33:19 -0700

    Serialize pro expiration and user title to Algolia

--- app/indices/algolia_users_index.rb
+++ app/indices/algolia_users_index.rb
@@ -1,7 +1,7 @@
 class AlgoliaUsersIndex < BaseIndex
   self.index_name = 'users'
 
-  attributes :name, :past_names, :slug
+  attributes :name, :past_names, :slug, :title, :pro_expires_at
   attribute :followers_count, frequency: 2.5
   attribute :avatar, format: AttachmentValueFormatter
 end
