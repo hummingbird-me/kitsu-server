d13e5678553fe05d76863fa6149effc41065fa58
Emma Lejeck <nuck@kitsu.io>
2018-07-25 21:22:34 -0700

    USe #remove instead of #delete

--- app/resources/character_resource.rb
+++ app/resources/character_resource.rb
@@ -16,7 +16,7 @@ class CharacterResource < BaseResource
 
   def description
     html = Nokogiri::HTML.fragment(_model.description)
-    html.css('data, source').delete
+    html.css('data, source').remove
     html.to_html
   end
 
