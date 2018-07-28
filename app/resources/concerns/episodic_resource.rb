dc0f17798a80fe862b16fb7be2928b547c645280
Emma Lejeck <nuck@kitsu.io>
2018-07-28 08:38:06 -0700

    Use minutes for total_length and episode_length in API

--- app/resources/concerns/episodic_resource.rb
+++ app/resources/concerns/episodic_resource.rb
@@ -2,7 +2,7 @@ module EpisodicResource
   extend ActiveSupport::Concern
 
   included do
-    attributes :episode_count, :episode_length
+    attributes :episode_count, :episode_length, :total_length
 
     query :episode_count, MediaResource::NUMERIC_QUERY
     query :episode_length, MediaResource::NUMERIC_QUERY
@@ -10,7 +10,11 @@ module EpisodicResource
     has_many :episodes
   end
 
-  def length
-    _model.length / 60 if _model.length
+  def episode_length
+    _model.episode_length / 60 if _model.episode_length
+  end
+
+  def total_length
+    _model.total_length / 60 if _model.total_length
   end
 end
