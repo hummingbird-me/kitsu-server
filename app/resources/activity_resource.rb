3cbab6bd5df98655e43d394ef0593b1218022f88
Emma Lejeck <nuck@kitsu.io>
2018-07-29 11:49:54 -0700

    Make Activity#actor polymorphic since it can be Media now
    Fixes SERVER-7FR

--- app/resources/activity_resource.rb
+++ app/resources/activity_resource.rb
@@ -6,7 +6,7 @@ class ActivityResource < BaseResource
   attributes :status, :verb, :time, :stream_id, :foreign_id, :rating,
     :progress, :reply_to_type, :reply_to_user, :nineteen_scale, :mentioned_users, :reason
 
-  has_one :actor, class_name: 'User', eager_load_on_include: false
+  has_one :actor, polymorphic: true, eager_load_on_include: false
   has_one :unit, polymorphic: true, eager_load_on_include: false
   has_one :media, polymorphic: true, eager_load_on_include: false
   has_one :subject, polymorphic: true, eager_load_on_include: false
