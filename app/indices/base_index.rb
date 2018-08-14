47fb3534dbf27f8e3c7b5d90a85c511c366bfe83
Emma Lejeck <nuck@kitsu.io>
2018-08-13 19:58:44 -0700

    Make Algolia search code more robust against missing results

--- app/indices/base_index.rb
+++ app/indices/base_index.rb
@@ -67,9 +67,9 @@ class BaseIndex
         acc[value['kind']] << value['id']
       end
       results = result_ids.map { |kind, ids|
-        [kind, kind.classify.safe_constantize.find(ids).index_by(&:id)]
+        [kind, kind.classify.safe_constantize.find_by(id: ids).index_by(&:id)]
       }.to_h
-      hits.map { |hit| results.dig(hit['kind'], hit['id']) }
+      hits.map { |hit| results.dig(hit['kind'], hit['id']) }.compact
     end
 
     def index
