e1ffc535d8e539e6ed0d8229a31164c39317ee46
Emma Lejeck <nuck@kitsu.io>
2018-08-27 16:50:08 -0700

    Improve ProSubscriptionController error messages

--- app/controllers/pro_subscription_controller.rb
+++ app/controllers/pro_subscription_controller.rb
@@ -10,6 +10,12 @@ class ProSubscriptionController < ApplicationController
     customer.save
     ProSubscription::StripeSubscription.create!(user: user)
     render status: 200
+  rescue Stripe::CardError
+    render_jsonapi_error 400, 'Invalid card'
+  rescue Stripe::APIConnectionError
+    render_jsonapi_error 502, 'Failed to connect to credit card processor'
+  rescue Stripe::StripeError
+    render_jsonapi_error 500, 'Something went wrong with our credit card processor'
   end
 
   def ios
@@ -17,22 +23,22 @@ class ProSubscriptionController < ApplicationController
     ProSubscription::AppleSubscription.create!(user: user, billing_id: receipt.billing_id)
     render status: 200
   rescue AppleReceiptService::Error::ServerUnavailable, AppleReceiptService::Error::InternalError
-    render status: 502
+    render_jsonapi_error 502, 'Failed to connect to Apple App Store'
   rescue AppleReceiptService::Error::MalformedReceipt,
          AppleReceiptService::Error::TestReceiptOnProduction,
          AppleReceiptService::Error::ProductionReceiptOnTest
-    render status: 400
+    render_jsonapi_error 400, 'Receipt data was malformed'
   rescue AppleReceiptService::Error::InvalidSecret, AppleReceiptService::Error::InvalidJSON
-    render status: 500
+    render_jsonapi_error 500, 'Something went wrong when validating with the Apple App Store'
   rescue AppleReceiptService::Error
-    render status: 402
+    render_jsonapi_error 402, 'An unknown error occurred'
   end
 
   def destroy
     case user.pro_subscription.billing_service
     when :apple_ios
       # We cannot cancel an iOS subscription — cancellation must be done through Apple or in-app
-      render status: 400
+      render_jsonapi_error 400, 'Cannot cancel an iOS subscription outside of the App Store'
     when :stripe
       user.pro_subscription.destroy!
       render status: 200
