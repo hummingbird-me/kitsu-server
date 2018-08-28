8b3e5a5f0ea602c5af66ba31663c16172f9fae94
Emma Lejeck <nuck@kitsu.io>
2018-08-27 19:49:31 -0700

    Add endpoint for sending PRO gifts to other users

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ Rails.application.routes.draw do
       post '/pro-subscription/ios', to: 'pro_subscription#ios'
       delete '/pro-subscription', to: 'pro_subscription#destroy'
       get '/pro-subscription', to: 'pro_subscription#show'
+      post '/pro-gifts', to: 'pro_gifts#create'
 
       ### Library
       get '/library-entries/_issues', to: 'library_entries#issues'
