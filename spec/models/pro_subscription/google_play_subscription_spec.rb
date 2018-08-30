ecf2389d55ad479877bac3db8b281aaaecdb1616
Emma Lejeck <nuck@kitsu.io>
2018-08-29 20:32:25 -0700

    Check that GooglePlaySubscription cancels the sub on destroy

--- spec/models/pro_subscription/google_play_subscription_spec.rb
+++ spec/models/pro_subscription/google_play_subscription_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe ProSubscription::GooglePlaySubscription, type: :model do
       ProSubscription::GooglePlaySubscription.new.billing_service
     end
   end
+
+  it 'should cancel the subscription on destroy' do
+    sub = ProSubscription::GooglePlaySubscription.create
+
+    service = instance_double('GooglePlaySubscriptionService')
+    allow(sub).to receive(:service).and_return(service)
+    expect(service).to receive(:cancel).once
+
+    sub.destroy
+  end
 end
