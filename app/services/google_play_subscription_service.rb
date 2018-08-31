1ab5580c2fca5a9428c3b4421f778a10f4080e68
Emma Lejeck <nuck@kitsu.io>
2018-08-31 13:09:36 -0700

    Fix complaints from Hound

--- app/services/google_play_subscription_service.rb
+++ app/services/google_play_subscription_service.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'google/apis/androidpublisher_v3'
 
 class GooglePlaySubscriptionService
--- spec/support/stubbed_apple_receipt_verification.rb
+++ spec/support/stubbed_apple_receipt_verification.rb
@@ -1,9 +1,6 @@
 RSpec.shared_context 'Stubbed Apple Receipt Verification' do
   let(:verify_url) { 'http://example.com/receipt-verify' }
-  before {
-    AppleReceiptService
-    stub_const('AppleReceiptService::VERIFICATION_URL', verify_url)
-  }
+  before { stub_const('AppleReceiptService::VERIFICATION_URL', verify_url) }
   subject { described_class.new('fake_receipt') }
 
   def stub_receipt_verification(latest_receipt_info: {}, status: 0)
