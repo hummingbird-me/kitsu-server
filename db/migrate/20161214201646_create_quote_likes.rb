e5c24ea7d81e17fe9e71c15cb94f7f721a4d2340
Nuck <peter.lejeck@gmail.com>
2018-10-23 16:56:25 -0700

    Remove foreign key from quote likes

--- db/migrate/20161214201646_create_quote_likes.rb
+++ db/migrate/20161214201646_create_quote_likes.rb
@@ -1,8 +1,8 @@
 class CreateQuoteLikes < ActiveRecord::Migration
   def change
     create_table :quote_likes do |t|
-      t.references :user, index: true, foreign_key: true, null: false
-      t.references :quote, index: true, foreign_key: true, null: false
+      t.references :user, index: true, null: false
+      t.references :quote, index: true, null: false
 
       t.timestamps null: false
     end
