fc79e364fbed763fd844b9ac10ba33bdee0c3633
Emma Lejeck <nuck@kitsu.io>
2018-10-26 11:58:57 -0700

    Fix CharacterVoice#media_character in JSON:API

--- app/resources/character_voice_resource.rb
+++ app/resources/character_voice_resource.rb
@@ -2,7 +2,7 @@ class CharacterVoiceResource < BaseResource
   immutable
   attribute :locale
 
-  has_one :media, polymorphic: true
+  has_one :media_character
   has_one :person
   has_one :licensor
 end
