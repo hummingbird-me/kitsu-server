5ede5b43e77606955a703df373ee4f5eeeb28760
Emma Lejeck <nuck@kitsu.io>
2018-11-05 12:13:17 -0800

    Allow gifts to be sent for either 1 month or 1 year

--- app/controllers/pro_gifts_controller.rb
+++ app/controllers/pro_gifts_controller.rb
@@ -5,18 +5,18 @@ class ProGiftsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    to = User.find(params[:to])
-    message = params[:message].presence
+    gift.validate!
 
     case params[:service].downcase
     when 'stripe'
-      StripeGiftService.new(from: user, to: to, token: params[:token]).call
-      ProGiftService.new(from: user, to: to, message: message).call
+      StripeGiftService.new(from: user, to: to, token: params[:token], length: params[:length]).call
     else
       return render_jsonapi_error 400, 'Unknown payment service'
     end
 
-    render status: 200
+    gift.send
+
+    render status: 200, json: gift
   rescue ProGiftService::InvalidSelfGift
     render_jsonapi_error 400, 'Cannot gift to self'
   rescue ProGiftService::RecipientIsPro
@@ -26,6 +26,22 @@ class ProGiftsController < ApplicationController
   end
 
   private
+
+  def gift
+    @gift ||= ProGiftService.new(from: user, to: to, message: message, length: params[:length])
+  end
+
+  def to
+    @to ||= User.find(params[:to])
+  end
+
+  def message
+    @message ||= params[:message].presence
+  end
+
+  def length
+    @length ||= params[:length].to_sym
+  end
 
   def check_feature_flag!
     render status: 404 unless Flipper[:pro_gifts].enabled?(user)
--- app/services/stripe_gift_service.rb
+++ app/services/stripe_gift_service.rb
@@ -1,19 +1,25 @@
 class StripeGiftService
-  def initialize(from:, to:, token:)
+  PRICES = {
+    month: 2_99,
+    year: 29_99
+  }.freeze
+
+  def initialize(from:, to:, token:, length:)
     @from = from
     @to = to
     @token = token
+    @length = length
   end
 
   def call
     Stripe::Charge.create(
-      # 1 year of PRO
-      amount: 29_99,
+      amount: PRICES[length],
       currency: 'usd',
       description: 'Kitsu Pro Gift',
       statement_descriptor: 'Kitsu Pro Gift',
       source: @token,
       metadata: {
+        length: length.to_s,
         gift: true,
         to: @to.id,
         from: @from.id
