fb145c388c9485a6b84bcce5bd44afe89c57e1aa
Emma Lejeck <nuck@kitsu.io>
2018-11-08 20:32:51 -0800

    Fix MAL Sync LibraryRemover for items not in MAL library
    Fixes SERVER-616

--- app/services/list_sync/my_anime_list/library_remover.rb
+++ app/services/list_sync/my_anime_list/library_remover.rb
@@ -5,12 +5,16 @@ module ListSync
 
       def run!
         check_authentication!
+
+        return if delete_form.nil? && edit_page.uri.to_s.include?('add')
+
         copy_csrf_token_into(delete_form)
         results_page = delete_form.click_button
 
         if results_page.at_css('.badresult')
           raise ListSync::RemoteError, results_page.at_css('.badresult').text
         end
+
         true
       end
 
