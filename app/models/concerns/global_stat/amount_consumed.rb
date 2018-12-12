3ae11d365d0f835111ac49292695677d488734b3
Emma Lejeck <nuck@kitsu.io>
2018-12-12 15:05:10 -0800

    Limit GlobalStat calculations to instances greater than zero

--- app/models/concerns/global_stat/amount_consumed.rb
+++ app/models/concerns/global_stat/amount_consumed.rb
@@ -25,14 +25,18 @@ class GlobalStat < ApplicationRecord
 
     def percentiles_for(field)
       percentiles = (0..1).step(BigDecimal('0.01')).to_a.map(&:to_s)
-      stat_class.pluck(<<-SQL).first
+      stats_for(field).pluck(<<-SQL).first
         percentile_disc(array[#{percentiles.join(',')}])
         WITHIN GROUP (ORDER BY (stats_data->>'#{field}')::integer)
       SQL
     end
 
     def average_for(field)
-      stat_class.average("(stats_data->>'#{field}')::integer").to_f
+      stats_for(field).average("(stats_data->>'#{field}')::integer").to_f
+    end
+
+    def stats_for(field)
+      stat_class.where("(stats_data->>'#{field}')::integer > 0")
     end
   end
 end
