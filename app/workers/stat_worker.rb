ce6ea958ba09964807be6c16ca037b07cc0f3bf5
Emma Lejeck <nuck@kitsu.io>
2018-12-14 17:03:36 -0800

    Skip StatWorker when the change predates the last recalculation

--- app/workers/stat_worker.rb
+++ app/workers/stat_worker.rb
@@ -11,6 +11,7 @@ class StatWorker
   def perform(stat, user_id, action, model, changes)
     # Bail if the User has stopped existing (deletion)
     return unless User.exists?(user_id)
+
     # Rehydrate the model
     model_class, model_attributes = model.values_at('class', 'attributes')
     model = model_class.constantize.new(model_attributes)
@@ -20,6 +21,10 @@ class StatWorker
     # Lock the User's stat row so no other workers can update it while we are
     Stat.transaction do
       stat = stat.constantize.for_user(user_id).lock!
+
+      # If the model data is older than the last stat recalculation, we don't have to do anything
+      return if model.updated_at && model.updated_at <= stat.recalculated_at
+
       # Call down to the stat to run the action
       stat.public_send("on_#{action}", wrapper) if stat.respond_to?("on_#{action}")
     end
