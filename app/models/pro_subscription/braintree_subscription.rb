39c2208d1992077ce7146a3b8823e286eb068d3f
Emma Lejeck <nuck@kitsu.io>
2019-02-06 18:44:16 -0800

    Add braintree pro endpoint

---
+++ app/models/pro_subscription/braintree_subscription.rb
@@ -0,0 +1,22 @@
+class ProSubscription
+  class BraintreeSubscription < ProSubscription
+    def billing_service
+      :braintree
+    end
+
+    def payment_method
+      user.braintree_customer.payment_methods.find(&:default?)
+    end
+
+    after_destroy do
+      $braintree.subscription.cancel(billing_id)
+    end
+
+    before_validation on: :create do
+      self.billing_id = $braintree.subscription.create(
+        payment_method_token: payment_method.token,
+        plan_id: "kitsu-pro-#{plan}"
+      ).id
+    end
+  end
+end
