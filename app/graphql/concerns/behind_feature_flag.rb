2adb4930c2542357e142093a159f647af58dbeea
Emma Lejeck <nuck@kitsu.io>
2019-02-10 19:11:49 -0800

    Add BehindFeatureFlag to put types/mutations behind flag

---
+++ app/graphql/concerns/behind_feature_flag.rb
@@ -0,0 +1,20 @@
+module BehindFeatureFlag
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def behind_feature_flag(flag)
+      if method_defined?(:visible?)
+        define_method(:visible?) do |context|
+          super && Flipper[flag].enabled?(context[:user])
+        end
+      elsif respond_to?(:visible?)
+        define_singleton_method(:visible?) do |context|
+          super && Flipper[flag].enabled?(context[:user])
+        end
+      else
+        ref = self&.name || self.class&.name
+        raise ArgumentError, "Cannot put #{ref} behind a feature flag"
+      end
+    end
+  end
+end
