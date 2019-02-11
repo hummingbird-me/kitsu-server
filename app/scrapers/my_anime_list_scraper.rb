634d18dc3c47eceabfad562f8b4b4d84d6cd7bf5
Emma Lejeck <nuck@kitsu.io>
2019-02-10 20:18:31 -0800

    Use @url instead of #url

--- app/scrapers/my_anime_list_scraper.rb
+++ app/scrapers/my_anime_list_scraper.rb
@@ -20,7 +20,7 @@ class MyAnimeListScraper < Scraper
   private
 
   def response
-    @response ||= http.get(url).tap do |res|
+    @response ||= http.get(@url).tap do |res|
       raise Scraper::PageNotFound if res.status == 404
       raise Scraper::TooManyRequests if res.status == 429
     end
