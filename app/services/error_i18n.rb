b4c76a732f7ad0349f8634e45f801252769a91dd
Emma Lejeck <nuck@kitsu.io>
2019-02-12 18:15:11 -0800

    Improve ErrorI18n to climb the ancestors chain

--- app/services/error_i18n.rb
+++ app/services/error_i18n.rb
@@ -1,6 +1,15 @@
 class ErrorI18n
-  def self.t(error)
-    key = error.class.underscore.tr('/', '.')
-    message = I18n.t(key, scope: 'errors', default: 'Unknown Error')
+  class << self
+    def self.translate(error)
+      klass = error.class
+      # Filter ancestry to just descendants of StandardError
+      ancestors = klass.ancestors.select { |c| c <= StandardError }
+      # Convert the class names into I18n keys
+      keys = ancestors.map { |c| c.name.underscore.tr('/', '.').to_sym }
+      # Look up the main key and fallback to the ancestors
+      I18n.translate(keys.shift, scope: 'errors', default: keys)
+    end
+
+    alias_method :t, :translate
   end
 end
