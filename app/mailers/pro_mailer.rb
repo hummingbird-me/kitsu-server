08fd1fcf6a91ff0709602cc3aeee405da68195be
Emma Lejeck <nuck@kitsu.io>
2019-02-15 20:12:52 -0800

    Expand ProMailer

--- app/mailers/pro_mailer.rb
+++ app/mailers/pro_mailer.rb
@@ -1,6 +1,38 @@
 class ProMailer < ApplicationMailer
   def gift_email(gift)
     @gift = gift
-    mail(to: gift.to.email, subject: "#{gift.from.name} gifted you a year of Kitsu PRO!")
+
+    mail(
+      to: gift.to.email,
+      subject: default_i18n_subject(name: gift.user.name, tier: gift.tier.titleize)
+    )
+  end
+
+  def cancellation_email(user, tier, reason = nil)
+    @user = user
+    @tier = tier
+    @reason = reason
+
+    mail(
+      to: user.email,
+      subject: default_i18n_subject(tier: tier.titleize)
+    )
+  end
+
+  def welcome_email(subscription)
+    @subscription = subscription
+
+    mail(
+      to: subscription.user.email,
+      subject: default_i18n_subject(tier: subscription.tier.titleize)
+    )
+  end
+
+  def billing_issue_email(subscription)
+    @subscription = subscription
+
+    mail(
+      to: subscription.user.email
+    )
   end
 end
