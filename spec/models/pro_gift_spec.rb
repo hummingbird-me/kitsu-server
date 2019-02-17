83515cf52e61d580cd982d9ee2828c7476495d33
Emma Lejeck <nuck@kitsu.io>
2019-02-16 21:08:21 -0800

    Update ProGift to use tier instead of length

--- spec/models/pro_gift_spec.rb
+++ spec/models/pro_gift_spec.rb
@@ -4,20 +4,4 @@ RSpec.describe ProGift, type: :model do
   it { should belong_to(:from).class_name('User') }
   it { should belong_to(:to).class_name('User') }
   it { should validate_length_of(:message).is_at_most(500) }
-
-  describe '#duration' do
-    context 'for a month of pro' do
-      subject { ProGift.new(length: :month) }
-      it 'should be 1 month of time' do
-        expect(subject.duration).to eq(1.month)
-      end
-    end
-
-    context 'for a year of pro' do
-      subject { ProGift.new(length: :year) }
-      it 'should be 1 year of time' do
-        expect(subject.duration).to eq(1.year)
-      end
-    end
-  end
 end
