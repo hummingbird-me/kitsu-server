a87e8869793ab17709804c63f7c2b3664557d4d4
Emma Lejeck <nuck@kitsu.io>
2019-02-22 20:48:07 -0800

    Extract pro mutations into a subtree

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,18 +1,4 @@
 class Types::MutationType < Types::BaseObject
   # Kitsu Pro
-  field :pro_subscribe_with_braintree,
-    mutation: Mutations::Pro::SubscribeWithBraintree,
-    description: 'Subscribe to Pro using Braintree'
-  field :pro_subscribe_with_stripe,
-    mutation: Mutations::Pro::SubscribeWithStripe,
-    description: 'Subscribe to Pro using Stripe'
-  field :pro_unsubscribe,
-    mutation: Mutations::Pro::Unsubscribe,
-    description: "End the user's pro subscription"
-  field :pro_set_message,
-    mutation: Mutations::Pro::SetMessage,
-    description: "Set the user's pro message"
-  field :pro_set_discord,
-    mutation: Mutations::Pro::SetDiscord,
-    description: "Set the user's discord tag"
+  field :pro, Types::ProMutation, null: false
 end
---
+++ app/graphql/types/pro_mutation.rb
@@ -0,0 +1,20 @@
+class Types::ProMutation < Types::BaseObject
+  # Subscription Management
+  field :subscribe_with_braintree,
+    mutation: Mutations::Pro::SubscribeWithBraintree,
+    description: 'Subscribe to Pro using Braintree'
+  field :subscribe_with_stripe,
+    mutation: Mutations::Pro::SubscribeWithStripe,
+    description: 'Subscribe to Pro using Stripe'
+  field :unsubscribe,
+    mutation: Mutations::Pro::Unsubscribe,
+    description: "End the user's pro subscription"
+
+  # Benefits Management
+  field :set_message,
+    mutation: Mutations::Pro::SetMessage,
+    description: "Set the user's Hall-of-Fame message"
+  field :set_discord,
+    mutation: Mutations::Pro::SetDiscord,
+    description: "Set the user's discord tag"
+end
