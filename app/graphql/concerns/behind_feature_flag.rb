cdb6f29f690fff4d523405896a1864ebfeccfb60
Emma Lejeck <nuck@kitsu.io>
2019-02-22 20:47:53 -0800

    Fix error in BehindFeatureFlag

--- app/graphql/concerns/behind_feature_flag.rb
+++ app/graphql/concerns/behind_feature_flag.rb
@@ -5,11 +5,11 @@ module BehindFeatureFlag
     def behind_feature_flag(flag)
       if method_defined?(:visible?)
         define_method(:visible?) do |context|
-          super && Flipper[flag].enabled?(context[:user])
+          super(context) && Flipper[flag].enabled?(context[:user])
         end
       elsif respond_to?(:visible?)
         define_singleton_method(:visible?) do |context|
-          super && Flipper[flag].enabled?(context[:user])
+          super(context) && Flipper[flag].enabled?(context[:user])
         end
       else
         ref = self&.name || self.class&.name
