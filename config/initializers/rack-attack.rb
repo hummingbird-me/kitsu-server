9c702ede8fc97f811691a939cd3453f56ed08466
Emma Lejeck <nuck@kitsu.io>
2019-07-28 17:27:43 -0700

    Add rack-attack for registrations-per-ip

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -5,6 +5,12 @@ class Rack::Attack
     end
   end
 
+  throttle('registrations/ip', limit: 5, period: 1.hour) do |req|
+    if req.path == '/api/edge/users' && req.post?
+      ActionDispatch::Request.new(req.env).remote_ip
+    end
+  end
+
   throttle('posts/token', limit: 3, period: 60.seconds) do |req|
     if req.path == '/api/edge/posts' && req.post?
       # return the email if present, nil otherwise
@@ -13,12 +19,11 @@ class Rack::Attack
   end
 
   throttle('likes/token', limit: 40, period: 120.seconds) do |req|
-    if req.path == '/api/edge/post-likes' || req.path =='/api/edge/comment-likes' && req.post?
+    if req.path == '/api/edge/post-likes' || req.path == '/api/edge/comment-likes' && req.post?
       # return the email if present, nil otherwise
       req.env['HTTP_AUTHORIZATION']
     end
   end
-
 
   throttle('follows/token', limit: 50, period: 300.seconds) do |req|
     if req.path == '/api/edge/follows' && req.post?
