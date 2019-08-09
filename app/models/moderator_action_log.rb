9c7f3ca34014000f1752dd9cf5a273b65ae6f6ab
Emma Lejeck <nuck@kitsu.io>
2019-08-09 13:43:46 -0700

    Fix ModeratorActionLog.generate!

--- app/models/moderator_action_log.rb
+++ app/models/moderator_action_log.rb
@@ -2,7 +2,7 @@ class ModeratorActionLog < ApplicationRecord
   belongs_to :user, required: true
   belongs_to :target, polymorphic: true, required: true
 
-  def generate!(actor, verb, target)
+  def self.generate!(actor, verb, target)
     create!(user: actor, verb: verb, target: target)
   end
 end
