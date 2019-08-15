ba26cf565ec727bb9b5acf3458b25a5cd3cad9a4
matthewdias <matthewdias@me.com>
2019-08-14 19:37:09 -0500

    Backfill nulls before adding constraint

--- db/migrate/20190103054632_make_abbreviated_titles_required.rb
+++ db/migrate/20190103054632_make_abbreviated_titles_required.rb
@@ -1,11 +1,16 @@
 class MakeAbbreviatedTitlesRequired < ActiveRecord::Migration
-  def up
+  using UpdateInBatches
+
+  def change
+    Anime.where(abbreviated_titles: nil).update_in_batches("abbreviated_titles = '{}'")
     change_column_null :anime, :abbreviated_titles, false
     change_column_default :anime, :abbreviated_titles, []
 
+    Manga.where(abbreviated_titles: nil).update_in_batches("abbreviated_titles = '{}'")
     change_column_null :manga, :abbreviated_titles, false
     change_column_default :manga, :abbreviated_titles, []
 
+    Drama.where(abbreviated_titles: nil).update_in_batches("abbreviated_titles = '{}'")
     change_column_null :dramas, :abbreviated_titles, false
     change_column_default :dramas, :abbreviated_titles, []
   end
