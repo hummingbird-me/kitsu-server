305b53c9948b5bdc7c43a555d6297b1950a8bdc2
Emma Lejeck <nuck@kitsu.io>
2019-12-24 17:49:50 -0800

    Handle TheChecker exceptions better

--- app/actions/accounts/prevalidate_email.rb
+++ app/actions/accounts/prevalidate_email.rb
@@ -1,4 +1,6 @@
 class PrevalidateEmail < Action
+  class TheCheckerError < StandardError; end
+
   HTTP_TIMEOUT = 10.seconds
   API_KEY = ENV['THECHECKER_API_KEY']
   URL = 'https://api.thechecker.co/v2/verify'.freeze
@@ -23,10 +25,25 @@ class PrevalidateEmail < Action
   def response_data
     return default_unknown_response unless API_KEY
 
-    @response_data ||= HTTP.timeout(HTTP_TIMEOUT).get(url).parse
+    if response.status == 200
+      @response_data ||= response.parse
+    else
+      Raven.capture_exception(TheCheckerError.new, extra: {
+        response_status: response.status,
+        response_data: response.parse
+      })
+      default_unknown_response
+    end
+  end
+
+  def response
+    return nil if @response == :timed_out
+
+    @response ||= HTTP.timeout(HTTP_TIMEOUT).get(url)
   rescue HTTP::TimeoutError => e
     Raven.capture_exception(e)
-    @response_data ||= default_unknown_response
+    @response = :timed_out
+    nil
   end
 
   def default_unknown_response
