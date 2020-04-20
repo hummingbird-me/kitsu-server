efeeb6f83fd02c0608e3a7ff524a6bb6fcd992a6
toyhammered <daniel@rassiner.com>
2020-04-19 17:05:57 -0700

    update base query to include id for mapping

--- app/models/list_import/anilist_v2.rb
+++ app/models/list_import/anilist_v2.rb
@@ -68,6 +68,7 @@ class ListImport
                   day
                 }
                 media {
+                  id
                   idMal
                   title {
                     romaji
@@ -100,6 +101,7 @@ class ListImport
                   day
                 }
                 media {
+                  id
                   idMal
                   title {
                     romaji
