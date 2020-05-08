c6e0db02fb688062cc1419eef592f1823764d179
toyhammered <daniel@rassiner.com>
2020-05-07 20:10:02 -0700

    remove comment because Model defines it

--- app/graphql/types/category.rb
+++ app/graphql/types/category.rb
@@ -5,34 +5,32 @@ class Types::Category < Types::BaseObject
 
   field :title, String,
     null: false,
-    description: 'The name of the category'
+    description: 'The name of the category.'
 
   field :description, String,
     null: true,
-    description: 'A description of the category'
+    description: 'A description of the category.'
 
   field :slug, String,
     null: false,
-    description: 'The URL-friendly identifier of this Category'
+    description: 'The URL-friendly identifier of this Category.'
 
   field :anidb_id, Integer,
     null: true,
-    description: 'Kitsu category mapping to Anidb category'
+    description: 'Kitsu category mapping to Anidb category.'
 
   field :total_media_count, Integer,
     null: false,
-    description: 'The amount of media that include this category'
+    description: 'The amount of media that include this category.'
 
-  # NOTE: unsure where this method would be defined
-  # saw it in Types::Media. Maybe through AR models it is auto defined?
   field :nsfw, Boolean,
     null: false,
-    description: 'Whether the category is Not-Safe-for-Work',
+    description: 'Whether the category is Not-Safe-for-Work.',
     method: :nsfw?
 
   field :image, Types::Image,
     null: true,
-    description: 'An image of the category'
+    description: 'An image of the category.'
 
   field :parent, Types::Category,
     null: true,
@@ -40,7 +38,7 @@ class Types::Category < Types::BaseObject
 
   field :child_count, Integer,
     null: false,
-    description: 'The amount of categories that are children to this category'
+    description: 'The amount of categories that are children to this category.'
 
   field :children, Types::Category.connection_type,
     null: true,
