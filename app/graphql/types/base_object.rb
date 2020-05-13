c7e03aac40e856820a16d2b104f3a8f01bb47159
toyhammered <daniel@rassiner.com>
2020-05-12 21:13:32 -0700

    add batch_load_field to automatically use AssociationLoader

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -1,2 +1,11 @@
 class Types::BaseObject < GraphQL::Schema::Object
+  def self.batch_load_field(name, *args, association_name: nil, **kwargs, &block)
+    if instance_methods(false).exclude?(name)
+      define_method name do
+        AssociationLoader.for(object.class, association_name.presence || name).load(object)
+      end
+    end
+
+    field(name, *args, **kwargs, &block)
+  end
 end
