e049cb0f3f693e64e25c6c189deb68ddde22818b
Emma Lejeck <nuck@kitsu.io>
2020-05-16 22:48:59 -0700

    Add Accounts::GenerateNoltToken action

---
+++ app/actions/accounts/generate_nolt_token.rb
@@ -0,0 +1,20 @@
+module Accounts
+  class GenerateNoltToken < Action
+    NOLT_SSO_SECRET = ENV['NOLT_SSO_SECRET']
+
+    parameter :user, load: User, required: true
+
+    def call
+      payload = {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        imageUrl: user.avatar.url(:original)
+      }
+
+      token = JWT.encode(payload, NOLT_SSO_SECRET, 'HS256')
+
+      { token: token }
+    end
+  end
+end
---
+++ spec/actions/accounts/generate_nolt_token_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Accounts::GenerateNoltToken do
+  let!(:user) { create(:user) }
+
+  it 'should correctly sign the JWT' do
+    stub_const('Accounts::GenerateNoltToken::NOLT_SSO_SECRET', 'boo')
+    token = described_class.call(user: user).token
+
+    expect {
+      JWT.decode(token, 'boo', true, { algorithm: 'HS256' })
+    }.not_to raise_error
+  end
+
+  it 'should have the user email, id, name, and avatar' do
+    stub_const('Accounts::GenerateNoltToken::NOLT_SSO_SECRET', 'boo')
+    token = described_class.call(user: user).token
+    contents = JWT.decode(token, 'boo', true, { algorithm: 'HS256' })[0]
+
+    expect(contents['id']).to eq(user.id)
+    expect(contents['email']).to eq(user.email)
+    expect(contents['name']).to eq(user.name)
+    expect(contents['imageUrl']).to eq(user.avatar.url(:original))
+  end
+end
