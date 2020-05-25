a39980179fbc35c46500e95a7860d47d55493404
toyhammered <daniel@rassiner.com>
2020-05-25 10:48:56 -0700

    add likes on a comment

--- app/graphql/types/comment.rb
+++ app/graphql/types/comment.rb
@@ -24,16 +24,18 @@ class Types::Comment < Types::BaseObject
     null: true,
     description: 'The parent comment if this comment was a reply to another.'
 
-  # field :likes, Types::CommentLike.connection_type,
-  #   null: false,
-  #   description: ''
+  field :likes, Types::Profile.connection_type,
+    null: false,
+    description: 'Users who liked this comment.'
 
   field :replies, Types::Comment.connection_type,
     null: false,
     description: 'All replies to a specific comment.'
 
   def likes
-    AssociationLoader.for(object.class, :likes).load(object)
+    AssociationLoader.for(object.class, :likes).load(object).then do |likes|
+      likes.map(&:user)
+    end
   end
 
   def replies
