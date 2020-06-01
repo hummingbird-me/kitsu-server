8d76265d34a19e3fd0857d30965ef53f1657c217
toyhammered <daniel@rassiner.com>
2020-05-31 19:54:43 -0700

    update descriptions for ratings

--- app/graphql/types/rating_system.rb
+++ app/graphql/types/rating_system.rb
@@ -1,5 +1,5 @@
 class Types::RatingSystem < Types::BaseEnum
   value 'SIMPLE', 'Smileys', value: 'simple'
-  value 'REGULAR', '1-10 whole numbers', value: 'regular'
-  value 'ADVANCED', '1-100 whole numbers', value: 'advanced'
+  value 'REGULAR', '5 Stars', value: 'regular'
+  value 'ADVANCED', '1-20 displayed as 1-10 in 0.5 increments', value: 'advanced'
 end
