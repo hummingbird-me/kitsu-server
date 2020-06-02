61d1c8ef228157b353e27ee0e066e78b3690a7f8
toyhammered <daniel@rassiner.com>
2020-06-01 19:56:55 -0700

    update rating system to be more descriptive

--- app/graphql/types/rating_system.rb
+++ app/graphql/types/rating_system.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class Types::RatingSystem < Types::BaseEnum
-  value 'SIMPLE', 'Smileys', value: 'simple'
-  value 'REGULAR', '5 Stars', value: 'regular'
-  value 'ADVANCED', '1-20 displayed as 1-10 in 0.5 increments', value: 'advanced'
+  SIMPLE = '1-20 displayed as 4 smileys - Awful (1), Meh (8), Good (14) and Great (20)'
+  REGULAR = '1-20 in increments of 2 displayed as 5 stars in 0.5 star increments'
+  ADVANCED = '1-20 in increments of 1 displayed as 1-10 in 0.5 increments'
+
+  value 'SIMPLE', SIMPLE, value: 'simple'
+  value 'REGULAR', REGULAR, value: 'regular'
+  value 'ADVANCED', ADVANCED, value: 'advanced'
 end
