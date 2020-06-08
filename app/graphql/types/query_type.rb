9d874b7b0ba0dd5c3614418f5f05a90282ca471e
toyhammered <daniel@rassiner.com>
2020-06-07 17:14:44 -0700

    make more verbose queries for categories

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -110,18 +110,21 @@ class Types::QueryType < Types::BaseObject
     User.patron.followed_first(context[:user]).order(followers_count: :desc)
   end
 
-  field :find_category, Types::Category, null: true do
-    description 'Find a category in the Kitsu Database by slug or ID'
-
-    argument :id, ID, required: false
-    argument :slug, String, required: false
+  field :find_category_by_id, Types::Category, null: false do
+    description 'Find a category by id in the Kitsu Database'
+    argument :id, ID, required: true
   end
 
-  def find_category(id: nil, slug: nil)
-    if id
-      ::Category.find(id)
-    elsif slug
-      ::Category.find_by(slug: slug)
-    end
+  def find_category_by_id(id:)
+    ::Category.find(id)
+  end
+
+  field :find_category_by_slug, Types::Category, null: false do
+    description 'Find a category by slug in the Kitsu Database'
+    argument :slug, String, required: true
+  end
+
+  def find_category_by_slug(slug:)
+    ::Category.find_by(slug: slug)
   end
 end
