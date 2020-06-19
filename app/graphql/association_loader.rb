98768217ca9350892bc53283478114d958ea7fcf
Daniel Rassiner <daniel@rassiner.com>
2020-06-18 22:06:16 -0700

    update association loader to automatically utilize pundit scopes (#666)

--- app/graphql/association_loader.rb
+++ app/graphql/association_loader.rb
@@ -4,18 +4,19 @@ class AssociationLoader < GraphQL::Batch::Loader
     nil
   end
 
-  def initialize(model, association_name)
+  def initialize(model, association_name, token: nil, policy: nil)
     @model = model
     @association_name = association_name
+    @token = token
+    @policy = (policy.presence || association_name).to_s
+
     validate
   end
 
-  def load(record)
-    unless record.is_a?(@model)
-      raise TypeError, "#{@model} loader can't load association for #{record.class}"
+  def scope(record)
+    load(record).then do |associations|
+      Promise.resolve(policy_klass.new(token, associations).resolve)
     end
-    return Promise.resolve(read_association(record)) if association_loaded?(record)
-    super
   end
 
   # We want to load the associations on all records, even if they have the same id
@@ -30,10 +31,26 @@ class AssociationLoader < GraphQL::Batch::Loader
 
   private
 
+  attr_reader :token, :policy
+
+  # This will return a Promise which we will chain the pundit scope onto.
+  # All the cached data will be pre-pundit scoped.
+  def load(record)
+    unless record.is_a?(@model)
+      raise TypeError, "#{@model} loader can't load association for #{record.class}"
+    end
+    return Promise.resolve(read_association(record)) if association_loaded?(record)
+    super
+  end
+
   def validate
     unless @model.reflect_on_association(@association_name)
       raise ArgumentError, "No association #{@association_name} on #{@model}"
     end
+  end
+
+  def policy_klass
+    @policy_klass ||= "#{policy.classify}Policy::Scope".constantize
   end
 
   def preload_association(records)
