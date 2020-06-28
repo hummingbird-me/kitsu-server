25edeb4ebc1f67f68913cb75ff00645057cdd2ce
toyhammered <daniel@rassiner.com>
2020-06-28 10:42:42 -0700

    update errors

--- app/graphql/concerns/error_mapping.rb
+++ app/graphql/concerns/error_mapping.rb
@@ -2,8 +2,6 @@ module ErrorMapping
   extend ActiveSupport::Concern
 
   included do
-    rescue_from ActiveRecord::RecordNotFound do |error|
-      raise Errors::RecordNotFound, error.message
-    end
+
   end
 end
