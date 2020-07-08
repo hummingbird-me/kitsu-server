ad86ffab083232c75bc3d1809928e3dedd281e4e
toyhammered <daniel@rassiner.com>
2020-07-07 20:12:33 -0700

    remove unnecessary implements from manga amount consumed

--- app/graphql/types/profile_stats/manga_amount_consumed.rb
+++ app/graphql/types/profile_stats/manga_amount_consumed.rb
@@ -1,4 +1,3 @@
 class Types::ProfileStats::MangaAmountConsumed < Types::BaseObject
-  implements Types::Interface::ProfileStats::Base
   implements Types::Interface::ProfileStats::AmountConsumed
 end
