09c6ac013f381997b9f533d17f1351225d74ee94
toyhammered <daniel@rassiner.com>
2020-07-14 19:26:26 -0700

    fix library entry status to have value

--- app/graphql/types/enum/library_entry_status.rb
+++ app/graphql/types/enum/library_entry_status.rb
@@ -1,7 +1,7 @@
 class Types::Enum::LibraryEntryStatus < Types::Enum::Base
-  value 'CURRENT', 'The user is currently reading or watching this media.'
-  value 'PLANNED', 'The user plans to read or watch this media in future.'
-  value 'COMPLETED', 'The user completed this media.'
-  value 'ON_HOLD', 'The user started but paused reading or watching this media.'
-  value 'DROPPED', 'The user started but chose not to finish this media.'
+  value 'CURRENT', 'The user is currently reading or watching this media.', value: :current
+  value 'PLANNED', 'The user plans to read or watch this media in future.', value: :planned
+  value 'COMPLETED', 'The user completed this media.', value: :completed
+  value 'ON_HOLD', 'The user started but paused reading or watching this media.', value: :on_hold
+  value 'DROPPED', 'The user started but chose not to finish this media.', value: :dropped
 end
