762c9d46a76693025cf7605041b169d30df23d76
toyhammered <daniel@rassiner.com>
2020-07-25 12:18:37 -0700

    fix cringe line to be correct

--- app/graphql/types/input/base.rb
+++ app/graphql/types/input/base.rb
@@ -3,7 +3,7 @@ class Types::Input::Base < GraphQL::Schema::InputObject
 
   # NOTE: https://github.com/rmosolgo/graphql-ruby/blob/master/lib/graphql/schema/input_object.rb#L136
   # These need to be unique, so I am forcible doing that.
-  # I am thou, and thou and I, but you will always be an input!
+  # I am thou, and thou art I, but you will always be an input!
   def self.graphql_name
     split_name = name.split('::')[1..-1]
 
