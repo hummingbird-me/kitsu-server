1fb521343cb5e4b427d85b091e0076b1454e2587
Daniel Rassiner <daniel@rassiner.com>
2020-07-29 20:53:16 -0700

    add missing count fields to manga (#756)

--- app/graphql/types/manga.rb
+++ app/graphql/types/manga.rb
@@ -4,4 +4,16 @@ class Types::Manga < Types::BaseObject
   field :subtype, Types::Enum::MangaSubtype,
     null: false,
     description: 'A secondary type for categorizing Manga.'
+
+  field :chapter_count, Integer,
+    null: true,
+    description: 'The number of chapters in this manga.'
+
+  field :chapter_count_guess, Integer,
+    null: true,
+    description: 'The estimated number of chapters in this manga.'
+
+  field :volume_count, Integer,
+    null: true,
+    description: 'The number of volumes in this manga.'
 end
