9811225bb95b938f8809a84ff24fd5fff896a097
toyhammered <daniel@rassiner.com>
2020-08-01 20:42:18 -0700

    remove unused type

--- app/graphql/types/delete_result.rb
+++
@@ -1,4 +0,0 @@
-class Types::DeleteResult < Types::BaseObject
-  field :id, ID, null: false, description: 'ID of the deleted entity'
-  field :type_name, String, null: false, description: 'Type of the deleted entity'
-end
