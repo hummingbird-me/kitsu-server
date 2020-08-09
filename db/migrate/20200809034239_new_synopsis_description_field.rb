860e8482c6756353b2c3c706a09648db8c500fb0
toyhammered <daniel@rassiner.com>
2020-08-08 21:02:07 -0700

    migrations to convert fields

---
+++ db/migrate/20200809034239_new_synopsis_description_field.rb
@@ -0,0 +1,16 @@
+class NewSynopsisDescriptionField < ActiveRecord::Migration[5.1]
+  def change
+    Anime.logger = Logger.new(STDERR)
+
+    %i(anime episodes manga chapters dramas).each do |model_name|
+      add_column model_name, :temp_synopsis, :jsonb, default: {}, null: false
+    end
+
+    %i(
+      amas categories characters community_recommendation_requests
+      genres group_categories people site_announcements
+    ).each do |model_name|
+      add_column model_name, :temp_description, :jsonb, default: {}, null: false
+    end
+  end
+end
---
+++ db/migrate/20200809034845_update_new_synopsis_description_field.rb
@@ -0,0 +1,18 @@
+class UpdateNewSynopsisDescriptionField < ActiveRecord::Migration[5.1]
+  def change
+    Anime.logger = Logger.new(STDERR)
+
+    %i(anime episodes manga chapters dramas).each do |model_name|
+      remove_column model_name, :synopsis
+      rename_column model_name, :temp_synopsis, :synopsis
+    end
+
+    %i(
+      amas categories characters community_recommendation_requests
+      genres group_categories people site_announcements
+    ).each do |model_name|
+      remove_column model_name, :description
+      rename_column model_name, :temp_description, :description
+    end
+  end
+end
