009b4eb167dea62713934b75ae31c76a6e9141c0
toyhammered <daniel@rassiner.com>
2020-08-14 18:53:31 -0700

    remove deprecated fields

--- app/graphql/types/video.rb
+++ app/graphql/types/video.rb
@@ -8,24 +8,6 @@ class Types::Video < Types::BaseObject
     null: false,
     description: 'The url of the video.'
 
-  field :available_regions, [String],
-    null: false,
-    description: 'Which regions this video is available in.',
-    deprecation_reason: 'Please use regions.',
-    method: :regions
-
-  field :sub_language, String,
-    null: false,
-    description: 'The language this is subbed in',
-    deprecation_reason: 'Please use subs',
-    method: :sub_lang
-
-  field :dub_language, String,
-    null: false,
-    description: 'The language this is dubbed in.',
-    deprecation_reason: 'Please use dubs',
-    method: :dub_lang
-
   field :episode, Types::Episode,
     null: false,
     description: 'The episode of this video'
