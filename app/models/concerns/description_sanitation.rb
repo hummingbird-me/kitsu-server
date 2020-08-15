96c1d7be9fcf58b770bd1f67790f9d0a6b43bb3e
toyhammered <daniel@rassiner.com>
2020-08-15 13:25:19 -0700

    add a bang bang

--- app/models/concerns/description_sanitation.rb
+++ app/models/concerns/description_sanitation.rb
@@ -6,6 +6,6 @@ module DescriptionSanitation
   end
 
   def sanitize_description
-    description.transform_values { |desc| Sanitize.fragment(desc, Sanitize::Config::RESTRICTED) }
+    description.transform_values! { |desc| Sanitize.fragment(desc, Sanitize::Config::RESTRICTED) }
   end
 end
