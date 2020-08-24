8307fe6530e5b82016101ebb3da76d9d1d561587
Daniel Rassiner <daniel@rassiner.com>
2020-08-24 08:55:48 -0700

    clean up garbage mappings (#806)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200812034201) do
+ActiveRecord::Schema.define(version: 20200822214657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
