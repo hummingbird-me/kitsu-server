ba8b3568df0a8e6924a69a467f0d819479727b25
toyhammered <daniel@rassiner.com>
2020-09-09 20:28:57 -0700

    fix media_type enum

--- app/graphql/types/enum/media_type.rb
+++ app/graphql/types/enum/media_type.rb
@@ -1,6 +1,6 @@
 class Types::Enum::MediaType < Types::Enum::Base
   description 'これはアニメやマンガです'
-  graphql_name 'MediaType'
+  graphql_name 'MediaTypeEnum'
 
   value 'ANIME', value: 'Anime'
   value 'MANGA', value: 'Manga'
