bee80c035f1b650a14246b931b63dbcb3f55f17c
toyhammered <daniel@rassiner.com>
2020-09-13 18:49:05 -0700

    update record loader to support sorting

--- app/graphql/record_loader.rb
+++ app/graphql/record_loader.rb
@@ -3,9 +3,30 @@ class RecordLoader < GraphQL::Batch::Loader
     @model = model
   end
 
+  def sort_load_many(ids, sort: nil)
+    load_many(ids).then do |results|
+      sort_results(results, sort)
+    end
+  end
+
   def perform(ids)
     @model.where(id: ids).each { |record| fulfill(record.id, record) }
 
-    ids.each { |id| fulfill(id, nil) unless fulfilled?(id) }
+    ids.each do |id|
+      next if fulfilled?(id)
+
+      fulfill(id, nil)
+    end
+  end
+
+  def sort_results(results, sorts)
+    return results if sorts.blank?
+
+    formatted_sort = sorts.each_with_object({}) do |sort, hash|
+      hash[sort.field] = sort.direction
+      hash
+    end
+
+    results.order(**formatted_sort)
   end
 end
