36d0ee8c085bb29104e2dec5b5cf72f1ea83c29e
toyhammered <daniel@rassiner.com>
2020-09-21 16:50:24 -0700

    include sort as part of graphql name

--- app/graphql/types/enum/base.rb
+++ app/graphql/types/enum/base.rb
@@ -1,5 +1,16 @@
 class Types::Enum::Base < GraphQL::Schema::Enum
+  OPERATIONS = %w[sort].freeze
+
   def self.default_graphql_name
-    "#{super}Enum"
+    split_name = name.split('::')[2..-1]
+
+    if split_name.first.downcase.starts_with?(*OPERATIONS)
+      # Used so that it will throw an error if name.nil?
+      super
+
+      "#{split_name.join}Enum"
+    else
+      "#{super}Enum"
+    end
   end
 end
