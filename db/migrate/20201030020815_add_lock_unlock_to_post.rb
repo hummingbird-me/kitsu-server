c4b3bad3d6b9cad3a19dca3afdce5b2a50d55191
toyhammered <daniel@rassiner.com>
2020-10-31 13:41:19 -0700

    update migration

--- db/migrate/20201030020815_add_lock_unlock_to_post.rb
+++ db/migrate/20201030020815_add_lock_unlock_to_post.rb
@@ -1,8 +1,8 @@
 class AddLockUnlockToPost < ActiveRecord::Migration[5.1]
   def change
-    add_column :posts, :locked_by, :integer
+    add_column :posts, :locked_by_id, :integer
     add_column :posts, :locked_at, :datetime
     add_column :posts, :lock_reason, :integer
-    add_index :posts, :locked_by
+    add_index :posts, :locked_by_id
   end
 end
