dcf9ce73b32a17e1301cb83948521f0a3aa4624d
Emma Lejeck <nuck@kitsu.io>
2020-10-31 21:46:56 -0700

    Give access to rails_admin to all mods/staff

--- lib/admin_constraint.rb
+++ lib/admin_constraint.rb
@@ -3,6 +3,6 @@ class AdminConstraint
     return false unless request.session[:token]
     token = Doorkeeper::AccessToken.by_token(request.session[:token])
     user = User.find(token[:resource_owner_id])
-    user && user.roles.where(name: 'admin').count > 0
+    user&.permissions&.any?
   end
 end
