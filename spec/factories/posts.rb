c6d106403bfdc7c757e28b637e4764f3b3fa086c
toyhammered <daniel@rassiner.com>
2020-11-01 19:14:51 -0800

    fix post factory

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -50,7 +50,7 @@ FactoryBot.define do
     trait :locked do
       association :locked_by, factory: :user, strategy: :build
       locked_at { DateTime.now }
-      locked_reason { :SPAM }
+      locked_reason { :spam }
     end
   end
 end
