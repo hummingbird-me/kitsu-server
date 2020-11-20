1a97b92c61317a718633aa5cdf5e47cc00862536
Daniel Rassiner <daniel@rassiner.com>
2020-11-19 19:25:04 -0800

    fix post resource (#891)

--- app/resources/post_resource.rb
+++ app/resources/post_resource.rb
@@ -1,6 +1,6 @@
 class PostResource < BaseResource
   caching
-  IMMUTABLE_FIELDS = %i[locked_by locked_at locked_reason].freeze
+  IMMUTABLE_FIELDS = %i[locked_at locked_reason].freeze
 
   attributes :content, :content_formatted, :comments_count, :post_likes_count,
     :spoiler, :nsfw, :blocked, :deleted_at, :top_level_comments_count,
@@ -14,16 +14,17 @@ class PostResource < BaseResource
   has_one :media, polymorphic: true
   has_one :spoiled_unit, polymorphic: true
   has_one :ama, foreign_key: 'original_post_id', foreign_key_on: :related
+  has_one :locked_by, class_name: 'User'
   has_many :post_likes
   has_many :comments
   has_many :uploads
 
   def self.creatable_fields(context)
-    super - IMMUTABLE_FIELDS
+    super - (IMMUTABLE_FIELDS + :locked_by)
   end
 
   def self.updatable_fields(context)
-    super - IMMUTABLE_FIELDS
+    super - (IMMUTABLE_FIELDS + :locked_by)
   end
 
   def target_interest=(val)
