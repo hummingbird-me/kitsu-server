354864db567428bfc510a4923c9f3fec8e0650e5
toyhammered <daniel@rassiner.com>
2020-12-06 16:14:14 -0800

    fix installments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201126190937) do
+ActiveRecord::Schema.define(version: 20201206202624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -714,7 +714,7 @@ ActiveRecord::Schema.define(version: 20201126190937) do
     t.integer "media_id"
     t.integer "franchise_id"
     t.string "media_type", null: false
-    t.integer "position", default: 0, null: false
+    t.integer "position"
     t.string "tag"
     t.datetime "created_at"
     t.datetime "updated_at"
