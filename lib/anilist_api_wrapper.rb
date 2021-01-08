3090df5a892fd20868bde0b54ddf0287f2096111
Emma Lejeck <nuck@kitsu.io>
2021-01-08 11:54:03 -0800

    Capture exceptions from Anilist API client booting

--- lib/anilist_api_wrapper.rb
+++ lib/anilist_api_wrapper.rb
@@ -6,13 +6,17 @@ require 'graphql/client/http'
 module AnilistApiWrapper
   GRAPHQL_API = 'https://graphql.anilist.co'
 
-  HTTP = GraphQL::Client::HTTP.new(GRAPHQL_API) do
-    def headers(_)
-      { 'Content-Type': 'application/json' }
+  begin
+    HTTP = GraphQL::Client::HTTP.new(GRAPHQL_API) do
+      def headers(_)
+        { 'Content-Type': 'application/json' }
+      end
     end
+
+    Schema = GraphQL::Client.load_schema(HTTP)
+
+    Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+  rescue StandardError => e
+    Sentry.capture_exception(e)
   end
-
-  Schema = GraphQL::Client.load_schema(HTTP)
-
-  Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
 end
