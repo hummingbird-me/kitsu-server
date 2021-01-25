0c6d4053bc06ad8b4a627028010b98204d9d8bbc
Emma Lejeck <nuck@kitsu.io>
2021-01-24 21:51:34 -0800

    Restore rack-attack config now that X-Forwarded-For is set up right

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,17 +1,13 @@
 class Rack::Attack
-  throttle('logins/ip', limit: 150, period: 60.seconds) do |req|
+  throttle('logins/ip', limit: 15, period: 60.seconds) do |req|
     if req.path == '/api/oauth/token' && req.post? && req.params['grant_type'] == 'password'
-      ip = ActionDispatch::Request.new(req.env).remote_ip
-      Rails.logger.warn "LOGIN FROM #{req.env['HTTP_X_FORWARDED_FOR']} (#{ip})"
-      ip
+      ActionDispatch::Request.new(req.env).remote_ip
     end
   end
 
-  throttle('registrations/ip', limit: 50, period: 1.hour) do |req|
+  throttle('registrations/ip', limit: 5, period: 1.hour) do |req|
     if req.path == '/api/edge/users' && req.post?
-      ip = ActionDispatch::Request.new(req.env).remote_ip
-      Rails.logger.warn "SIGNUP FROM #{req.env['HTTP_X_FORWARDED_FOR']} (#{ip})"
-      ip
+      ActionDispatch::Request.new(req.env).remote_ip
     end
   end
 
