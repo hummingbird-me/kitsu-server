c67ff599d514e66104ff781a819ad904d3961e40
Emma Lejeck <nuck@kitsu.io>
2021-01-25 21:56:21 -0800

    Add SlugLoader

---
+++ app/graphql/loaders/slug_loader.rb
@@ -0,0 +1,9 @@
+class Loaders::SlugLoader < RecordLoader
+  def initialize(model, column: :slug, **args)
+    super(model, column: column, **args)
+  end
+
+  def cache_key(load_key)
+    load_key.downcase
+  end
+end
