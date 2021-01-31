6e7776d5deaa8e264b162eb28bff3bd8d4ae15a3
toyhammered <daniel@rassiner.com>
2021-01-30 21:31:31 -0800

    whooops...

--- app/graphql/types/input/titles_list.rb
+++ app/graphql/types/input/titles_list.rb
@@ -1,4 +1,5 @@
 class Types::Input::TitlesList < Types::Input::Base
+  argument :canonical, String, required: false
   argument :localized, Types::Map, required: false
   argument :alternatives, [String], required: false
   argument :canonical_locale, String, required: false
