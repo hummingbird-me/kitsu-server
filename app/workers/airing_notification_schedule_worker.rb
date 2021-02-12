fdf75c6ddc881a0a8c76993ccf8026586e767fab
Emma Lejeck <nuck@kitsu.io>
2021-02-11 19:06:18 -0800

    Allow disabling airing_notifications

--- app/workers/airing_notification_schedule_worker.rb
+++ app/workers/airing_notification_schedule_worker.rb
@@ -2,6 +2,8 @@ class AiringNotificationScheduleWorker
   include Sidekiq::Worker
 
   def perform(klass_name)
+    return unless Flipper.enabled?(:airing_notifications)
+
     klass = klass_name.safe_constantize
     klass.current.where.not(release_schedule: nil).each do |media|
       airings = media.release_schedule.occurrences_between(Time.now, 7.days.from_now)
