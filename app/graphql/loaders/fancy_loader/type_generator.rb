9406ad591b4e83aa4357c857d66d2be9b4f24013
Emma Lejeck <nuck@kitsu.io>
2021-02-15 20:01:35 -0800

    Add FancyLoader::TypeGenerator

---
+++ app/graphql/loaders/fancy_loader/type_generator.rb
@@ -0,0 +1,41 @@
+##
+# Generate parameter types for sorting
+class Loaders::FancyLoader::TypeGenerator
+  class SortDirection < GraphQL::Schema::Enum
+    graphql_name 'SortDirectionEnum'
+
+    value :ASCENDING, value: :asc
+    value :DESCENDING, value: :desc
+  end
+
+  def initialize(loader, name: loader.model.name)
+    @loader = loader
+    @name = name
+  end
+
+  def sorts_enum
+    sorts = @loader.sorts
+    name = "#{@name}SortEnum"
+
+    @sorts_enum ||= Class.new(GraphQL::Schema::Enum) do
+      graphql_name name
+      sorts.each_key do |sort_name|
+        value(sort_name.to_s.underscore.upcase, value: sort_name)
+      end
+    end
+  end
+
+  def sorts_option
+    enum = sorts_enum
+    name = "#{@name}SortOption"
+    @sorts_option ||= Class.new(GraphQL::Schema::InputObject) do
+      graphql_name name
+      argument :on, enum, required: true
+      argument :direction, SortDirection, required: true
+    end
+  end
+
+  def sorts_list
+    @sorts_list ||= GraphQL::Schema::List.new(sorts_option)
+  end
+end
