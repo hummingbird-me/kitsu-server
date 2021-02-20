4b7827687ea93791353abb64d76d7ce13c2018af
toyhammered <daniel@rassiner.com>
2021-02-19 21:08:29 -0800

    update #total_count to use items instead of nodes

--- app/graphql/types/base_connection.rb
+++ app/graphql/types/base_connection.rb
@@ -3,7 +3,12 @@ class Types::BaseConnection < GraphQL::Types::Relay::BaseConnection
     null: false,
     description: 'The total amount of nodes.'
 
+  # NOTE: In 1.11.6 we used object.nodes,
+  # but in 1.12.0 they added the #first to limit the amount of nodes it gets.
+  # We now grab the object.items because that uses ActiveRecord direclty,
+  # instead of the #nodes method in graphql (which wraps AR).
+  # This will not trigger a limit based on the #first method and grab all records now.
   def total_count
-    object.nodes.count
+    object.items.count
   end
 end
