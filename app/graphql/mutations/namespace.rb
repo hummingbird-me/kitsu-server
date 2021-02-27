ed087657ab4a62676d9fce0c70e48e5aa858e14d
Emma Lejeck <nuck@kitsu.io>
2021-02-27 11:57:38 -0800

    Hack around issue with GraphQL mutation namespaces

--- app/graphql/mutations/namespace.rb
+++ app/graphql/mutations/namespace.rb
@@ -2,4 +2,10 @@ class Mutations::Namespace < Types::BaseObject
   def self.default_graphql_name
     "#{name.split('::')[1..-1].join}Mutations"
   end
+
+  # HACK: The GraphQL runtime gets confused by the nil objects in mutations. So we override the
+  # object method to just return a hash with all fields being hashes.
+  def object
+    Hash.new { |hash, key| hash[key] = {} }
+  end
 end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,8 +1,14 @@
 class Types::MutationType < Types::BaseObject
   field :pro, Mutations::Pro, null: false
-  field :anime, Mutations::Anime, null: true
-  field :episode, Mutations::Episode, null: true
-  field :library_entry, Mutations::LibraryEntry, null: true
-  field :mapping, Mutations::Mapping, null: true
-  field :post, Mutations::Post, null: true
+  field :anime, Mutations::Anime, null: false
+  field :episode, Mutations::Episode, null: false
+  field :library_entry, Mutations::LibraryEntry, null: false
+  field :mapping, Mutations::Mapping, null: false
+  field :post, Mutations::Post, null: false
+
+  # HACK: The GraphQL runtime gets confused by the nil objects in mutations. So we override the
+  # object method to just return a hash with all fields being hashes.
+  def object
+    Hash.new { |hash, key| hash[key] = {} }
+  end
 end
