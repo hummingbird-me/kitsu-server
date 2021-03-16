6459a31ae261b48bb4bbef6b14bdada65b8864b6
Emma Lejeck <nuck@kitsu.io>
2021-03-15 21:26:30 -0700

    Seed announcement views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_232334) do
+ActiveRecord::Schema.define(version: 2021_03_16_035000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
