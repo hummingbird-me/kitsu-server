ff6ef0dbb1d2442f9c2ba50a4eb31fb5d2b1323a
toyhammered <daniel@rassiner.com>
2021-04-01 20:18:41 -0700

    Add Installment type

---
+++ app/graphql/types/enum/installment_tag.rb
@@ -0,0 +1,4 @@
+class Types::Enum::InstallmentTag < Types::Enum::Base
+  value 'SIDE_STORY', '', value: :side_story
+  value 'SPINOFF', '', value: :spinoff
+end
---
+++ app/graphql/types/installment.rb
@@ -0,0 +1,31 @@
+class Types::Installment < Types::BaseObject
+  implements Types::Interface::WithTimestamps
+
+  description ''
+
+  field :id, ID, null: false
+
+  field :release_position, Integer,
+    null: true,
+    description: ''
+
+  field :chronological_position, Integer,
+    null: true,
+    description: ''
+
+  field :tag, Types::Enum::InstallmentTag,
+    null: true,
+    description: ''
+
+  field :media, Types::Interface::Media,
+    null: false,
+    description: 'The media related to this installment'
+
+  def media
+    RecordLoader.for(object.media_type.safe_constantize).load(object.media_id)
+  end
+
+  field :franchise, Types::Franchise,
+    null: false,
+    description: 'The franchise related to this installment'
+end
