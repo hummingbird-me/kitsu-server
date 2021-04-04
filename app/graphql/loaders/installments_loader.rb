7826aec2fdf0ba8808fbada25a0745e6178ef497
toyhammered <daniel@rassiner.com>
2021-04-03 20:54:28 -0700

    fix linting issues

--- app/graphql/loaders/installments_loader.rb
+++ app/graphql/loaders/installments_loader.rb
@@ -1,8 +1,16 @@
 class Loaders::InstallmentsLoader < Loaders::FancyLoader
   from Installment
   modify_query ->(query) {
-    release_rank = Loaders::FancyLoader::RankQueryGenerator.new(:release_order, @find_by, table).arel
-    alternative_rank = Loaders::FancyLoader::RankQueryGenerator.new(:alternative_order, @find_by, table).arel
+    release_rank = Loaders::FancyLoader::RankQueryGenerator.new(
+      :release_order,
+      @find_by,
+      table
+    ).arel
+    alternative_rank = Loaders::FancyLoader::RankQueryGenerator.new(
+      :alternative_order,
+      @find_by,
+      table
+    ).arel
 
     query.project(release_rank).project(alternative_rank)
   }
