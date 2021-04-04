cf606629a8b062e3edea69bfe19c2d882731a3e6
toyhammered <daniel@rassiner.com>
2021-04-03 20:44:07 -0700

    Add Installments Loader

--- app/graphql/loaders/installments_loader.rb
+++ app/graphql/loaders/installments_loader.rb
@@ -1,5 +1,11 @@
 class Loaders::InstallmentsLoader < Loaders::FancyLoader
   from Installment
+  modify_query ->(query) {
+    release_rank = Loaders::FancyLoader::RankQueryGenerator.new(:release_order, @find_by, table).arel
+    alternative_rank = Loaders::FancyLoader::RankQueryGenerator.new(:alternative_order, @find_by, table).arel
+
+    query.project(release_rank).project(alternative_rank)
+  }
 
   sort :release_order
   sort :alternative_order
