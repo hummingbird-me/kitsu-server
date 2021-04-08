182206454d169f17277c4765483b299eb5a3a64b
toyhammered <daniel@rassiner.com>
2021-04-08 11:46:41 -0700

    remove unused types

--- app/graphql/types/enum/wiki_submission_action.rb
+++
@@ -1,5 +0,0 @@
-class Types::Enum::WikiSubmissionAction < Types::Enum::Base
-  value 'CREATE', '', value: :create
-  value 'UPDATE', '', value: :update
-  value 'DELETE', '', value: :delete
-end
--- app/graphql/types/enum/wiki_submission_type.rb
+++
@@ -1,4 +0,0 @@
-class Types::Enum::WikiSubmissionType < Types::Enum::Base
-  value 'ANIME', '', value: :anime
-  value 'MANGA', '', value: :manga
-end
