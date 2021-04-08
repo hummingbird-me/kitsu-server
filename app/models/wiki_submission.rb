013d46e87c09ea94fa53c833ecf56fb906e41dcc
toyhammered <daniel@rassiner.com>
2021-04-08 11:47:15 -0700

    update model

--- app/models/wiki_submission.rb
+++ app/models/wiki_submission.rb
@@ -1,22 +1,9 @@
 class WikiSubmission < ApplicationRecord
-  self.inheritance_column = :_type_disabled
-
   belongs_to :user
-
-  enum type: {
-    anime: 0,
-    manga: 1
-  }
 
   enum status: {
     draft: 0,
-    pending: 1,
-    approved: 2
-  }
-
-  enum action: {
-    create: 0,
-    update: 1,
-    delete: 2
+    approved: 1,
+    rejected: 2
   }
 end
