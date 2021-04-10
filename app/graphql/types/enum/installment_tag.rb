c7f8fa8c40fc22ab6d056eabcf798c9dbb7d9733
toyhammered <daniel@rassiner.com>
2021-04-10 09:46:54 -0700

    add installment tag descriptions

--- app/graphql/types/enum/installment_tag.rb
+++ app/graphql/types/enum/installment_tag.rb
@@ -1,8 +1,7 @@
 class Types::Enum::InstallmentTag < Types::Enum::Base
-  value 'MAIN_STORY', '', value: :main_story
+  value 'MAIN_STORY', 'The main story', value: :main_story
   value 'SIDE_STORY', '', value: :side_story
-  value 'SPINOFF', '', value: :spinoff
-  value 'MAIN_ENTRY', '', value: :main_entry
-  value 'CROSSOVER', '', value: :crossover
-  value 'ALTERNATE_SETTING', '', value: :alternate_setting
+  value 'SPINOFF', 'Related to main story with a new narrative', value: :spinoff
+  value 'CROSSOVER', 'When characters from different media come together', value: :crossover
+  value 'ALTERNATE_SETTING', 'Same universe, with different characters or visa-versa', value: :alternate_setting
 end
