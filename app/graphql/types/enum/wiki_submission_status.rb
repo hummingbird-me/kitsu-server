3c1704350ab72991bacf507dae5c15928ad0d208
toyhammered <daniel@rassiner.com>
2021-04-10 21:29:55 -0700

    wiki submission and logs

--- app/graphql/types/enum/wiki_submission_status.rb
+++ app/graphql/types/enum/wiki_submission_status.rb
@@ -2,4 +2,5 @@ class Types::Enum::WikiSubmissionStatus < Types::Enum::Base
   value 'DRAFT', '', value: :draft
   value 'PENDING', '', value: :pending
   value 'APPROVED', '', value: :approved
+  value 'REJECTED', '', value: :rejected
 end
--- app/models/wiki_submission.rb
+++ app/models/wiki_submission.rb
@@ -1,9 +1,13 @@
 class WikiSubmission < ApplicationRecord
   belongs_to :user
+  has_many :logs, class_name: 'WikiSubmissionLog', dependent: :destroy
+  has_one :parent, foreign_key: :parent_id, class: 'WikiSubmission', optional: true,
+                   dependent: :destroy, inverse_of: :wiki_submission
 
   enum status: {
     draft: 0,
-    approved: 1,
-    rejected: 2
+    pending: 1,
+    approved: 2,
+    rejected: 3
   }
 end
