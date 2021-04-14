fc7e3a1fb23b56717748d1196d321cf8e1843f93
toyhammered <daniel@rassiner.com>
2021-04-14 09:49:46 -0700

    update wiki submission with specs

--- app/models/wiki_submission.rb
+++ app/models/wiki_submission.rb
@@ -2,8 +2,10 @@ class WikiSubmission < ApplicationRecord
   belongs_to :user
   has_many :logs, class_name: 'WikiSubmissionLog', dependent: :destroy
 
-  belongs_to :parent, inverse_of: :child, foreign_key: :parent_id, optional: true
-  has_one :child, inverse_of: :parent, dependent: :destroy
+  belongs_to :parent, inverse_of: :child, class_name: 'WikiSubmission',
+                      foreign_key: :parent_id, optional: true
+  has_one :child, inverse_of: :parent, class_name: 'WikiSubmission', foreign_key: :parent_id,
+                  dependent: :destroy
 
   enum status: {
     draft: 0,
