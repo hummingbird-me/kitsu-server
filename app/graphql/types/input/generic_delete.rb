1734b1b23c1dd9b17f2662039bd6495af5735e5b
toyhammered <daniel@rassiner.com>
2021-04-15 18:49:12 -0700

    remove graphql_name override due to not being needed.

--- app/graphql/types/input/generic_delete.rb
+++ app/graphql/types/input/generic_delete.rb
@@ -1,5 +1,3 @@
 class Types::Input::GenericDelete < Types::Input::Base
-  graphql_name 'GenericDeleteInput'
-
   argument :id, ID, required: true
 end
--- app/graphql/types/input/titles_list.rb
+++ app/graphql/types/input/titles_list.rb
@@ -1,6 +1,4 @@
 class Types::Input::TitlesList < Types::Input::Base
-  graphql_name 'TitlesListInput'
-
   argument :canonical, String, required: false
   argument :localized, Types::Map, required: false
   argument :alternatives, [String], required: false
