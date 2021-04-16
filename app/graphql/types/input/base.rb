d53f3777b5d5a7b3518100f3ada4f68547d8a624
Daniel Rassiner <daniel@rassiner.com>
2021-04-15 20:01:08 -0700

    GraphQL - Update Input Type Generation (#1002)

--- app/graphql/types/input/base.rb
+++ app/graphql/types/input/base.rb
@@ -1,19 +1,9 @@
 class Types::Input::Base < GraphQL::Schema::InputObject
-  CRUD_OPERATION = %w[create update delete].freeze
-
   # These need to be unique, so I am forcible doing that.
   # I am thou, and thou art I, but you will always be an input!
   def self.default_graphql_name
-    split_name = name.split('::')[2..-1]
-
-    if split_name.last.downcase.starts_with?(*CRUD_OPERATION)
-      # Used so that it will throw an error if name.nil?
-      super
-      # Types::Input::Anime::Create -> AnimeCreate
-      "#{split_name.join}Input"
-    else
-      "#{super}Input"
-    end
+    # Types::Input::Anime::Create -> AnimeCreate
+    "#{name.split('::')[2..-1].join}Input"
   end
 
   def to_model
