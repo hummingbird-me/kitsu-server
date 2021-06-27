892df44aefac4a62cfcd1994041cf3de3e09ede8
Emma Lejeck <nuck@kitsu.io>
2021-06-27 14:11:58 -0700

    Fix system user migration

--- db/migrate/20210605031927_add_system_user.rb
+++ db/migrate/20210605031927_add_system_user.rb
@@ -1,15 +1,11 @@
 class AddSystemUser < ActiveRecord::Migration[5.2]
-  class NonvalidatingUser < User
-    def not_reserved_name; end
-  end
-
   def change
     # TODO: once we get unregistered accounts, switch it to one of those
-    NonvalidatingUser.create!(
+    User.new(
       name: 'System',
       id: -11,
       email: 'noreply+system@kitsu.io',
       password: SecureRandom.base64(45)
-    )
+    ).save!(validate: false)
   end
 end
