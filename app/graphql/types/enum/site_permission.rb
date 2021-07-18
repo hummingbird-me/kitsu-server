8fddea1b8f8d14ff86df3b744f122a615f3f8053
Emma Lejeck <nuck@kitsu.io>
2021-07-17 20:41:13 -0700

    Fix SitePermission enum

--- app/graphql/types/enum/site_permission.rb
+++ app/graphql/types/enum/site_permission.rb
@@ -1,5 +1,5 @@
 class Types::Enum::SitePermission < Types::Enum::Base
-  value 'ADMIN', 'Administrator/staff member of Kitsu', value: 'admin'
-  value 'COMMUNITY_MOD', 'Moderator of community behavior', value: 'community_mod'
-  value 'DATABASE_MOD', 'Maintainer of the Kitsu media database', value: 'database_mod'
+  value 'ADMIN', 'Administrator/staff member of Kitsu', value: :admin
+  value 'COMMUNITY_MOD', 'Moderator of community behavior', value: :community_mod
+  value 'DATABASE_MOD', 'Maintainer of the Kitsu media database', value: :database_mod
 end
