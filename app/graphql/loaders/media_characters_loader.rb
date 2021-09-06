bf4f7c3c1c9879cbec7bb6d6dc632e52153907b0
Daniel Rassiner <daniel@rassiner.com>
2021-09-05 18:04:05 -0700

    GraphQL - add media characters fancy loader (#1066)
    * add media characters fancy loader
    
    * add sorting by role
    
    * have DESC return main at top (rather than ASC)

---
+++ app/graphql/loaders/media_characters_loader.rb
@@ -0,0 +1,7 @@
+class Loaders::MediaCharactersLoader < Loaders::FancyLoader
+  from MediaCharacter
+
+  sort :created_at
+  sort :updated_at
+  sort :role, on: -> { Arel::Nodes::Subtraction.new(0, MediaCharacter.arel_table[:role]) }
+end
