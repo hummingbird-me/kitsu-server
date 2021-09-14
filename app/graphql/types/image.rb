3950d1c4c4ec36500a8b9b9da914c0d43bd0b0f8
Emma Lejeck <nuck@kitsu.io>
2021-09-13 21:49:16 -0700

    Add support for Shrine images in GraphQL

--- app/graphql/types/image.rb
+++ app/graphql/types/image.rb
@@ -4,12 +4,22 @@ class Types::Image < Types::BaseObject
     description: 'The original image'
 
   def original
-    {
-      name: 'original',
-      url: object.url(:original),
-      width: object.width(:original),
-      height: object.height(:original)
-    }
+    case object
+    when Paperclip::Attachment
+      {
+        name: 'original',
+        url: object.url(:original),
+        width: object.width(:original),
+        height: object.height(:original)
+      }
+    when Shrine::Attacher
+      {
+        name: 'original',
+        url: object.file.url,
+        width: object.file.url,
+        height: object.file.url
+      }
+    end
   end
 
   field :views, [Types::ImageView], null: false do
@@ -17,19 +27,39 @@ class Types::Image < Types::BaseObject
     argument :names, [String], required: false
   end
 
-  def views(names: object.styles.keys)
-    styles = object.styles.keys
-    (names.map(&:to_sym) & styles).map do |style|
-      {
-        name: style,
-        url: object.url(style),
-        width: object.width(style),
-        height: object.height(style)
-      }
+  def views(names: case object; when Paperclip::Attachment then object.styles.keys; when Shrine::Attacher then object.derivatives.keys; end)
+    case object
+    when Paperclip::Attachment
+      styles = object.styles.keys
+      (names.map(&:to_sym) & styles).map do |style|
+        {
+          name: style,
+          url: object.url(style),
+          width: object.width(style),
+          height: object.height(style)
+        }
+      end
+    when Shrine::Attacher
+      derivatives = object.derivatives
+      (names.map(&:to_sym) & derivatives.keys).map do |name|
+        {
+          name: name,
+          url: derivatives[name].url,
+          width: derivatives[name].width,
+          height: derivatives[name].height
+        }
+      end
     end
   end
 
   field :blurhash, String, null: true do
     description 'A blurhash-encoded version of this image'
+  end
+
+  def blurhash
+    case object
+    when Paperclip::Attachment then object.blurhash
+    when Shrine::Attacher then object.file.blurhash
+    end
   end
 end
