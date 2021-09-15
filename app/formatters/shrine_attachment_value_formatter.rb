2b23d001bfbe8ca3d6619cd2a5dc4f62d5d3fba8
Emma Lejeck <nuck@kitsu.io>
2021-09-15 14:30:13 -0700

    Add derivative support to ShrineAttachmentValueFormatter

--- app/formatters/shrine_attachment_value_formatter.rb
+++ app/formatters/shrine_attachment_value_formatter.rb
@@ -1,8 +1,19 @@
 class ShrineAttachmentValueFormatter < JSONAPI::ValueFormatter
   def self.format(value)
-    return nil if value.blank?
-    raise 'Invalid attachment field' unless value.is_a? Shrine::UploadedFile
+    return nil if value.blank? || value.file.blank?
+    raise 'Invalid attachment field' unless value.is_a? Shrine::Attacher
 
-    { original: value.url, meta: {} }
+    output = value.derivatives.transform_values(&:url)
+    output[:original] = value.file.url
+
+    styles_dims = value.derivatives.transform_values do |derivative|
+      {
+        width: derivative.width,
+        height: derivative.height
+      }
+    end
+    output[:meta] = { dimensions: styles_dims }
+
+    output
   end
 end
