dfbac6956aae02a13dca6bcc93b9011ec7b911b7
Emma Lejeck <nuck@kitsu.io>
2021-09-17 20:06:14 -0700

    Add animation support to cover images

--- app/uploaders/cover_image_uploader.rb
+++ app/uploaders/cover_image_uploader.rb
@@ -1,24 +1,52 @@
 require 'image_processing/vips'
+require 'image_processing/mini_magick'
 
 class CoverImageUploader < Shrine
   include ImageUploader
   include PublicUploader
 
   DERIVATIVES = {
-    tiny: ->(vips) {
-      vips.resize_to_fill(840, 200).convert(:jpeg).saver(quality: 90, strip: true).call
+    magick: {
+      tiny: ->(magick) {
+        magick.resize_to_fill(840, 200).set('dispose', 'background').convert(:gif).call
+      },
+      tiny_webp: ->(magick) {
+        magick.resize_to_fill(840, 200).set('dispose', 'background').convert(:webp).call
+      },
+      small: ->(magick) {
+        magick.resize_to_fill(1680, 400).set('dispose', 'background').convert(:gif).call
+      },
+      small_webp: ->(magick) {
+        magick.resize_to_fill(1680, 400).set('dispose', 'background').convert(:webp).call
+      },
+      large: ->(magick) {
+        magick.resize_to_fill(3360, 800).set('dispose', 'background').convert(:gif).call
+      },
+      large_webp: ->(magick) {
+        magick.resize_to_fill(3360, 800).set('dispose', 'background').convert(:webp).call
+      }
     },
-    small: ->(vips) {
-      vips.resize_to_fill(1680, 400).convert(:jpeg).saver(quality: 75, strip: true).call
-    },
-    large: ->(vips) {
-      vips.resize_to_fill(3360, 800).convert(:jpeg).saver(quality: 50, strip: true).call
+    vips: {
+      tiny: ->(vips) {
+        vips.resize_to_fill(840, 200).convert(:jpeg).saver(quality: 90, strip: true).call
+      },
+      small: ->(vips) {
+        vips.resize_to_fill(1680, 400).convert(:jpeg).saver(quality: 75, strip: true).call
+      },
+      large: ->(vips) {
+        vips.resize_to_fill(3360, 800).convert(:jpeg).saver(quality: 50, strip: true).call
+      }
     }
   }.freeze
 
   Attacher.derivatives do |original|
-    vips = ImageProcessing::Vips.source(original)
-
-    DERIVATIVES.transform_values { |proc| proc.call(vips) }
+    info = ImageInfo.new(original)
+    if info.animated?
+      vips = ImageProcessing::Vips.source(original)
+      DERIVATIVES[:vips].transform_values { |proc| proc.call(vips) }
+    else
+      magick = ImageProcessing::MiniMagick.source(original).loader(loader: info.type)
+      DERIVATIVES[:magick].transform_values { |proc| proc.call(magick) }
+    end
   end
 end
