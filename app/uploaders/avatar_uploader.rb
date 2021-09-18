0d911dd0d9e3e6e971fc6a65c2f1488c45d558a8
Emma Lejeck <nuck@kitsu.io>
2021-09-17 20:06:26 -0700

    Add AvatarUploader

---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,61 @@
+require 'image_processing/vips'
+require 'image_processing/mini_magick'
+
+class AvatarUploader < Shrine
+  include ImageUploader
+  include PublicUploader
+
+  DERIVATIVES = {
+    magick: {
+      tiny: ->(magick) {
+        magick.resize_to_fill(40, 40).set('dispose', 'background').convert(:gif).call
+      },
+      tiny_webp: ->(magick) {
+        magick.resize_to_fill(40, 40).set('dispose', 'background').convert(:webp).call
+      },
+      small: ->(magick) {
+        magick.resize_to_fill(64, 64).set('dispose', 'background').convert(:gif).call
+      },
+      small_webp: ->(magick) {
+        magick.resize_to_fill(64, 64).set('dispose', 'background').convert(:webp).call
+      },
+      medium: ->(magick) {
+        magick.resize_to_fill(100, 100).set('dispose', 'background').convert(:gif).call
+      },
+      medium_webp: ->(magick) {
+        magick.resize_to_fill(100, 100).set('dispose', 'background').convert(:webp).call
+      },
+      large: ->(magick) {
+        magick.resize_to_fill(200, 200).set('dispose', 'background').convert(:gif).call
+      },
+      large_webp: ->(magick) {
+        magick.resize_to_fill(200, 200).set('dispose', 'background').convert(:webp).call
+      }
+    },
+    vips: {
+      tiny: ->(vips) {
+        vips.resize_to_fill(40, 40).convert(:jpeg).saver(quality: 100, strip: true).call
+      },
+      small: ->(vips) {
+        vips.resize_to_fill(64, 64).convert(:jpeg).saver(quality: 80, strip: true).call
+      },
+      medium: ->(vips) {
+        vips.resize_to_fill(100, 100).convert(:jpeg).saver(quality: 70, strip: true).call
+      },
+      large: ->(vips) {
+        vips.resize_to_fill(200, 200).convert(:jpeg).saver(quality: 60, strip: true).call
+      }
+    }
+  }.freeze
+
+  Attacher.derivatives do |original|
+    info = ImageInfo.new(original)
+    if info.animated?
+      vips = ImageProcessing::Vips.source(original)
+      DERIVATIVES[:vips].transform_values { |proc| proc.call(vips) }
+    else
+      magick = ImageProcessing::MiniMagick.source(original).loader(loader: info.type)
+      DERIVATIVES[:magick].transform_values { |proc| proc.call(magick) }
+    end
+  end
+end
