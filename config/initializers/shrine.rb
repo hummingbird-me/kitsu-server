2b6b178990fdcb88d254eca56c5c098273a52a41
Emma Lejeck <nuck@kitsu.io>
2021-09-23 16:38:24 -0700

    Separate dev/prod Shrine configs

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -12,37 +12,57 @@ Shrine.plugin :derivatives
 Shrine.plugin :backgrounding
 Shrine.plugin :url_options, store: { host: 'https://media.kitsu.io' }
 
-backblaze = ENV['B2_BUCKET'].presence && Shrine::Storage::S3.new(
-  endpoint: 'https://s3.us-west-002.backblazeb2.com',
-  region: 'us-west-002',
-  bucket: ENV['B2_BUCKET'],
-  access_key_id: ENV['B2_ACCESS_KEY_ID'],
-  secret_access_key: ENV['B2_SECRET_ACCESS_KEY']
-)
+if Rails.env.production?
+  # Primary storage
+  backblaze = Shrine::Storage::S3.new(
+    endpoint: 'https://s3.us-west-002.backblazeb2.com',
+    region: 'us-west-002',
+    bucket: ENV['B2_BUCKET'],
+    access_key_id: ENV['B2_ACCESS_KEY_ID'],
+    secret_access_key: ENV['B2_SECRET_ACCESS_KEY']
+  )
 
-digitalocean = ENV['DO_BUCKET'].presence && Shrine::Storage::S3.new(
-  endpoint: 'https://sfo3.digitaloceanspaces.com',
-  region: 'sfo3',
-  bucket: ENV['DO_BUCKET'],
-  access_key_id: ENV['DO_ACCESS_KEY_ID'],
-  secret_access_key: ENV['DO_SECRET_ACCESS_KEY']
-)
+  # Temporary upload storage
+  cache = Shrine::Storage::S3.new(
+    endpoint: 'https://s3.us-west-002.backblazeb2.com',
+    region: 'us-west-002',
+    bucket: ENV['B2_BUCKET'],
+    access_key_id: ENV['B2_ACCESS_KEY_ID'],
+    secret_access_key: ENV['B2_SECRET_ACCESS_KEY']
+  )
 
-cache = Shrine::Storage::S3.new(
-  endpoint: 'https://s3.us-west-002.backblazeb2.com',
-  region: 'us-west-002',
-  bucket: ENV['B2_UPLOAD_BUCKET'],
-  access_key_id: ENV['B2_ACCESS_KEY_ID'],
-  secret_access_key: ENV['B2_SECRET_ACCESS_KEY']
-)
+  # Mirror storage
+  digitalocean = Shrine::Storage::S3.new(
+    endpoint: 'https://sfo3.digitaloceanspaces.com',
+    region: 'sfo3',
+    bucket: ENV['DO_BUCKET'],
+    access_key_id: ENV['DO_ACCESS_KEY_ID'],
+    secret_access_key: ENV['DO_SECRET_ACCESS_KEY']
+  )
 
-Shrine.storages = {
-  digitalocean: digitalocean,
-  cache: cache,
-  store: backblaze
-}.compact
-
-Shrine.plugin :mirroring, mirror: { store: :digitalocean }
+  Shrine.storages = {
+    store: backblaze,
+    cache: cache,
+    digitalocean: digitalocean
+  }
+  Shrine.plugin :mirroring, mirror: :digitalocean
+else
+  store = Shrine::Storage::S3.new(
+    endpoint: ENV['AWS_ENDPOINT'],
+    bucket: ENV['AWS_BUCKET'] || 'kitsu-media',
+    force_path_style: true
+  )
+  cache = Shrine::Storage::S3.new(
+    endpoint: ENV['AWS_ENDPOINT'],
+    bucket: ENV['AWS_BUCKET'] || 'kitsu-media',
+    prefix: 'cache',
+    force_path_style: true
+  )
+  Shrine.storages = {
+    store: store,
+    cache: cache
+  }
+end
 
 Shrine::Attacher.promote_block do |attacher|
   attacher.atomic_promote
