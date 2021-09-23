5cf16bd4bd5008023f4eb18b4a3f1aafa1745925
Emma Lejeck <nuck@kitsu.io>
2021-09-23 16:26:56 -0700

    Set Shrine to use only B2 and DigitalOcean

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -28,41 +28,21 @@ digitalocean = ENV['DO_BUCKET'].presence && Shrine::Storage::S3.new(
   secret_access_key: ENV['DO_SECRET_ACCESS_KEY']
 )
 
-# This clusterfuck is temporary until we're off AWS
-cache = if ENV['B2_BUCKET'].present?
-          Shrine::Storage::S3.new(
-            endpoint: 'https://s3.us-west-002.backblazeb2.com',
-            region: 'us-west-002',
-            bucket: ENV['B2_BUCKET'],
-            access_key_id: ENV['B2_ACCESS_KEY_ID'],
-            secret_access_key: ENV['B2_SECRET_ACCESS_KEY']
-          )
-        else
-          Shrine::Storage::S3.new({
-            endpoint: ENV['AWS_ENDPOINT'] || nil,
-            region: 'us-east-1',
-            bucket: ENV['AWS_BUCKET'] || 'kitsu-media',
-            force_path_style: true
-          }.compact)
-        end
-
-aws = Shrine::Storage::S3.new({
-  endpoint: ENV['AWS_ENDPOINT'] || nil,
-  region: 'us-east-1',
-  bucket: ENV['AWS_BUCKET'] || 'kitsu-media',
-  force_path_style: true
-}.compact)
+cache = Shrine::Storage::S3.new(
+  endpoint: 'https://s3.us-west-002.backblazeb2.com',
+  region: 'us-west-002',
+  bucket: ENV['B2_UPLOAD_BUCKET'],
+  access_key_id: ENV['B2_ACCESS_KEY_ID'],
+  secret_access_key: ENV['B2_SECRET_ACCESS_KEY']
+)
 
 Shrine.storages = {
-  backblaze: backblaze,
   digitalocean: digitalocean,
   cache: cache,
-  store: aws
+  store: backblaze
 }.compact
 
-Shrine.plugin :mirroring, mirror: {
-  store: Shrine.storages.keys & %i[backblaze digitalocean]
-}
+Shrine.plugin :mirroring, mirror: { store: :digitalocean }
 
 Shrine::Attacher.promote_block do |attacher|
   attacher.atomic_promote
