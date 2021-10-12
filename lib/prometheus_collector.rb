79c3f1f87c88f88bfb5d2f0ba9aede085c19e0ff
Emma Lejeck <nuck@kitsu.io>
2021-10-12 16:09:22 -0700

    Only set up prometheus exporter when in prometheus exporter

--- lib/prometheus_collector.rb
+++ lib/prometheus_collector.rb
@@ -1,4 +1,6 @@
-require 'graphql/tracing'
+if defined?(PrometheusExporter::Server)
+  require 'graphql/tracing'
 
-class PrometheusCollector < GraphQL::Tracing::PrometheusTracing::GraphQLCollector
+  class PrometheusCollector < GraphQL::Tracing::PrometheusTracing::GraphQLCollector
+  end
 end
