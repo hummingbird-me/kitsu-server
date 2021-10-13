33da694c3869ad91eed9cfc21c6f63a1335df85d
Emma Lejeck <nuck@kitsu.io>
2021-10-13 10:10:31 -0700

    Separate OTLP logger

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -2,7 +2,11 @@ if Rails.env.production? || Rails.env.staging?
   require 'opentelemetry/sdk'
   require 'opentelemetry/exporter/otlp'
 
+  otlp_logger = Logger.new($stdout)
+  otlp_logger.level = :debug
+
   OpenTelemetry::SDK.configure do |c|
+    c.logger = otlp_logger
     c.service_name = 'kitsu-api-web'
     c.use 'OpenTelemetry::Instrumentation::Rack'
     c.use 'OpenTelemetry::Instrumentation::ActionPack'
