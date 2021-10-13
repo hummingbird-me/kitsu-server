90e1ff7d545bb9c55e619c96bb030ef68aa2e877
Emma Lejeck <nuck@kitsu.io>
2021-10-12 22:16:14 -0700

    Reduce our opentelemetry integrations to debug

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -5,6 +5,10 @@ if Rails.env.production? || Rails.env.staging?
 
   OpenTelemetry::SDK.configure do |c|
     c.service_name = 'kitsu-api-web'
-    c.use_all
+    c.use 'OpenTelemetry::Instrumentation::Rack'
+    c.use 'OpenTelemetry::Instrumentation::ActionPack'
+    c.use 'OpenTelemetry::Instrumentation::ActiveRecord'
+    c.use 'OpenTelemetry::Instrumentation::Rails'
+    c.use 'OpenTelemetry::Instrumentation::GraphQL'
   end
 end
