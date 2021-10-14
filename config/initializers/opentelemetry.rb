7d6ade58d846a922e294b8c8f6b6c2efa10f270b
Emma Lejeck <nuck@kitsu.io>
2021-10-13 19:20:32 -0700

    Switch back to use_all

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -8,10 +8,6 @@ if Rails.env.production? || Rails.env.staging?
   OpenTelemetry::SDK.configure do |c|
     c.logger = otlp_logger
     c.service_name = 'kitsu-api-web'
-    c.use 'OpenTelemetry::Instrumentation::Rack'
-    c.use 'OpenTelemetry::Instrumentation::ActionPack'
-    c.use 'OpenTelemetry::Instrumentation::ActiveRecord'
-    c.use 'OpenTelemetry::Instrumentation::Rails'
-    c.use 'OpenTelemetry::Instrumentation::GraphQL'
+    c.use_all
   end
 end
