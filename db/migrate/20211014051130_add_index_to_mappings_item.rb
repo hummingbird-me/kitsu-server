6d84e611843dee4adb5a7333abf04922b0e087e4
Emma Lejeck <nuck@kitsu.io>
2021-10-13 23:26:00 -0700

    Fix that migration oops

--- db/migrate/20211014051130_add_index_to_mappings_item.rb
+++ db/migrate/20211014051130_add_index_to_mappings_item.rb
@@ -1,6 +1,6 @@
 class AddIndexToMappingsItem < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
   def change
-    create_index :mappings, %i[item_type item_id], algorithm: :concurrently
+    add_index :mappings, %i[item_type item_id], algorithm: :concurrently
   end
 end
