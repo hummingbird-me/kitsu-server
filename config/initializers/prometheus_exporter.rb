f45039ab6ddc1f10abacbb29aa06b5006696d6aa
Emma Lejeck <nuck@kitsu.io>
2021-10-15 09:44:07 -0700

    Use local prometheus exporter in test/dev

--- config/initializers/prometheus_exporter.rb
+++ config/initializers/prometheus_exporter.rb
@@ -2,3 +2,13 @@ require 'prometheus_exporter/client'
 require 'prometheus_exporter/middleware'
 
 Rails.application.middleware.unshift PrometheusExporter::Middleware
+
+if Rails.env.development? || Rails.env.test?
+  require 'prometheus_exporter/server'
+
+  server = PrometheusExporter::Server::WebServer.new bind: 'localhost', port: 9394
+  server.start
+  PrometheusExporter::Client.default = PrometheusExporter::LocalClient.new(
+    collector: server.collector
+  )
+end
