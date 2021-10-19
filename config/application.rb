d53add5497614dbc298334e1e6b63591184447b5
Emma Lejeck <nuck@kitsu.io>
2021-10-19 14:39:09 -0700

    Downgrade back to Rails 5.2.4.4

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ Bundler.require(*Rails.groups)
 module Kitsu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 5.2
 
     # Enable assets (used by rails_admin, emails)
     config.assets.enabled = true
