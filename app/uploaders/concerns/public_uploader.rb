2bc493a52aa2289b8cce1c59bba4b507a34cfbf5
Emma Lejeck <nuck@kitsu.io>
2021-12-02 23:55:47 -0800

    Fix keyword params in PublicUploader

--- app/uploaders/concerns/public_uploader.rb
+++ app/uploaders/concerns/public_uploader.rb
@@ -2,6 +2,6 @@ module PublicUploader
   extend ActiveSupport::Concern
 
   included do
-    plugin :url_options, Shrine.opts[:url_options].deep_merge(store: { public: true })
+    plugin :url_options, **Shrine.opts[:url_options].deep_merge(store: { public: true })
   end
 end
