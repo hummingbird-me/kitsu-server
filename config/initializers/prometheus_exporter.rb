8d0bcffc4f62c941b363cab6ba75f41e17e3aef3
Peter Lejeck <nuck@kitsu.io>
2021-12-03 20:49:23 -0800

    Create NullClient for prometheus in dev/test (#1139)

--- config/initializers/prometheus_exporter.rb
+++ config/initializers/prometheus_exporter.rb
@@ -6,9 +6,9 @@ Rails.application.middleware.unshift PrometheusExporter::Middleware
 if Rails.env.development? || Rails.env.test?
   require 'prometheus_exporter/server'
 
-  server = PrometheusExporter::Server::WebServer.new bind: 'localhost', port: 9394
-  server.start
-  PrometheusExporter::Client.default = PrometheusExporter::LocalClient.new(
-    collector: server.collector
-  )
+  class NullClient < PrometheusExporter::Client
+    def send(*); end
+  end
+
+  PrometheusExporter::Client.default = NullClient.new
 end
