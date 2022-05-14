60366646a5cc608b1b7436fde5981ea80cb64944
Emma Lejeck <nuck@kitsu.io>
2022-05-13 22:49:19 -0700

    Add error handling to account creation mutation

--- app/graphql/mutations/account/create.rb
+++ app/graphql/mutations/account/create.rb
@@ -28,8 +28,15 @@ class Mutations::Account::Create < Mutations::Base
       description: 'An external identity to associate with the account on creation'
   end
   result Types::Account
+  errors Types::Errors::Validation
 
   def resolve(input:)
     AccountMutator.create(**input)
+  rescue ActiveRecord::RecordInvalid => e
+    validation_errors = Types::Errors::Validation.for_record(e.record, transform_path: ->(path) {
+      path = path.map { |p| p == 'password_digest' ? 'password' : p }
+      ['input', *path]
+    })
+    errors.push(*validation_errors)
   end
 end
