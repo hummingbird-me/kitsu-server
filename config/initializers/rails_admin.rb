24004a6a111beae5ddb64d79ef635b85b541d7a4
Emma Lejeck <nuck@kitsu.io>
2022-05-29 23:44:24 -0700

    Remove deprecated RA config

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -37,9 +37,6 @@ RailsAdmin.config do |config|
     end
   end
 
-  # Display as scrollable list
-  config.total_columns_width = 9_999_999
-
   # Display canonical_title for label on media
   config.label_methods += %i[canonical_title site_name]
 
