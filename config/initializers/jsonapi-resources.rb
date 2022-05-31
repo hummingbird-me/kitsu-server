0233621e94f72f7ebaddce3fd8ba7861e330ebf4
Emma Lejeck <nuck@kitsu.io>
2022-05-30 17:02:09 -0700

    Update monkeypatch for include errors in JR

--- config/initializers/jsonapi-resources.rb
+++ config/initializers/jsonapi-resources.rb
@@ -1,11 +1,24 @@
-module SilenceIncludeWarnings
+module FixIncludeErrors
   def get_related(current_path)
-    silence_warnings do
-      super(current_path)
+    current = @include_directives_hash
+    current_resource_klass = @resource_klass
+    current_path.split('.').each do |fragment|
+      fragment = fragment.to_sym
+
+      if current_resource_klass
+        current_relationship = current_resource_klass._relationships[fragment]
+        current_resource_klass = current_relationship.try(:resource_klass)
+      end
+
+      include_in_join = @force_eager_load || !current_relationship || current_relationship.eager_load_on_include
+
+      current[:include_related][fragment] ||= { include: false, include_related: {}, include_in_join: include_in_join }
+      current = current[:include_related][fragment]
     end
+    current
   end
 end
-JSONAPI::IncludeDirectives.prepend(SilenceIncludeWarnings)
+JSONAPI::IncludeDirectives.prepend(FixIncludeErrors)
 
 JSONAPI.configure do |config|
   # Keying
