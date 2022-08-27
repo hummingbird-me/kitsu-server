c166ff75f1d28fae8a34f1a5e3faaa76c06160a5
Shomy <61943525+ShomyKohai@users.noreply.github.com>
2022-08-28 01:23:50 +0200

    Made fields non nullable
    Made the fields `validate_find` and `validate_replace` non nullable to maintain consistency (as this field must be populated in database)

--- app/graphql/types/profile_link_site.rb
+++ app/graphql/types/profile_link_site.rb
@@ -10,8 +10,10 @@ class Types::ProfileLinkSite < Types::BaseObject
     description: 'Name of the external profile website.'
 
   field :validate_find, String,
+    null: false,
     description: 'Regex pattern used to validate the profile link.'
 
   field :validate_replace, String,
+    null: false,
     description: 'Pattern to be replaced after validation.'
 end
