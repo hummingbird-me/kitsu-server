378698062cc83fc579202b974d7d3bcbef756e6a
Shomy <61943525+ShomyKohai@users.noreply.github.com>
2022-12-15 12:40:39 +0100

    Fix for checks pt. 2

--- app/graphql/mutations/favorite/delete.rb
+++ app/graphql/mutations/favorite/delete.rb
@@ -1,33 +1,32 @@
 class Mutations::Favorite::Delete < Mutations::Base
-	prepend RescueValidationErrors
+  prepend RescueValidationErrors
 
-	argument :input,
-		Types::Input::Favorite::Delete,
-		required: true,
-		description: 'Delete a Favorite Entry',
-		as: :favorite
+  argument :input,
+    Types::Input::Favorite::Delete,
+    required: true,
+    description: 'Delete a Favorite Entry',
+    as: :favorite
 
-	field :favorite, Types::GenericDelete, null: true
-	field :errors, [Types::Interface::Error], null: true
+  field :favorite, Types::GenericDelete, null: true
+  field :errors, [Types::Interface::Error], null: true
 
-	def load_favorite(value)
-		Favorite.find(value.id)
-	end
+  def load_favorite(value)
+    Favorite.find(value.id)
+  end
 
-	def authorized?(favorite:)
-		return true if FavoritePolicy.new(context[:token], favorite).destroy?
+  def authorized?(favorite:)
+    return true if FavoritePolicy.new(context[:token], favorite).destroy?
 
-		[false, {
-			errors: [
-				{ message: 'Not Authorized', code: 'NotAuthorized' }
-			]
-		}]
-	end
+    [false, {
+      errors: [
+        { message: 'Not Authorized', code: 'NotAuthorized' }
+      ]
+    }]
+  end
 
-	def resolve(favorite:)
-		favorite.destroy!
+  def resolve(favorite:)
+    favorite.destroy!
 
-		{ favorite: { id: favorite.id } }
-	end
+    { favorite: { id: favorite.id } }
+  end
 end
-  
