396e0ad2c57a28f66dead9889ecc41d86bcdd560
toyhammered <daniel@rassiner.com>
2023-01-18 20:47:40 -0800

    add new query for media by id and type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -143,6 +143,19 @@ class Types::QueryType < GraphQL::Schema::Object
     end
   end
 
+  field :find_media_by_id_and_type, Types::Interface::Media, null: true do
+    description 'Find a single Media by ID and Type'
+    argument :id, ID, required: true
+    argument :media_type, Types::Enum::MediaType, required: true
+  end
+
+  def find_media_by_id_and_type(id:, media_type:)
+    case media_type
+    when 'anime' then find_anime_by_id(id: id)
+    when 'manga' then find_manga_by_id(id: id)
+    end
+  end
+
   field :random_media, Types::Interface::Media, null: false do
     description 'Random anime or manga'
     argument :media_type, Types::Enum::MediaType, required: true
@@ -316,6 +329,8 @@ class Types::QueryType < GraphQL::Schema::Object
   def find_library_entry_by_id(id:)
     Loaders::RecordLoader.for(::LibraryEntry, token: context[:token]).load(id)
   end
+
+  # find_library_entry_by_media(media_id:, media_type:)
 
   field :library_entries_by_media_type, Types::LibraryEntry.connection_type, null: true do
     description 'List of Library Entries by MediaType'
