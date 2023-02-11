877beb73b6d05605d82ec48cf87aa665471ce02f
Emma Lejeck <nuck@kitsu.io>
2023-02-11 00:04:56 -0800

    Fix Faker usage in chapters factory

--- spec/factories/chapters.rb
+++ spec/factories/chapters.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     association :volume, factory: :volume
     titles { { en_jp: Faker::Name.name } }
     canonical_title { 'en_jp' }
-    description { { en: Faker::Lorem.paragraph(sentences: 4) } }
+    description { { en: Faker::Lorem.paragraph(sentence_count: 4) } }
     length { rand(20..60) }
     published { Faker::Date.between(from: 20.years.ago, to: Date.today) }
     volume_number { rand(1..10) }
