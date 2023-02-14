c917abb9bf8e8082d8612d731ffec63d6f6a6232
Emma Lejeck <nuck@kitsu.io>
2023-02-13 19:41:03 -0800

    Replace LoremPixel with LoremFlickr

--- spec/factories/site_announcements.rb
+++ spec/factories/site_announcements.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     association :user, strategy: :build
     title { Faker::Lorem.sentence(word_count: 5) }
     description { { en: Faker::Lorem.sentences(number: 2) } }
-    image_url { Faker::LoremPixel.image }
+    image_url { Faker::LoremFlickr.image }
     link { Faker::Internet.url }
   end
 end
