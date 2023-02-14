4782f4ac99343629d2852a64ae2cc9d7fb68fc4a
Emma Lejeck <nuck@kitsu.io>
2023-02-13 19:25:26 -0800

    Fix fakers for site announcement

--- spec/factories/site_announcements.rb
+++ spec/factories/site_announcements.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :site_announcement do
     association :user, strategy: :build
-    title { Faker::Lorem.sentence }
-    description { { en: Faker::Lorem.sentence(number: 3) } }
+    title { Faker::Lorem.sentence(word_count: 5) }
+    description { { en: Faker::Lorem.sentences(number: 2) } }
     image_url { Faker::LoremPixel.image }
     link { Faker::Internet.url }
   end
