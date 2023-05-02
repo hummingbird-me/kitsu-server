d1c96f5a164f939a29c4d9369066e94db51f0a9c
Emma Lejeck <nuck@kitsu.io>
2023-05-02 00:16:25 -0700

    Hoist message default up to Error interface

--- app/graphql/types/interface/error.rb
+++ app/graphql/types/interface/error.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Types::Interface::Error
   include Types::Interface::Base
 
@@ -7,6 +9,12 @@ module Types::Interface::Error
   field :message, String,
     null: false,
     description: 'A description of the error'
+
+  def message
+    message_key = self.class.name.split('::')[2..].join('.').underscore
+
+    I18n.t("graphql.errors.#{message_key}")
+  end
 
   field :path, [String],
     null: true,
