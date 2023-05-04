49895dc1c776c9343eec4ed991d09bdb6e5ecc5e
Emma Lejeck <nuck@kitsu.io>
2023-05-03 20:10:56 -0700

    Add Account#change_password mutation

--- app/graphql/mutations/account.rb
+++ app/graphql/mutations/account.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 class Mutations::Account < Mutations::Namespace
   field :send_password_reset,
     mutation: Mutations::Account::SendPasswordReset,
     description: 'Send a password reset email'
+  field :change_password, mutation: Mutations::Account::ChangePassword
 end
