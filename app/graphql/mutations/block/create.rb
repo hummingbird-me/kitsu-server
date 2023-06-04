28ba49b4d96f8b8391706b5eb9f5491d318575e0
Shomy <61943525+ShomyKohai@users.noreply.github.com>
2023-06-04 10:46:30 +0200

    Add NotAuthorized to Block

--- app/graphql/mutations/block/create.rb
+++ app/graphql/mutations/block/create.rb
@@ -12,21 +12,23 @@ class Mutations::Block::Create < Mutations::Base
   end
   result Types::Block
   errors Types::Errors::NotAuthenticated,
-    Types::Errors::NotFound
+    Types::Errors::NotFound,
+    Types::Errors::NotAuthorized
 
   def ready?(blocked_id:, **)
     authenticate!
+    return errors << Types::Errors::NotAuthenticated.build if current_user.nil?
     @block_user = User.find_by(id: blocked_id)
     return errors << Types::Errors::NotFound.build(path: %w[input blocked_id]) if @block_user.nil?
-    true
-  end
-
-  def resolve(blocked_id:, **)
     @block = Block.new(
       user_id: current_user.id,
       blocked_id:
     )
     authorize!(@block, :create?)
+    true
+  end
+
+  def resolve(**)
     @block.tap(&:save!)
   end
 end
