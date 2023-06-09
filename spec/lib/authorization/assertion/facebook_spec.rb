e37e1f3cd91ff6525655e7c40c04236a2d00e704
Emma Lejeck <nuck@kitsu.io>
2023-06-09 04:11:04 -0700

    Fix facebook assertion grant spec webmocks

--- spec/lib/authorization/assertion/facebook_spec.rb
+++ spec/lib/authorization/assertion/facebook_spec.rb
@@ -1,32 +1,35 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'authorization/assertion/facebook'
 
 RSpec.describe Authorization::Assertion::Facebook do
   before do
-    stub_request(:get, %r{https://graph.facebook.com/v6.0/me\?.*})
+    stub_request(:get, %r{https://graph.facebook.com/v.*/me\?.*})
       .to_return(body: fixture('auth/facebook/self.json'))
-    stub_request(:get, %r{https://graph.facebook.com/v6.0/me/friends\?.*})
+    stub_request(:get, %r{https://graph.facebook.com/v.*/me/friends\?.*})
       .to_return(body: fixture('auth/facebook/friends.json'))
   end
+
   let(:facebook_auth) { Authorization::Assertion::Facebook.new('any token') }
 
   describe '#user!' do
-    let!(:user) { create :user, facebook_id: '1659565134412042' }
-
     subject { facebook_auth.user! }
 
-    it 'should return user' do
+    let!(:user) { create :user, facebook_id: '1659565134412042' }
+
+    it 'returns user' do
       expect(subject).to eq(user)
     end
   end
 
   describe '#auto_follows' do
+    subject { Follow.where(follower: user, followed: friend) }
+
     let!(:user) { create :user, facebook_id: '1659565134412042' }
     let!(:friend) { create :user, facebook_id: '10204220238175291' }
 
     before { facebook_auth.auto_follows }
-
-    subject { Follow.where(follower: user, followed: friend) }
 
     it 'follow should exist' do
       expect(subject).to exist
