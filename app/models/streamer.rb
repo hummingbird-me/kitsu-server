31e2299fe7016824623d1b1ed7dd61b195285db5
Emma Lejeck <nuck@kitsu.io>
2023-08-20 19:02:04 -0700

    Add Streamer.by_name scope

--- app/models/streamer.rb
+++ app/models/streamer.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 class Streamer < ApplicationRecord
   has_many :streaming_links
   has_many :videos
 
   validates :site_name, presence: true
+
+  scope :by_name, ->(*names) { where('lower(site_name) IN (?)', names.flatten.map(&:downcase)) }
 
   def self.find_by_name(name)
     Streamer.where('lower(site_name) = ?', name.downcase).first
