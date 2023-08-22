30ca4f8ae7888813ba149e1a0bc7123ba9a07166
Emma Lejeck <nuck@kitsu.io>
2023-08-21 22:14:50 -0700

    Create a TypesenseSearchService

---
+++ app/services/typesense_search_service.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+# Provides a helpful base class for search services backed by typesense.
+#
+# @abstract
+class TypesenseSearchService < SearchService
+  def total_count
+    query_results.count
+  end
+
+  private
+
+  def apply_auto_filter_for(scope, field, filter_param: field)
+    if filters[filter_param]
+      scope.filter(auto_query_for(field, filters[filter_param]))
+    else
+      scope
+    end
+  end
+
+  def auto_query_for(field, value)
+    case value
+    when Range
+      { field => "[#{value.min}..#{value.max}]" }
+    when Array
+      { field => "=[#{value.join(',')}]" }
+    when Date
+      { field => "=#{value.to_time.to_i}" }
+    else
+      { field => "=#{value}" }
+    end
+  end
+
+  def apply_order_to(scope)
+    scope.sort(orders)
+  end
+
+  def apply_limit_to(scope)
+    return scope unless _limit
+
+    scope.per(_limit)
+  end
+
+  def apply_offset_to(scope)
+    return scope unless _offset
+
+    scope.page((_offset / _limit).floor)
+  end
+
+  def orders
+    @orders ||=
+      _order
+      &.map { |o| o.transform_keys { |k| k == '_score' ? '_text_match' : k } }
+      &.reduce(&:merge)
+  end
+
+  # Combine the queries and filters into a single hash
+  def filters
+    @filters ||= _filters.merge(_queries)
+  end
+end
