195a1963d687a16ad11d2fea51fd9532c20a2ea5
Emma Lejeck <nuck@kitsu.io>
2023-09-10 20:45:20 -0700

    Update to Puma 6.x

--- config/puma.rb
+++ config/puma.rb
@@ -4,21 +4,14 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 4)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 6)
 threads 0, threads_count
 
-nakayoshi_fork true
 preload_app!
 quiet
 
-rackup      DefaultRackup
 port        ENV['PORT'] || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
   require 'prometheus_exporter/instrumentation'
-
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-  ActiveRecord::Base.connection.execute("SET statement_timeout = '12s'")
   PrometheusExporter::Instrumentation::Process.start(type: 'web')
   PrometheusExporter::Instrumentation::ActiveRecord.start(
     custom_labels: { type: 'web' },
