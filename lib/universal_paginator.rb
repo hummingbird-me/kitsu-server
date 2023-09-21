4c29a8a8e3c9e433756aed854e7c27a1535e29f9
Emma Lejeck <nuck@kitsu.io>
2023-09-20 20:32:54 -0700

    Strengthen UniversalPaginator

--- lib/universal_paginator.rb
+++ lib/universal_paginator.rb
@@ -4,13 +4,18 @@ class UniversalPaginator < JSONAPI::Paginator
   attr_reader :params
 
   def initialize(params)
+    super
     @params = params
     # Initialize sub-paginator
     paginator
   end
 
   def offset?
-    params[:offset].present? || params[:limit].present?
+    if params&.fetch(:offset, nil) || params&.fetch(:limit, nil)
+      true
+    else
+      false
+    end
   end
 
   def page?
@@ -32,10 +37,12 @@ class UniversalPaginator < JSONAPI::Paginator
     if offset?
       paginator.apply(relation, _order_options)
     else
+      params = params.to_h
       # We apply the PagedPaginator logic ourselves because it uses offset/limit
       # We want to have page/per method calls to be applied to the relation
       relation = relation.page(params[:number]) if params[:number].present?
       relation = relation.per(params[:size]) if params[:size].present?
+      relation
     end
   end
 end
