baf31197c90395285f21d41d4fef06b30182a08a
Emma Lejeck <nuck@kitsu.io>
2023-09-25 18:13:13 -0700

    Stop applying NSFW scopes in graphql

--- app/services/algolia_graphql_search_service.rb
+++ app/services/algolia_graphql_search_service.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AlgoliaGraphqlSearchService
   def initialize(model, token)
     @model = model
@@ -6,29 +8,13 @@ class AlgoliaGraphqlSearchService
   end
 
   def search(query, restrict_searchable_attributes: nil, filters: nil, **opts)
-    opts[:filters] = scoped_filters(filters)
+    opts[:filters] = filters if filters.present?
     opts[:restrict_searchable_attributes] = format_attributes(restrict_searchable_attributes)
 
     @index.search(query, opts.compact)
   end
 
-  def scope
-    return @scope if @scope
-    policy = Pundit::PolicyFinder.new(@model).policy
-    @scope = policy::AlgoliaScope.new(@token).resolve
-  rescue NameError
-    ''
-  end
-
   private
-
-  def scoped_filters(filters)
-    return filters if scope.blank?
-    # if there are no filters supplied, just return the scope
-    return scope if filters.blank?
-
-    "#{filters} AND #{scope}"
-  end
 
   def format_attributes(attributes)
     attributes.map { |attribute| attribute.camelize(:lower) }
