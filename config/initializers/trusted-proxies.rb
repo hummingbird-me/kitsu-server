c73fc59e60b02ec5c11d0f288f6b3bc5eadf87a5
Emma Lejeck <nuck@kitsu.io>
2023-12-14 00:18:01 -0800

    Try to make trusted-proxies init more robust

--- config/initializers/trusted-proxies.rb
+++ config/initializers/trusted-proxies.rb
@@ -1,30 +1,42 @@
 # frozen_string_literal: true
 
-if Rails.env.production? || Rails.env.staging?
-  # Grab Cloudflare IPs
-  cloudflare_ips = %w[
-    https://www.cloudflare.com/ips-v4
-    https://www.cloudflare.com/ips-v6
-  ].map { |url| URI.parse(url).open.read.split("\n") }.flatten
+Sentry.with_scope do |scope|
+  scope.set_transaction_name('initializers/trusted-proxies')
 
-  # Grab IPs for all our Droplets
-  droplet_ips = if ENV['DIGITALOCEAN_TOKEN'].present?
-    HTTP
-      .auth("Bearer #{ENV.fetch('DIGITALOCEAN_TOKEN', nil)}")
-      .get('https://api.digitalocean.com/v2/droplets?per_page=200')
-      .parse['droplets'].map { |droplet|
-      [
-        droplet['networks']['v4'].pluck('ip_address'),
-        droplet['networks']['v6'].pluck('ip_address')
-      ]
-    }.flatten
-  else
-    []
+  if true || Rails.env.production? || Rails.env.staging?
+    cloudflare_ips = begin
+      # Grab Cloudflare IPs
+      %w[
+        https://www.cloudflare.com/ips-v4
+        https://www.cloudflare.com/ips-v6
+      ].map { |url| URI.parse(url).open.read.split("\n") }.flatten
+    rescue StandardError => e
+      Sentry.capture_exception(e)
+      []
+    end
+
+    # Grab IPs for all our Droplets
+    droplet_ips = begin
+      next [] if ENV['DIGITALOCEAN_TOKEN'].blank?
+
+      response = HTTP.auth("Bearer #{ENV.fetch('DIGITALOCEAN_TOKEN', nil)}")
+                     .get('https://api.digitalocean.com/v2/droplets?per_page=200')
+
+      response.parse['droplets'].map { |droplet|
+        [
+          droplet['networks']['v4'].pluck('ip_address'),
+          droplet['networks']['v6'].pluck('ip_address')
+        ]
+      }.flatten
+    rescue StandardError => e
+      Sentry.capture_exception(e)
+      []
+    end
+
+    env_ips = ENV['TRUSTED_PROXIES']&.split(',') || []
+
+    trusted_ips = [cloudflare_ips, droplet_ips, env_ips].flatten.map { |ip| IPAddr.new(ip) }
+
+    Rails.application.config.action_dispatch.trusted_proxies = trusted_ips
   end
-
-  env_ips = ENV['TRUSTED_PROXIES']&.split(',') || []
-
-  trusted_ips = [cloudflare_ips, droplet_ips, env_ips].flatten.map { |ip| IPAddr.new(ip) }
-
-  Rails.application.config.action_dispatch.trusted_proxies = trusted_ips
 end
