175dc21a35a1dc9072855c00003f2accb06669fa
Emma Lejeck <nuck@kitsu.io>
2023-12-16 18:15:28 -0800

    Sample 50% of transactions over 20 seconds

--- config/initializers/sentry-ruby.rb
+++ config/initializers/sentry-ruby.rb
@@ -2,15 +2,15 @@
 
 Sentry.init do |config|
   config.release = Rails.root.join('.version').read.strip if Rails.root.join('.version').exist?
-  config.traces_sample_rate = 0.0
-  config.profiles_sample_rate = 0.0
+  config.traces_sample_rate = 0.5
+  config.profiles_sample_rate = 1.0
   config.breadcrumbs_logger = %i[sentry_logger http_logger active_support_logger]
   config.excluded_exceptions += [
     'Rack::Utils::InvalidParameterError' # Rack was unable to decode a parameter
   ]
   config.before_send_transaction = ->(event, _hint) do
     duration = event.timestamp - event.start_timestamp
-    event if duration > 5.seconds
+    event if duration > 20.seconds
   end
 end
 
