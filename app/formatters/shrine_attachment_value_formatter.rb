2095020bcc1d547b71baee3c24abf01fe933138b
Emma Lejeck <nuck@kitsu.io>
2023-12-16 18:57:56 -0800

    Wrap ShrineAttachmentValueFormatter in a span

--- app/formatters/shrine_attachment_value_formatter.rb
+++ app/formatters/shrine_attachment_value_formatter.rb
@@ -1,20 +1,28 @@
+# frozen_string_literal: true
+
 class ShrineAttachmentValueFormatter < JSONAPI::ValueFormatter
   def self.format(value)
     return nil if value.blank?
     raise 'Invalid attachment field' unless value.is_a? Shrine::Attacher
     return nil if value.file.blank?
 
-    output = value.derivatives.transform_values(&:url)
-    output[:original] = value.file.url
+    Sentry.with_child_span(op: 'jsonapi.format_value',
+      description: 'ShrineAttachmentValueFormatter') do |span|
+      span.set_data(:record, value.record.to_global_id.to_s)
+      span.set_data(:name, value.name.to_s)
 
-    styles_dims = value.derivatives.transform_values do |derivative|
-      {
-        width: derivative.width,
-        height: derivative.height
-      }
+      output = value.derivatives.transform_values(&:url)
+      output[:original] = value.file.url
+
+      styles_dims = value.derivatives.transform_values do |derivative|
+        {
+          width: derivative.width,
+          height: derivative.height
+        }
+      end
+      output[:meta] = { dimensions: styles_dims }
+
+      output
     end
-    output[:meta] = { dimensions: styles_dims }
-
-    output
   end
 end
