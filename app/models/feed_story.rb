b611c5f2c40fe32aef0c850ba244adc6ae63cc2b
Emma Lejeck <nuck@kitsu.io>
2024-03-29 00:39:54 -0700

    Set up CPK on FeedStory

--- app/models/feed_story.rb
+++ app/models/feed_story.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class FeedStory < ApplicationRecord
+  self.primary_keys = %i[feed_id story_id]
   belongs_to :story, inverse_of: :feed_stories
   belongs_to :feed, class_name: 'NewFeed', inverse_of: :stories
 end
