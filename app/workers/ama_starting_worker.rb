b7eb326a598dc26f7b236053d357e28e0dedc287
Emma Lejeck <nuck@kitsu.io>
2024-04-30 19:49:54 -0700

    Serialize/deserialize AMA in AMAStartingWorker

--- app/workers/ama_starting_worker.rb
+++ app/workers/ama_starting_worker.rb
@@ -2,7 +2,12 @@ class AMAStartingWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'soon'
 
+  def self.perform_async(ama)
+    super(ama.to_global_id.to_s)
+  end
+
   def perform(ama)
+    ama = GlobalID::Locator.locate(ama)
     ama.send_ama_notification
   end
 end
