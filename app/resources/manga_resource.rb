5de3fdba29bdbfb2ebdfc922c17d36cbd88d32bc
Emma Lejeck <nuck@kitsu.io>
2024-10-06 14:22:35 -0700

    Hook up typesense for jsonapi manga search

--- app/resources/manga_resource.rb
+++ app/resources/manga_resource.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class MangaResource < MediaResource
   attributes :chapter_count, :volume_count, :serialization
   attribute :manga_type # DEPRECATED
@@ -9,4 +11,8 @@ class MangaResource < MediaResource
   has_many :chapters
   has_many :manga_characters
   has_many :manga_staff
+
+  def self._search_service
+    MangaSearchService if Flipper[:typesense_manga_search].enabled?(User.current)
+  end
 end
