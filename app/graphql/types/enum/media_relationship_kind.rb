5e1bc47895c12d1824754aadcf52b3a3b8610898
Emma Lejeck <nuck@kitsu.io>
2024-10-13 14:59:56 -0700

    Fix MediaRelationshipKind enum values

--- app/graphql/types/enum/media_relationship_kind.rb
+++ app/graphql/types/enum/media_relationship_kind.rb
@@ -3,16 +3,16 @@
 class Types::Enum::MediaRelationshipKind < Types::Enum::Base
   description 'The relationship kind from one media entry to another'
 
-  value 'SEQUEL', value: :sequel
-  value 'PREQUEL', value: :prequel
-  value 'ALTERNATIVE_SETTING', value: :alternative_setting
-  value 'ALTERNATIVE_VERSION', value: :alternative_version
-  value 'SIDE_STORY', value: :side_story
-  value 'PARENT_STORY', value: :parent_story
-  value 'SUMMARY', value: :summary
-  value 'FULL_STORY', value: :full_story
-  value 'SPINOFF', value: :spinoff
-  value 'ADAPTATION', value: :adaptation
-  value 'CHARACTER', value: :character
-  value 'OTHER', value: :other
+  value 'SEQUEL', value: 'sequel'
+  value 'PREQUEL', value: 'prequel'
+  value 'ALTERNATIVE_SETTING', value: 'alternative_setting'
+  value 'ALTERNATIVE_VERSION', value: 'alternative_version'
+  value 'SIDE_STORY', value: 'side_story'
+  value 'PARENT_STORY', value: 'parent_story'
+  value 'SUMMARY', value: 'summary'
+  value 'FULL_STORY', value: 'full_story'
+  value 'SPINOFF', value: 'spinoff'
+  value 'ADAPTATION', value: 'adaptation'
+  value 'CHARACTER', value: 'character'
+  value 'OTHER', value: 'other'
 end
